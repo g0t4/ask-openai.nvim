6eeb27f47cc5dc046b943c85562cde09c67fcce8
Wes Higbee <wes.mcclure@gmail.com>
2024-11-04 00:23:07 -0600

    testing

--- lua/ask-openai/init.lua
+++ lua/ask-openai/init.lua
@@ -1,8 +1,6 @@
 local M = {}
 
 function M.setup(opts)
-    vim.notify("setup called")
-
 
     -- TODO modify this to use github copilot subscription/api using chat model and see how it performs vs openai gpt4o (FYI windows terminal chat w/ copilot was clearly inferior vs gpt4o but ... win term chat might have been using gpt3.5 or smth else, just FYI"
     -- TODO port to lua
