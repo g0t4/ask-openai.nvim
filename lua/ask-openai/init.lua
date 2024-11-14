72a850d1c9be313acf1a1e36084b764f97bfaa6c
Wes Higbee <wes.mcclure@gmail.com>
2024-11-14 02:02:23 -0600

    cleanup

--- lua/ask-openai/init.lua
+++ lua/ask-openai/init.lua
@@ -1,8 +1,6 @@
 local M = {}
 
 function M.setup(opts)
-    require("ask-openai.cmd-suggest")
-
     require("ask-openai.config").set_user_opts(opts)
 
     -- TODO modify this to use github copilot subscription/api using chat model and see how it performs vs openai gpt4o (FYI windows terminal chat w/ copilot was clearly inferior vs gpt4o but ... win term chat might have been using gpt3.5 or smth else, just FYI"
