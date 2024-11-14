9e6d011b83a024079e7ecb0e19e5a6d1955ea421
Wes Higbee <wes.mcclure@gmail.com>
2024-11-14 02:01:41 -0600

    split out

--- lua/ask-openai/cmd-suggest.lua
+++ lua/ask-openai/cmd-suggest.lua
@@ -87,4 +87,41 @@ end
 
 M.GetCommandSuggestion = GetCommandSuggestion
 
+function M.setup_cmd_suggestions()
+
+    local function trim_null_characters(input)
+        -- Replace null characters (\x00) with an empty string
+        -- was getting ^@ at end of command output w/ system call (below)
+        if input == nil then
+            return ""
+        end
+        return input:gsub("%z", "")
+    end
+
+    function AskOpenAILua()
+        -- leave name slightly different so no confusion about vimscript func vs lua func
+        local cmdline = vim.fn.getcmdline()
+
+        local stdin_text = ' env: nvim (neovim) command mode (return a valid command w/o the leading : ) \n question: ' ..
+            cmdline
+
+        local result = GetCommandSuggestion(stdin_text)
+
+        return trim_null_characters(result)
+    end
+
+    vim.cmd [[
+        function! AskOpenAI()
+            " just a wrapper so the CLI shows "AskOpenAI" instead of "luaeval('AskOpenAILua()')"
+            return luaeval('AskOpenAILua()')
+            " also, FYI, luaeval and the nature of a cmap modifying cmdline means there really is no way to present errors short of putting them into the command line, which is fine and how I do it in my CLI equivalents of this
+            "   only issue w/ putting into cmdline is no UNDO AFAIK for going back to what you had typed... unlike how I can do that in fish shell and just ctrl+z to undo the error message, but error messages are gonna be pretty rare so NBD for now
+        endfunction
+    ]]
+
+    -- [e]valuate expression AskOpenAI() in command-line mode
+    -- DO NOT SET silent=true, messes up putting result into cmdline
+    vim.api.nvim_set_keymap('c', '<C-b>', '<C-\\>eAskOpenAI()<CR>', { noremap = true, })
+end
+
 return M
