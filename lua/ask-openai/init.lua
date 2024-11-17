67c149c3c761373f2dfb8fea30d74c9079ea8a01
Wes Higbee <wes.mcclure@gmail.com>
2024-11-17 01:14:30 -0600

    rename

--- lua/ask-openai/init.lua
+++ lua/ask-openai/init.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 function M.setup(opts)
-    require("ask-openai.new") -- source new funcs TODO how do I want this to work?
+    require("ask-openai.copilot").setup()
+
+    require("ask-openai.suggest") -- source new funcs TODO how do I want this to work?
 
     require("ask-openai.config").set_user_opts(opts)
 
-    require("ask-openai.copilot").setup()
-    local token = require("ask-openai.copilot").token
 
     -- TODO modify this to use github copilot subscription/api using chat model and see how it performs vs openai gpt4o (FYI windows terminal chat w/ copilot was clearly inferior vs gpt4o but ... win term chat might have been using gpt3.5 or smth else, just FYI"
 
