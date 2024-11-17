d05b7edcde2ed99543367c49a0ccb859db88075f
Wes Higbee <wes.mcclure@gmail.com>
2024-11-17 01:56:05 -0600

    notes

--- lua/ask-openai/init.lua
+++ lua/ask-openai/init.lua
@@ -11,7 +11,7 @@ end
 
 M.ask_openai = function()
     local cmdline = vim.fn.getcmdline()
-    print("asking...") -- overwrites showing luaeval("ask_openai()") in cmdline, this looks better, could also do print("AskOpenAI()") if I want to go back to the previous style
+    print("asking...") -- overwrites showing luaeval("...") in cmdline 
 
     local stdin_text = ' env: nvim (neovim) command mode (return a valid command w/o the leading : ) \n question: ' ..
         cmdline
@@ -24,10 +24,9 @@ function M.setup(opts)
     require("ask-openai.config").set_user_opts(opts)
     require("ask-openai.suggest")
 
-    -- [e]valuate expression luaeval("ask_openai()") in command-line mode
+    -- [e]valuate vimscript expression luaeval("...") which runs nested lua code
     -- DO NOT SET silent=true, messes up putting result into cmdline, also I wanna see print messages, IIUC that would be affected
     -- FYI `<C-\>e` is critical in the following, don't remove the `e` and `\\` is to escape the `\` in lua
-    -- vim.api.nvim_set_keymap('c', '<C-b>', '<C-\\>eluaeval("ask_openai()")<CR>', { noremap = true, })
     vim.api.nvim_set_keymap('c', '<C-b>', '<C-\\>eluaeval("require(\'ask-openai\').ask_openai()")<CR>', { noremap = true, })
 end
 
