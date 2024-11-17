0d996d087b6f81e122187ff7b6044ad5b6818785
Wes Higbee <wes.mcclure@gmail.com>
2024-11-17 01:45:36 -0600

    this works too, hrm lets use it

--- lua/ask-openai/init.lua
+++ lua/ask-openai/init.lua
@@ -17,7 +17,7 @@ function M.setup(opts)
 
     function ask_openai()
         local cmdline = vim.fn.getcmdline()
-        print("asking...") -- overwrites AskOpenAI() in cmdline (I kinda like showing AskOpenAI() though so people know what is bound to that key?
+        print("asking...") -- overwrites showing luaeval("ask_openai()") in cmdline, this looks better, could also do print("AskOpenAI()") if I want to go back to the previous style
 
         local stdin_text = ' env: nvim (neovim) command mode (return a valid command w/o the leading : ) \n question: ' ..
             cmdline
@@ -27,19 +27,9 @@ function M.setup(opts)
         return trim_null_characters(result)
     end
 
-    vim.cmd [[
-        function! AskOpenAI()
-            " just a wrapper so the CLI shows "AskOpenAI" instead of "luaeval('ask_openai()')"
-            "    wait... I can use print() in lua to show a diff message after this! it will show last line printed
-            return luaeval('ask_openai()')
-            " also, FYI, luaeval and the nature of a cmap modifying cmdline means there really is no way to present errors short of putting them into the command line, which is fine and how I do it in my CLI equivalents of this
-            "   only issue w/ putting into cmdline is no UNDO AFAIK for going back to what you had typed... unlike how I can do that in fish shell and just ctrl+z to undo the error message, but error messages are gonna be pretty rare so NBD for now
-        endfunction
-    ]]
-
-    -- [e]valuate expression AskOpenAI() in command-line mode
-    -- DO NOT SET silent=true, messes up putting result into cmdline
-    vim.api.nvim_set_keymap('c', '<C-b>', '<C-\\>eAskOpenAI()<CR>', { noremap = true, })
+    -- [e]valuate expression luaeval("ask_openai()") in command-line mode
+    -- DO NOT SET silent=true, messes up putting result into cmdline, also I wanna see print messages, IIUC that would be affected
+    vim.api.nvim_set_keymap('c', '<C-b>', '<C-\\>eluaeval("ask_openai()")<CR>', { noremap = true, })
 end
 
 return M
