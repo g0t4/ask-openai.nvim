799e81240edbfb103096f7245747e3843aef4e06
Wes Higbee <wes.mcclure@gmail.com>
2024-11-19 02:46:04 -0600

    notes

--- lua/ask-openai/providers/byok.lua
+++ lua/ask-openai/providers/byok.lua
@@ -4,4 +4,5 @@ local function create_provider_for_func(get_bearer_token)
     }
 end
 
+-- FYI yes this looks silly but wrapping the func in a module (like other providers) means some providers can have more behavior than the one function (i.e. copilot)
 return create_provider_for_func
