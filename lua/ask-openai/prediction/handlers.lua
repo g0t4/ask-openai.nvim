4f364472f1b01b8c88c8f609b991b2b28aea7f62
Wes Higbee <wes.mcclure@gmail.com>
2025-01-25 01:15:40 -0600

    test wrapping a streaming command into pipes for STDIO

--- lua/ask-openai/prediction/handlers.lua
+++ lua/ask-openai/prediction/handlers.lua
@@ -1,23 +1,58 @@
+local uv = vim.uv
 local M = {}
 
+print("starting")
+local stdout = uv.new_pipe(false)
+local stderr = uv.new_pipe(false)
+local command = "fish"
+local args = {
+    "-c",
+    "for i in (seq 1 10); echo $i; sleep 1; end"
+    -- "echo foo && sleep 2 && echo bar",
+}
+
+local handle, pid = uv.spawn(command, {
+    args = args,
+    stdio = { nil, stdout, stderr },
+}, function(code, signal)
+    print("Exit code:", code, "Signal:", signal)
+    stdout:close()
+    stderr:close()
+end)
+print("handle", handle, pid)
+
+uv.read_start(stdout, function(err, data)
+    assert(not err, err)
+    if data then
+        print("STDOUT:", data)
+    end
+end)
+
+uv.read_start(stderr, function(err, data)
+    assert(not err, err)
+    if data then
+        print("STDERR:", data)
+    end
+end)
+
 function M.ask_for_prediction()
-    print("Asking for prediction...")
+    -- print("Asking for prediction...")
 end
 
 function M.reject()
-    print("Rejecting prediction...")
+    -- print("Rejecting prediction...")
 end
 
 function M.accept_all()
-    print("Accepting all predictions...")
+    -- print("Accepting all predictions...")
 end
 
 function M.accept_line()
-    print("Accepting line prediction...")
+    -- print("Accepting line prediction...")
 end
 
 function M.accept_word()
-    print("Accepting word prediction...")
+    -- print("Accepting word prediction...")
 end
 
 return M
