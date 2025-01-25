095e721ce69bbdc192387d2c1079f05605c74473
Wes Higbee <wes.mcclure@gmail.com>
2025-01-25 01:22:08 -0600

    on trigger

--- lua/ask-openai/prediction/handlers.lua
+++ lua/ask-openai/prediction/handlers.lua
@@ -1,42 +1,41 @@
 local uv = vim.uv
 local M = {}
 
-print("starting")
-local stdout = uv.new_pipe(false)
-local stderr = uv.new_pipe(false)
-local command = "fish"
-local args = {
-    "-c",
-    "for i in (seq 1 10); echo $i; sleep 1; end"
-    -- "echo foo && sleep 2 && echo bar",
-}
-
-local handle, pid = uv.spawn(command, {
-    args = args,
-    stdio = { nil, stdout, stderr },
-}, function(code, signal)
-    print("Exit code:", code, "Signal:", signal)
-    stdout:close()
-    stderr:close()
-end)
-print("handle", handle, pid)
-
-uv.read_start(stdout, function(err, data)
-    assert(not err, err)
-    if data then
-        print("STDOUT:", data)
-    end
-end)
-
-uv.read_start(stderr, function(err, data)
-    assert(not err, err)
-    if data then
-        print("STDERR:", data)
-    end
-end)
-
 function M.ask_for_prediction()
     -- print("Asking for prediction...")
+
+    local stdout = uv.new_pipe(false)
+    local stderr = uv.new_pipe(false)
+    local command = "fish"
+    local args = {
+        "-c",
+        "for i in (seq 1 10); echo $i; sleep 1; end"
+        -- "echo foo && sleep 2 && echo bar",
+    }
+
+    local handle, pid = uv.spawn(command, {
+        args = args,
+        stdio = { nil, stdout, stderr },
+    }, function(code, signal)
+        print("Exit code:", code, "Signal:", signal)
+        stdout:close()
+        stderr:close()
+    end)
+    print("handle", handle, pid)
+
+    uv.read_start(stdout, function(err, data)
+        assert(not err, err)
+        if data then
+            print("STDOUT:", data)
+        end
+    end)
+
+    uv.read_start(stderr, function(err, data)
+        assert(not err, err)
+        if data then
+            print("STDERR:", data)
+        end
+    end)
 end
 
 function M.reject()
