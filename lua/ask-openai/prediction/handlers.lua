40070d9663864d242d1de38b7ba3dbde2df53aaa
Wes Higbee <wes.mcclure@gmail.com>
2025-02-03 00:11:50 -0600

    wow 14b thanks to streaming is working well enough

--- lua/ask-openai/prediction/handlers.lua
+++ lua/ask-openai/prediction/handlers.lua
@@ -113,8 +113,10 @@ function M.ask_for_prediction()
         --    for FIM, you won't likely use /chat/completions (two different tasks and models are trained on FIM alone, not w/ chat messages in prompt)
         --
         -- model = "qwen2.5-coder:7b", --0.5b, 1b, 3b*, 7b, 14b*, 32b
-        model = "qwen2.5-coder:7b-instruct-q8_0",
+        -- model = "qwen2.5-coder:7b-instruct-q8_0",
+        model = "qwen2.5-coder:14b-instruct-q8_0", -- works well if I can make sure nothing else is using up GPU space
         -- TODO try deepseek-coder-v2 (MOE 16b model)
+        --
         -- model = "codellama:7b-code-q4_K_M", -- FYI only -code models have PSM in template? or is that a mistake in some of the -instruct models... I thought instruct had infill?
         -- btw => codellama:-code uses: <PRE> -- calculator\nlocal M = {}\n\nfunction M.add(a, b)\n    return a + b\nend1 <SUF>1\n\n\n\nreturn M <MID>
         --      Admittedly it is nice to switch models and have the template handle the FIM token differences...
