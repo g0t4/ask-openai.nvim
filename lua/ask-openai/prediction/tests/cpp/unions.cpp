07db1b0946d2f3cd27657dd63a928183bdfc73a4
Wes Higbee <wes.mcclure@gmail.com>
2025-02-02 23:46:54 -0600

    unions ex

---
+++ lua/ask-openai/prediction/tests/cpp/unions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+// demo of union in cpp
+// define a union
+union Data {
+    int i;
+    float f;
+    char str[20];
+};
+int main() {
+    Data data;
+    data.i = 10;
+    cout << "data.i : " << data.i << endl;
+    cout << " data.f : " << data.f << endl;
+    // the value of i above will be overwritten by f below
+    data.f = 220.5;
+    cout << " data.i : " << data.i << endl;
+    cout << " data.f : " << data.f << endl;
+    strcpy(data.str, "C Programming");
+    cout << " data.str : " << data.str << endl;
+    cout << " data.i : " << data.i << endl;
+    cout << " data.f : " << data.f << endl;
+
+
+
+    return 0;
+}
