ed5fe86a9b99dd0316af1cb70716cc39d1c50fe1
Wes Higbee <wes.mcclure@gmail.com>
2025-02-05 20:34:14 -0600

    looking into changes

---
+++ lua/ask-openai/prediction/edits/changes.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+function M.get_change_list_with_lines()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local result = vim.fn.getchangelist(bufnr) -- Retrieve change list
+    local changelist = result[1] -- first is change list
+    -- local position_in_list = result[2]
+    -- todo don't take items after position in list?
+    -- print("position: ", position_in_list)
+
+    local changes = {}
+    for _, change in ipairs(changelist) do
+        local lnum = change.lnum
+        if lnum ~= nil and changes[lnum] == nil then
+            -- ensure only one inclusion of a given line, or allow dups?
+            local line = vim.api.nvim_buf_get_lines(bufnr, lnum - 1, lnum, false)[1] or ""
+            changes[lnum] = { lnum = lnum, col = change.col, line = line }
+        end
+    end
+    return changes
+end
+
+function M.print_changes()
+    local changes = M.get_change_list_with_lines()
+
+    for _, change in pairs(changes) do
+        print(string.format("Line %d, Column %d: %s", change.lnum, change.col, change.line))
+    end
+end
+
+return M
