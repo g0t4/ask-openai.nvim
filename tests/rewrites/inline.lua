d1d9dd2c387f361373eff4483e7b3fb095d0f0f1
Wes Higbee <wes.mcclure@gmail.com>
2025-03-22 00:06:02 -0500

    check for ``` and `

--- tests/rewrites/inline.lua
+++ tests/rewrites/inline.lua
@@ -10,11 +10,14 @@ describe("test model responses", function()
         local user_prompt = "Finish this function"
         local response = rewrites.send_to_ollama(user_prompt, code, filename)
 
-        -- printit
         print(response)
 
-        -- run the response with python and assert result is correct for 2 + 3
-        -- cc
-        assert.is_equal("def add(x, y):\n  return x + y", response)
+        -- just use assert to compare and if match then no need to look further
+        --   else, can visually compare
+        --   mostly looking for no ``` and ` in response
+        assert.is_nil(string.match(response, '```'), 'response has ```')
+        assert.is_nil(string.match(response, '`'), 'response has backtick')
+
+        assert.is_equal("def add(x, y):\n    return x + y", response)
     end)
 end)
