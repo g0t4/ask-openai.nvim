937d6699d38c7a530ee1440fd60d529db4282ebd
Wes Higbee <wes.mcclure@gmail.com>
2025-03-26 12:40:35 -0500

    AskRewrite did this :)

--- lua/ask-openai/questions/ask.lua
+++ lua/ask-openai/questions/ask.lua
@@ -67,9 +67,27 @@ local function ask_question(opts)
     local user_prompt = opts.args
     local response = M.send_question(user_prompt)
     -- TODO request markdown as response format... and highlight that as markdown in a buffer
-    print(response)
-end
 
+    -- Create a popup window
+    local win_width = 80
+    local win_height = 10
+    local row = vim.api.nvim_get_option('lines') / 2 - win_height / 2
+    local col = vim.api.nvim_get_option('columns') / 2 - win_width / 2
+
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_name(bufnr, 'Question Response')
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { response })
+
+    local winid = vim.api.nvim_open_win(bufnr, true, {
+        relative = 'editor',
+        width = win_width,
+        height = win_height,
+        row = row,
+        col = col,
+        style = 'minimal',
+        border = 'single'
+    })
+end
 function M.setup()
     -- once again, pass question in command line for now... b/c then I can use cmd history to ask again or modify question easily
     --  if I move to a float window, I'll want to add history there then which I can handle later when this falls apart
