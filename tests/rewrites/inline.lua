510595dcbe142f1db988b661f099aad03406e52b
Wes Higbee <wes.mcclure@gmail.com>
2025-03-25 23:48:53 -0500

    new test for not removing comments

--- tests/rewrites/inline.lua
+++ tests/rewrites/inline.lua
@@ -1,4 +1,4 @@
-local rewrites = require("ask-openai.rewrites.inline")
+local rewrites = require("lua.ask-openai.rewrites.inline")
 local assert = require("luassert")
 
 describe("test strip markdown from completion responses", function()
