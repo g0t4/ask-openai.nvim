37f8cabbf5564aaa8d4e0fb94de915b18eefc9f9
Wes Higbee <wes.mcclure@gmail.com>
2025-03-26 14:57:13 -0500

    review

--- lua/ask-openai/questions/backends/chat_completions.lua
+++ lua/ask-openai/questions/backends/chat_completions.lua
@@ -45,17 +45,19 @@ function M.process_sse(data)
         -- {"id":"chatcmpl-209","object":"chat.completion.chunk","created":1743021818,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":""},"finish_reason":"stop"}]}
         if success and parsed and parsed.choices and parsed.choices[1] then
             local choice = parsed.choices[1]
-            if choice.finish_reason then --  no finish reason? not sure if ever happens... keep going...
-                local finish_reason = choice.finish_reason
+            local finish_reason = choice.finish_reason
+            if finish_reason ~= nil and finish_reason ~= vim.NIL then
                 done = true
-                if finish_reason ~= "stop" then
+                if finish_reason ~= "stop" and finish_reason ~= "length" then
                     log:warn("WARN - unexpected /v1/chat/completions finish_reason: ", finish_reason, " do you need to handle this too?")
-                    -- ok for now to continue too
                 end
+            end
+            if choice.delta == nil or choice.delta.content == nil then
+                log:warn("WARN - unexpected, no delta.content in completion choice, do you need to add special logic to handle this?")
             end
             chunk = (chunk or "") .. choice.delta.content
         else
-            log:warn("SSENEW json parse failed for ss_event: ", ss_event)
+            log:warn("SSE json parse failed for ss_event: ", ss_event)
         end
     end
     return chunk, done
