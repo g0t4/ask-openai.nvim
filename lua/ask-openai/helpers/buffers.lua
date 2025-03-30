0581d5386cabd99c5cf8a46b71d98495b23afc59
Wes Higbee <wes.mcclure@gmail.com>
2025-03-30 13:45:53 -0500

    rename

--- lua/ask-openai/helpers/buffers.lua
+++ lua/ask-openai/helpers/buffers.lua
@@ -6,7 +6,7 @@ function M.get_visual_selection()
     --   getcharpos also resolves the issue with v:maxcol as the returned col number (i.e. in visual line mode selection)
     local _, start_line, start_col, _ = unpack(vim.fn.getcharpos("'<"))
     local _, end_line, end_col, _ = unpack(vim.fn.getcharpos("'>"))
-    local lines = vim.fn.getline(start_line, end_line)
+    local selected_lines = vim.fn.getline(start_line, end_line)
 
     log:info("GETCHARPOS start(line=" .. start_line .. ",col=" .. start_col
         .. ") end(line=" .. end_line .. ",col=" .. end_col .. ")")
@@ -16,12 +16,12 @@ function M.get_visual_selection()
     -- - empty line selected by shift+V j    -- has end_line > start_line
     -- FYI these tests are working in my initial testing
 
-    if #lines == 0 then return "" end
+    if #selected_lines == 0 then return "" end
 
-    lines[#lines] = string.sub(lines[#lines], 1, end_col)
-    lines[1] = string.sub(lines[1], start_col)
+    selected_lines[#selected_lines] = string.sub(selected_lines[#selected_lines], 1, end_col)
+    selected_lines[1] = string.sub(selected_lines[1], start_col)
 
-    return vim.fn.join(lines, "\n"), start_line, start_col, end_line, end_col
+    return vim.fn.join(selected_lines, "\n"), start_line, start_col, end_line, end_col
 end
 
 return M
