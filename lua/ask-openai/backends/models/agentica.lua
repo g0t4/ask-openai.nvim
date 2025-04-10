5a07e95e7d1dfe89d99f875d0e51655f73a31263
Wes Higbee <wes.mcclure@gmail.com>
2025-04-10 00:55:55 -0500

    split out

---
+++ lua/ask-openai/backends/models/agentica.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+-- TODO for agentic... and all reasoning models, I need to split apart the <think> chunk when its done and display spearately, right?
+--    make this a step before calling this method or a separate aspect so it can be reused in rewrites and other usage (i.e. agent tool eventually)
+-- i.e. agentica's https://huggingface.co/agentica-org/DeepCoder-14B-Preview#usage-recommendations
+M.DeepCoder = {
+    messages = {
+        -- TODO if agentica recommends no system prompt.. would it make more sense to just use legacy completions for that use case oai_completions?
+        { role = "user", content = system_prompt .. "\n" .. user_message },
+    },
+    -- Avoid adding a system prompt; all instructions should be contained within the user prompt.
+    model = "agentica-org/DeepCoder-1.5B-Preview",
+    -- TODO 14B-Preview quantized variant
+    temperature = 0.6,
+    top_p = 0.95,
+    -- max_tokens set to at least 64000
+    max_tokens = 64000,
+    -- TODO can I just not set max_tokens too?
+}
+
+return M
--- lua/ask-openai/questions/ask.lua
+++ lua/ask-openai/questions/ask.lua
@@ -14,25 +14,6 @@ function M.send_question(user_prompt, code, file_name)
             .. ":\n\n" .. code
     end
 
-    -- TODO USE model specific params passed and use for body (merge)
-    --    make this a step before calling this method or a separate aspect so it can be reused in rewrites and other usage (i.e. agent tool eventually)
-    -- i.e. agentica's https://huggingface.co/agentica-org/DeepCoder-14B-Preview#usage-recommendations
-    local agentica_params = {
-        -- TODO for agentic... and all reasoning models, I need to split apart the <think> chunk when its done and display spearately, right?
-        messages = {
-            -- TODO if agentica recommends no system prompt.. would it make more sense to just use legacy completions for that use case oai_completions?
-            { role = "user", content = system_prompt .. "\n" .. user_message },
-        },
-        -- Avoid adding a system prompt; all instructions should be contained within the user prompt.
-        model = "agentica-org/DeepCoder-1.5B-Preview",
-        -- TODO 14B-Preview quantized variant
-        temperature = 0.6,
-        top_p = 0.95,
-        -- max_tokens set to at least 64000
-        max_tokens = 64000,
-        -- TODO can I just not set max_tokens too?
-    }
-
     local ollama_qwen_params = {
         messages = {
             { role = "system", content = system_prompt },
