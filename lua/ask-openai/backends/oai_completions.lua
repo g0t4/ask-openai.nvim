22d2c56bfe71b0d573fba39d237873768142a7bf
Wes Higbee <wes.mcclure@gmail.com>
2025-04-10 02:01:31 -0500

    wip

--- lua/ask-openai/backends/oai_completions.lua
+++ lua/ask-openai/backends/oai_completions.lua
@@ -12,97 +12,108 @@ local M = {}
 local log = require("ask-openai.prediction.logger").predictions()
 _G.PLAIN_FIND = true
 
-local function body_for(prefix, suffix, _recent_edits)
+function M.terminate(request)
+    -- TODO move this onto a request class?
+    --  and if its on request, the request can be marked w/ a status instead of nil for values
+    -- PRN add interface to frontend to be notified when a request is aborted or its status changes in general
+    if request == nil or request.handle == nil then
+        return
+    end
 
-    -- IDEA - pass param sets in to make model specific alterations
-    local agentica_params = {
-        -- https://huggingface.co/agentica-org/DeepCoder-14B-Preview#usage-recommendations
+    local handle = request.handle
+    local pid = request.pid
+    request.handle = nil
+    request.pid = nil
+    if handle ~= nil and not handle:is_closing() then
+        log:trace("Terminating process, pid: ", pid)
 
-
-    }
-
-
-
-    local body = {
-        -- FYI this is for vllm/openai/ollama "legacy" /completions endpoint
-        -- /completions https://docs.vllm.ai/en/stable/serving/openai_compatible_server.html#completions-api
-        -- TODO! rewrite the following file fully to use /v1/completions
-        --   FYI! this was a backend impl from predictions for vllm so I might have to change alot here
-        --     at first I planned to just make this a vllm backend for instruct models but I wanna generalize to the backend endpoint instead
-
-
-        -- agentica-org models
-        -- fine tune of deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
-        model = "agentica-org/DeepCoder-1.5B-Preview", -- reminder as vllm serve dictates the model
-        -- https://huggingface.co/mradermacher/DeepCoder-1.5B-Preview-GGUF - quantizeds
-
-        -- model = "Qwen/Qwen2.5-Coder-7B-Instruct",
-        --
-        -- quantized variants:
-        -- model = "Qwen/Qwen2.5-Coder-32B-Instruct-AWQ", -- ~20GB (4GBx5)
-        -- model = "Qwen/Qwen2.5-Coder-32B-Instruct-GPTQ-Int8", -- won't fit for me
-        -- model = "Qwen/Qwen2.5-Coder-32B-Instruct-GPTQ-Int4", -- SHOULD WORK!
-
-        stream = true,
-
-        max_tokens = 400,
-
-        -- TODO temperature, top_p,
-
-        -- options = {
-        --     -- stop_token_ids: Optional[list[int]] = Field(default_factory=list)  -- vllm
-        --     -- any params for parallelization like I had w/ ollama/
-        --     --   num_ctx = 8192, -- ollama
-        -- }
-    }
-
-    -- TODO do any of these work in chat completions API? IIRC <|im_end|> might, I saw it with tool use instructions, IIRC
-    local sentinel_tokens = {
-        -- qwen2.5-coder:
-        fim_prefix = "<|fim_prefix|>",
-        fim_middle = "<|fim_middle|>",
-        fim_suffix = "<|fim_suffix|>",
-        -- fim_pad = "<|fim_pad|>",
-        repo_name = "<|repo_name|>",
-        file_sep = "<|file_sep|>",
-        im_start = "<|im_start|>",
-        im_end = "<|im_end|>",
-        -- todo others?
-        -- endoftext = "<|endoftext|>"
-    }
-
-    -- TODO MESSAGES
-    -- body.messages = ?
-
-
-    local body_json = vim.json.encode(body)
-
-    log:trace("body", body_json)
-
-    return body_json
+        handle:kill("sigterm")
+        handle:close()
+        -- FYI ollama should show that connection closed/aborted
+    end
 end
 
+function M.curl_for(body, base_url, frontend)
+    local request = {}
 
-function M.build_request(prefix, suffix, recent_edits)
+    body.stream = true
+    local json = vim.fn.json_encode(body)
+
+    -- TODO look for "curl" and "--no-buffer" to find all spots to merge together into this final backend
     local options = {
         command = "curl",
         args = {
             "-fsSL",
-            "--no-buffer", -- curl seems to be the culprit... w/o this it batches (test w/ `curl *` vs `curl * | cat` and you will see difference)
+            "--no-buffer", -- w/o this curl batches (test w/ `curl *` vs `curl * | cat` and you will see difference)
             "-X", "POST",
-            "http://ollama:8000/v1/completions", -- TODO pass in api base_url (via config)
+            base_url .. "/v1/chat/completions",
             "-H", "Content-Type: application/json",
-            "-d", body_for(prefix, suffix, recent_edits)
+            "-d", json
         },
     }
-    return options
+
+    local stdout = uv.new_pipe(false)
+    local stderr = uv.new_pipe(false)
+
+    options.on_exit = function(code, signal)
+        if code ~= 0 then
+            log:error("spawn - non-zero exit code:", code, "Signal:", signal)
+        end
+        stdout:close()
+        stderr:close()
+
+        -- this shoudl be attacked to a specific request (not any module)
+        -- clear out refs
+        request.handle = nil
+        request.pid = nil
+    end
+
+    -- TODO fix... by having the frontend do this, it is not the backends job to track which requests to abort
+    --   this was when I only envisioned one request at a time but now with agents I could run them in parallel and why not with parallel capacity just sitting there on my GPUs
+    M.terminate()
+
+    request.handle, request.pid = uv.spawn(options.command, {
+        args = options.args,
+        stdio = { nil, stdout, stderr },
+    }, options.on_exit)
+
+    options.on_stdout = function(err, data)
+        -- log:trace("on_stdout chunk: ", data)
+        if err then
+            log:warn("on_stdout error: ", err)
+            return
+        end
+        if data then
+            vim.schedule(function()
+                local chunk, generation_done, done_reason = M.sse_to_chunk(data)
+                if chunk then
+                    frontend.process_chunk(chunk)
+                end
+                -- PRN anything on done?
+                -- if generation_done then
+                --     PRN add for empty response checking like with predictions (need to capture all chunks to determine this and its gonna be basically impossible to have the response be valid and empty, so not a priority)
+                --     this_prediction:mark_generation_finished()
+                -- end
+            end)
+        end
+    end
+    uv.read_start(stdout, options.on_stdout)
+
+    options.on_stderr = function(err, data)
+        log:warn("on_stderr chunk: ", data)
+        if err then
+            log:warn("on_stderr error: ", err)
+        end
+        -- TODO frontend.handle_error()?
+    end
+    uv.read_start(stderr, options.on_stderr)
+
+    return request
 end
 
-function M.process_sse(data)
+function M.sse_to_chunk(data)
     -- SSE = Server-Sent Event
     -- split on lines first (each SSE can have 0+ "event" - one per line)
-
-    -- TODO probably need to bring over legacy-completions.lua AS it might be closer to openai compat responses from vllm...
 
     -- FYI use nil to indicate nothing in the SSE... vs empty line which is a valid thingy right?
     local chunk = nil -- combine all chunks into one string and check for done
