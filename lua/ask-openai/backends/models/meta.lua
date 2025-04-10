16032f09ca586f9a2ab6bb28e0dce4a73fbe983e
Wes Higbee <wes.mcclure@gmail.com>
2025-04-10 00:27:10 -0500

    organize

--- lua/ask-openai/backends/models/meta.lua
+++ lua/ask-openai/backends/models/meta.lua
@@ -1,7 +1,13 @@
--- codellama template:
---    {{- if .Suffix }}<PRE> {{ .Prompt }} <SUF>{{ .Suffix }} <MID>
-sentinel_tokens = {
-    fim_prefix = "<PRE> ",
-    fim_suffix = " <SUF>",
-    fim_middle = " <MID>",
+local M = {}
+
+M.codellama = {
+    -- codellama template:
+    --    {{- if .Suffix }}<PRE> {{ .Prompt }} <SUF>{{ .Suffix }} <MID>
+    sentinel_tokens = {
+        fim_prefix = "<PRE> ",
+        fim_suffix = " <SUF>",
+        fim_middle = " <MID>",
+    },
 }
+
+return M
--- lua/ask-openai/backends/models/qwen.lua
+++ lua/ask-openai/backends/models/qwen.lua
@@ -1,13 +1,17 @@
-local sentinel_tokens = {
-    -- qwen2.5-coder:
-    fim_prefix = "<|fim_prefix|>",
-    fim_middle = "<|fim_middle|>",
-    fim_suffix = "<|fim_suffix|>",
-    -- fim_pad = "<|fim_pad|>",
-    repo_name = "<|repo_name|>",
-    file_sep = "<|file_sep|>",
-    im_start = "<|im_start|>",
-    im_end = "<|im_end|>",
-    -- todo others?
-    -- endoftext = "<|endoftext|>"
+local M = {}
+
+M.qwen25coder = {
+    sentinel_tokens = {
+        fim_prefix = "<|fim_prefix|>",
+        fim_middle = "<|fim_middle|>",
+        fim_suffix = "<|fim_suffix|>",
+        -- fim_pad = "<|fim_pad|>",
+        repo_name = "<|repo_name|>",
+        file_sep = "<|file_sep|>",
+        im_start = "<|im_start|>",
+        im_end = "<|im_end|>",
+        -- endoftext = "<|endoftext|>"
+    },
 }
+
+return M
