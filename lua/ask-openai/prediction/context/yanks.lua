8614ceb582afcc242d12510610d87c6bf1e1d2b7
Wes Higbee <wes.mcclure@gmail.com>
2025-04-10 19:16:49 -0500

    yanks

--- lua/ask-openai/prediction/context/yanks.lua
+++ lua/ask-openai/prediction/context/yanks.lua
@@ -3,8 +3,48 @@
 
 local M = {}
 
+function M.dump_yank_event()
+    print("yanked: " .. vim.inspect(vim.v.event))
+    -- yanked: {
+    --   inclusive = false,
+    --   operator = "y",
+    --   regcontents = { '    print("yanked: " .. vim.inspect(vim.v.event))' },
+    --   regname = "",
+    --   regtype = "V",
+    --   visual = false
+    -- }
+end
+
+M.yanks = {}
+function M.on_yank()
+    table.insert(M.yanks, vim.v.event.regcontents)
+end
 
 function M.setup()
+    vim.api.nvim_create_augroup('ContextYank', {})
+
+    vim.api.nvim_create_autocmd("TextYankPost", {
+        pattern = '*',
+        callback = M.dump_yank_event,
+        group = 'ContextYank',
+        desc = 'Prediction context yanks'
+    })
 end
 
 return M
+
+-- :h TextYankPost
+-- Just after a |yank| or |deleting| command,
+-- but not if the black hole register |quote_| is used nor for |setreg()|.
+-- Pattern must be "*".
+-- Sets these |v:event| keys:
+--     inclusive
+--     operator
+--     regcontents
+--     regname
+--     regtype
+--     visual
+-- The `inclusive` flag combined with the |'[| and |']| marks
+-- can be used to calculate the precise region of the operation.
+--
+-- Non-recursive (event cannot trigger itself).
