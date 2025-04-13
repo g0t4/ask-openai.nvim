a986bb621865752f8b256347d60be21727095ead
Wes Higbee <wes.mcclure@gmail.com>
2025-04-13 00:56:41 -0500

    test SSE tool parsing

--- lua/ask-openai/tools/tests/sse.lua
+++ lua/ask-openai/tools/tests/sse.lua
@@ -1,12 +1,39 @@
 local curls = require("ask-openai.backends.curl_streaming")
+local oai_chat = require("ask-openai.backends.oai_chat")
 local assert = require("luassert")
 
-describe("tool use SSE parsing", function()
-    it("", function()
-        -- assert.are.equal("foo", response)
+-- PRN move this to backends dir and consolidate all tests there?
+
+describe("tool use SSE parsing in /v1/chat/completions", function()
+    it("parses ollama tool_calls - all in one SSE", function()
+        -- *** escape SSE log outputs: \" => \\"    (only backslashes, not " b/c you are putting this inside a ' single quote)
+        local sse =
+        'data: {"id":"chatcmpl-304","object":"chat.completion.chunk","created":1744521962,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":"","tool_calls":[{"id":"call_lbcjwr0u","index":0,"type":"function","function":{"name":"run_command","arguments":"{\\"command\\":\\"ls -a\\"}"}}]},"finish_reason":null}]}'
+        -- "choices":[
+        --   {"index":0,"delta":
+        --     {"role":"assistant","content":"","tool_calls":
+        --       [{
+        --         "id":"call_lbcjwr0u","index":0,"type":"function",
+        --         "function": {
+        --           "name":"run_command",
+        --           "arguments":"{\"command\":\"ls -a\"}"
+        --         }
+        --       }]
+        --     },
+        --     "finish_reason":null
+        --   }]
+        local chunk, _, tool_calls = curls.sse_to_chunk(sse, oai_chat.parse_choice)
+        assert.are.equal(#tool_calls, 1)
+        local tool = tool_calls[1]
+        assert.are.equal(tool.id, "call_lbcjwr0u")
+        func = tool["function"]
+        assert.are.equal(type(func), "table")
+        assert.are.equal(func.name, "run_command")
+        assert.are.equal(func.arguments, '{"command":"ls -a"}')
     end)
-    it("", function()
-    end)
-    it("", function()
-    end)
+    -- it("", function()
+    --     -- data: {"id":"chatcmpl-304","object":"chat.completion.chunk","created":1744521962,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":""},"finish_reason":"tool_calls"}]}
+    -- end)
+    -- it("", function()
+    -- end)
 end)
