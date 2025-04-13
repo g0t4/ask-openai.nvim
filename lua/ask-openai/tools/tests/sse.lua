3d9d9ef2b74a13d1f42b4607ad2df19c1a5671ed
Wes Higbee <wes.mcclure@gmail.com>
2025-04-13 01:09:30 -0500

    start my own assertions that will be strongly typed

--- lua/ask-openai/tools/tests/sse.lua
+++ lua/ask-openai/tools/tests/sse.lua
@@ -36,12 +36,17 @@ describe("tool use SSE parsing in /v1/chat/completions", function()
         -- TODO leave arguments as serialized json as it'll be passed as is to MCP (IIRC)
     end)
 
+    -- TODO do I like this style of assertions? if so add more
+    local function should_be_nil(t)
+        assert.is_nil(t)
+    end
+
     it("parses ollama finish_reason", function()
         local data =
         'data: {"id":"chatcmpl-304","object":"chat.completion.chunk","created":1744521962,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":""},"finish_reason":"tool_calls"}]}'
         local _, finish_reason, tool_calls = curls.sse_to_chunk(data, oai_chat.parse_choice)
         assert.are.equal(finish_reason, "tool_calls")
-        assert.is_nil(tool_calls)
+        should_be_nil(finish_reason)
     end)
 
     -- it("", function()
