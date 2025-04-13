f3880116caad1be18c0f5a526510ebe192c0aa75
Wes Higbee <wes.mcclure@gmail.com>
2025-04-13 01:06:15 -0500

    testing

--- lua/ask-openai/tools/tests/sse.lua
+++ lua/ask-openai/tools/tests/sse.lua
@@ -6,9 +6,9 @@ local assert = require("luassert")
 -- ***! use <leader>u to run tests in this file! (habituate that, don't type out the cmd yourself)
 
 describe("tool use SSE parsing in /v1/chat/completions", function()
-    it("parses ollama tool_calls - all in one SSE", function()
+    it("parses ollama tool_calls", function()
         -- *** escape SSE log outputs: \" => \\"    (only backslashes, not " b/c you are putting this inside a ' single quote)
-        local sse =
+        local data =
         'data: {"id":"chatcmpl-304","object":"chat.completion.chunk","created":1744521962,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":"","tool_calls":[{"id":"call_lbcjwr0u","index":0,"type":"function","function":{"name":"run_command","arguments":"{\\"command\\":\\"ls -a\\"}"}}]},"finish_reason":null}]}'
         -- "choices":[
         --   {"index":0,"delta":
@@ -23,7 +23,7 @@ describe("tool use SSE parsing in /v1/chat/completions", function()
         --     },
         --     "finish_reason":null
         --   }]
-        local chunk, _, tool_calls = curls.sse_to_chunk(sse, oai_chat.parse_choice)
+        local _, _, tool_calls = curls.sse_to_chunk(data, oai_chat.parse_choice)
         assert.are.equal(#tool_calls, 1)
         local tool = tool_calls[1]
         assert.are.equal(tool.id, "call_lbcjwr0u")
@@ -33,10 +33,17 @@ describe("tool use SSE parsing in /v1/chat/completions", function()
         assert.are.equal(type(func), "table")
         assert.are.equal(func.name, "run_command")
         assert.are.equal(func.arguments, '{"command":"ls -a"}')
+        -- TODO leave arguments as serialized json as it'll be passed as is to MCP (IIRC)
     end)
-    -- it("", function()
-    --     -- data: {"id":"chatcmpl-304","object":"chat.completion.chunk","created":1744521962,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":""},"finish_reason":"tool_calls"}]}
-    -- end)
+
+    it("parses ollama finish_reason", function()
+        local data =
+        'data: {"id":"chatcmpl-304","object":"chat.completion.chunk","created":1744521962,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":""},"finish_reason":"tool_calls"}]}'
+        local _, finish_reason, tool_calls = curls.sse_to_chunk(data, oai_chat.parse_choice)
+        assert.are.equal(finish_reason, "tool_calls")
+        assert.are.equal(tool_calls, nil)
+    end)
+
     -- it("", function()
     -- end)
 end)
