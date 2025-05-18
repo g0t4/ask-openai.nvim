711286f5d414c92a8cd9d01f1b1175ce95263260
Wes Higbee <wes.mcclure@gmail.com>
2025-05-18 18:29:36 -0500

    clarify more of details I didn't give Qwen3
    I love that it made me think about some initial test cases!
    - So my first stab it it was giving it feedback.
    - Then I added to the initial set... and its often this initial set that are the easiest.
    - So why not save my mental energey for the difficult ones!?

--- lua/ask-openai/rewrites/inline.tests.lua
+++ lua/ask-openai/rewrites/inline.tests.lua
@@ -9,14 +9,16 @@ describe("test strip markdown from completion responses", function()
         local completion = "```\nfoo\n```"
         local lines = vim.split(completion, "\n")
         local response = rewrites.strip_md_from_completion(lines)
-        assert.are.same({"foo"}, response)
+        assert.are.same({ "foo" }, response)
     end)
+
     it("should remove markdown block with filetype specified", function()
         local completion = "```python\nfoo\n```"
         local lines = vim.split(completion, "\n")
         local response = rewrites.strip_md_from_completion(lines)
-        assert.are.same({"foo"}, response)
+        assert.are.same({ "foo" }, response)
     end)
+
     it("should not remove code blocks in the middle of the completion", function()
         -- i.e. when completing markdown content, maybe I should have a special check for that? cuz then wouldn't it be reasonable to even allow a full completion that is one code block?
         local completion = "This is some text\n```\nprint('Hello World')\n```\nfoo the bar"
@@ -24,26 +26,35 @@ describe("test strip markdown from completion responses", function()
         local response = rewrites.strip_md_from_completion(lines)
         assert.are.same(response, lines)
     end)
-    it("should remove special html tags <foo> and </foo>", function()
+end)
+
+describe("test strip special html thinking tags from completion responses", function()
+    -- BTW... don't include the think tag example if you want help...
+    -- it ends up stopping the model when it first reflects on the closing tag
+
+    it("should remove one set of special html tags <foo> and </foo>", function()
         local completion = "This is some text with <foo>special tag</foo> and more text."
         local lines = vim.split(completion, "\n")
         local response = rewrites.strip_md_from_completion(lines)
-        assert.are.same({"This is some text with  and more text."}, response)
+        assert.are.same({ "This is some text with  and more text." }, response)
     end)
-    it("should remove multiple special html tags", function()
-        local completion = "Multiple <foo>tags</foo> and <bar>another</bar> and <baz>one more</baz>."
+
+    -- TODO what about open only? for now, lets do nothing
+    -- TODO what about close only? for now, lets do nothing
+
+    it("when multiple tagged regions, should only remove the first one", function()
+        local completion = "Multiple <foo>tags</foo> and <foo>another</foo> and <foo>one more</foo>."
         local lines = vim.split(completion, "\n")
         local response = rewrites.strip_md_from_completion(lines)
-        assert.are.same({"Multiple  and  and ."}, response)
+        assert.are.same({ "Multiple  and  and ." }, response)
     end)
+
     it("should not remove other html tags", function()
         local completion = "This is a <div>normal html tag</div> and <p>some text</p>."
         local lines = vim.split(completion, "\n")
         local response = rewrites.strip_md_from_completion(lines)
-        assert.are.same({"This is a <div>normal html tag</div> and <p>some text</p>."}, response)
+        assert.are.same({ "This is a <div>normal html tag</div> and <p>some text</p>." }, response)
     end)
     -- PRN can I find a library/algo someone already setup to do this?
     -- or can I use structured outputs with ollama? I know I can with vllm... that might help too
 end)
-
-
