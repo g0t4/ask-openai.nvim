1fd94261473010ebe5dab14095df48aaa7d8ce06
Wes Higbee <wes.mcclure@gmail.com>
2025-05-18 18:26:06 -0500

    wow qwen3, I selected all existing tests, asked it to only add new tests, it had to rewrite existing tests and then add new and it rewrote all the existing! yes! IT EVEN KEPT MY COMMENTS!!!!! (at bottom of describe block) ... wow and this was /no_think

--- lua/ask-openai/rewrites/inline.tests.lua
+++ lua/ask-openai/rewrites/inline.tests.lua
@@ -24,6 +24,26 @@ describe("test strip markdown from completion responses", function()
         local response = rewrites.strip_md_from_completion(lines)
         assert.are.same(response, lines)
     end)
+    it("should remove special html tags <foo> and </foo>", function()
+        local completion = "This is some text with <foo>special tag</foo> and more text."
+        local lines = vim.split(completion, "\n")
+        local response = rewrites.strip_md_from_completion(lines)
+        assert.are.same({"This is some text with  and more text."}, response)
+    end)
+    it("should remove multiple special html tags", function()
+        local completion = "Multiple <foo>tags</foo> and <bar>another</bar> and <baz>one more</baz>."
+        local lines = vim.split(completion, "\n")
+        local response = rewrites.strip_md_from_completion(lines)
+        assert.are.same({"Multiple  and  and ."}, response)
+    end)
+    it("should not remove other html tags", function()
+        local completion = "This is a <div>normal html tag</div> and <p>some text</p>."
+        local lines = vim.split(completion, "\n")
+        local response = rewrites.strip_md_from_completion(lines)
+        assert.are.same({"This is a <div>normal html tag</div> and <p>some text</p>."}, response)
+    end)
     -- PRN can I find a library/algo someone already setup to do this?
     -- or can I use structured outputs with ollama? I know I can with vllm... that might help too
 end)
+
+
