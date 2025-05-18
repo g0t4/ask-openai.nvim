7a882d7f80202335228c8e8a4e56d58533d38317
Wes Higbee <wes.mcclure@gmail.com>
2025-05-18 17:02:37 -0500

    fix tests to pass/rx lines not text

--- lua/ask-openai/rewrites/inline.tests.lua
+++ lua/ask-openai/rewrites/inline.tests.lua
@@ -5,19 +5,22 @@ local assert = require("luassert")
 describe("test strip markdown from completion responses", function()
     it("should remove markdown from a simple completion", function()
         local completion = "```\nfoo\n```"
-        local response = rewrites.strip_md_from_completion(completion)
-        assert.are.equal("foo", response)
+        local lines = vim.split(completion, "\n")
+        local response = rewrites.strip_md_from_completion(lines)
+        assert.are.same({"foo"}, response)
     end)
     it("should remove markdown block with filetype specified", function()
         local completion = "```python\nfoo\n```"
-        local response = rewrites.strip_md_from_completion(completion)
-        assert.are.equal("foo", response)
+        local lines = vim.split(completion, "\n")
+        local response = rewrites.strip_md_from_completion(lines)
+        assert.are.same({"foo"}, response)
     end)
     it("should not remove code blocks in the middle of the completion", function()
         -- i.e. when completing markdown content, maybe I should have a special check for that? cuz then wouldn't it be reasonable to even allow a full completion that is one code block?
         local completion = "This is some text\n```\nprint('Hello World')\n```\nfoo the bar"
-        local response = rewrites.strip_md_from_completion(completion)
-        assert.are.equal(response, completion)
+        local lines = vim.split(completion, "\n")
+        local response = rewrites.strip_md_from_completion(lines)
+        assert.are.same(response, lines)
     end)
     -- PRN can I find a library/algo someone already setup to do this?
     -- or can I use structured outputs with ollama? I know I can with vllm... that might help too
