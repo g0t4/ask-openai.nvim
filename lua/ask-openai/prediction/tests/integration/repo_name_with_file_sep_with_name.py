5cf84c5f62e017e24b8f92cef96e6cce9a9c298e
Wes Higbee <wes.mcclure@gmail.com>
2025-05-25 04:22:10 -0500

    notes

--- lua/ask-openai/prediction/tests/integration/repo_name_with_file_sep_with_name.py
+++ lua/ask-openai/prediction/tests/integration/repo_name_with_file_sep_with_name.py
@@ -1,7 +1,10 @@
 from show import show_completion_for
 
-# WOW WORKS fine! no impact on completions (nothing noticeable thus far)
+# FYI run as a module (not script):
+#   i.e. `python -m file_level_fim`
+#   `python -m file_name_without_ext` b/c then I can keep shared show lib in dir above and nest diff "modules" in dirs like "worked"
 
+# FYI also works fine to add file_sep with calc.py (as is expected)
 request_body = {
     "prompt": "<|repo_name|>maths<|file_sep|>calc.py<|fim_prefix|>local M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\n<|fim_suffix|>\n\n\nreturn M<|fim_middle|>",
     "options": {
