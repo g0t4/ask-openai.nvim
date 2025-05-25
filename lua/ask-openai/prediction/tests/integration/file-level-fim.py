8d2ba90ba595daf12e5a0e5a6d0892ca6bef4327
Wes Higbee <wes.mcclure@gmail.com>
2025-05-25 03:29:02 -0500

    review

--- lua/ask-openai/prediction/tests/integration/file-level-fim.py
+++ lua/ask-openai/prediction/tests/integration/file-level-fim.py
@@ -1,40 +1,44 @@
-
-
 import json
 import requests
+
+raw_print = print
 from rich import print
 
+def show_completion_for(body):
+    print("[blue]## PROMPT")
+    raw_print(body["prompt"])
+
+    response = requests.post(
+        "http://ollama:11434/api/generate",
+        headers={"Content-Type": "application/json"},
+        data=json.dumps(body),
+    )
+
+    print("[yellow]## OUTPUT")
+    result = response.json()
+    print(f"done: {result.get('done')}")
+    print(f"done_reason: {result.get('done_reason')}")
+    print(f"total_duration: {float(result.get('total_duration', 0)) / 1000000:.2f} ms")
+    print(f"load_duration: {float(result.get('load_duration', 0)) / 1000000:.2f} ms")
+    print(f"prompt_eval_count: {result.get('prompt_eval_count')}")
+    print(f"prompt_eval_duration: {float(result.get('prompt_eval_duration', 0)) / 1000000:.2f} ms")
+    print(f"eval_count: {result.get('eval_count')}")
+    print(f"eval_duration: {float(result.get('eval_duration', 0)) / 1000000:.2f} ms")
+    print()
+
+    print("[blue]## COMPLETION")
+    completion = result["response"]
+    raw_print(completion)
+
 request_body = {
-    "prompt": "</think>local M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\n\nreturn M\n",
+    "prompt": "<|fim_prefix|>local M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\n<|fim_suffix|>\n\n\nreturn M<|fim_middle|>",
     "options": {
         "num_ctx": 8192
     },
     "raw": True,
     "num_predict": 200,
     "stream": False,
-    "model": "qwen2.5-coder:7b-base-q8_0"
+    "model": "qwen2.5-coder:7b-base-q8_0",
 }
 
-print("[blue]## PROMPT[/blue]")
-print(json.dumps(request_body["prompt"], indent=2))
-
-curl_command = "curl -fsSL --no-buffer -H \"Content-Type: application/json\" http://ollama:11434/api/generate -d @-"
-
-response = requests.post(
-    "http://ollama:11434/api/generate",
-    headers={"Content-Type": "application/json"},
-    data=json.dumps(request_body)
-)
-
-print("[yellow]## OUTPUT[/yellow]")
-print(f"done: {response.json().get('done')}")
-print(f"done_reason: {response.json().get('done_reason')}")
-print(f"total_duration: {float(response.json().get('total_duration', 0)) / 1000000:.2f} ms")
-print(f"load_duration: {float(response.json().get('load_duration', 0)) / 1000000:.2f} ms")
-print(f"prompt_eval_count: {response.json().get('prompt_eval_count')}")
-print(f"prompt_eval_duration: {float(response.json().get('prompt_eval_duration', 0)) / 1000000:.2f} ms")
-print(f"eval_count: {response.json().get('eval_count')}")
-print(f"eval_duration: {float(response.json().get('eval_duration', 0)) / 1000000:.2f} ms")
-print()
-
-print("[blue]## COMPLETION[/blue]")echo $response_json | jq -r .response
+show_completion_for(request_body)
