30e1ec87c3381d936bacb8da3fa1fb9c0d710d16
Wes Higbee <wes.mcclure@gmail.com>
2025-05-25 11:21:41 -0500

    dup

---
+++ tests/manual_prompting/repo_calc_edits.py
@@ -0,0 +1,33 @@
+from show import show_completion_for
+
+# FYI WORKS!
+
+# FYI run as a module (not script):
+#   i.e. `python -m test_calculator_context`
+
+calc_user_file = """
+local calc = require("calc")
+print("1+3 = " .. calc.add(1, 3))
+
+print("sqrt of -5 = " .. tostring(calc.sqroot(-5)))
+"""
+prompt_calc_user = "<|file_sep|>calc_user.py\n" + calc_user_file
+
+prompt_calc = "<|file_sep|>calc.py\n<|fim_prefix|>local M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\n<|fim_suffix|>\n\n\nreturn M<|fim_middle|>"
+
+# TODO drop repo_name, is it ok then?
+prompt = "<|repo_name|>maths\n" + prompt_calc_user + "\n" + prompt_calc
+
+# FYI also works fine to add file_sep with calc.py (as is expected)
+request_body = {
+    "prompt": prompt,
+    "options": {
+        "num_ctx": 8192
+    },
+    "raw": True,
+    "num_predict": 200,
+    "stream": False,
+    "model": "qwen2.5-coder:7b-base-q8_0",
+}
+
+show_completion_for(request_body)
