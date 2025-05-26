fdea5e69910b5644ff596e5a8a2baa52b1e6d156
Wes Higbee <wes.mcclure@gmail.com>
2025-05-26 02:52:17 -0500

    notes

--- lua/ask-openai/prediction/handlers.lua
+++ lua/ask-openai/prediction/handlers.lua
@@ -6,6 +6,8 @@ local CurrentContext = require("ask-openai.prediction.context")
 local OllamaFimBackend = require("ask-openai.prediction.backends.ollama")
 -- TODO! rewrite other backends to use new builder pattern (not a big change):
 --    TODO add :new, rearrange to self: methods
+--    TODO only do this if and when I switch to another backend...
+--    PRN use llama-server w/ its infill endpoint?!
 -- local backend = require("ask-openai.prediction.backends.legacy-completions")
 -- local backend = require("ask-openai.prediction.backends.backendsvllm")
 
