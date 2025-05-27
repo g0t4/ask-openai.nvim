b5952533b80088b98686cc3cda5a5fd629d202f6
Wes Higbee <wes.mcclure@gmail.com>
2025-05-27 14:16:12 -0500

    test starcoder2 prompt

---
+++ lua/ask-openai/backends/models/tests/fim.lua
@@ -0,0 +1,16 @@
+local fim = require("ask-openai.backends.models.fim")
+local should = require("devtools.tests.should")
+
+
+describe("starcoder2", function()
+    it("get_fim_prompt", function()
+        local request = {
+            prefix = "foo\nthe\nprefix",
+            suffix = "bar\nbaz",
+            current_context = {
+                yanks = "yanks",
+            },
+        }
+        local prompt = fim.starcoder2.get_fim_prompt(request)
+    end)
+end)
