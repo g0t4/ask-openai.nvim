ac9a3bc751f990c362d29c0c114f96210d39c8fb
Wes Higbee <wes.mcclure@gmail.com>
2025-05-27 18:46:41 -0500

    move qwen prompt code

--- lua/ask-openai/prediction/backends/ollama.lua
+++ lua/ask-openai/prediction/backends/ollama.lua
@@ -149,25 +149,6 @@ function OllamaFimBackend:body_for()
     return body_json
 end
 
-function OllamaFimBackend:get_file_level_fim_prompt()
-    log:trace("prefix", "'" .. self.prefix .. "'")
-    log:trace("suffix", "'" .. self.suffix .. "'")
-
-    -- *** File-level FIM template:
-    --   <|fim_prefix|>{code_pre}<|fim_suffix|>{code_suf}<|fim_middle|>{code_mid}<|endoftext|>
-    --   from Tech Report: https://arxiv.org/pdf/2409.12187
-    --   official example: https://github.com/QwenLM/Qwen2.5-Coder/blob/main/examples/Qwen2.5-Coder-fim.py
-
-    -- TODO ESCAPE presence of any sentinel tokens! i.e. should be rare but if someone is working on LLM code it may not be!
-
-    -- Qwen2.5-Coder:
-    local prompt = self.sentinel_tokens.fim_prefix .. self.prefix
-        .. self.sentinel_tokens.fim_suffix .. self.suffix
-        .. self.sentinel_tokens.fim_middle
-
-    return prompt
-end
-
 function OllamaFimBackend:get_current_file_path()
     -- TODO which is better?
     -- local buffer_name = vim.api.nvim_buf_get_name(0)  -- buffer's file path
@@ -177,54 +158,6 @@ end
 function OllamaFimBackend:get_repo_name()
     -- TODO confirm repo naming? is it just basename of repo root? or GH link? or org/repo?
     return vim.fn.getcwd():match("([^/]+)$")
-end
-
-function OllamaFimBackend:get_qwen2_5_coder_prompt_repo_style_with_context()
-    -- FYI! see fim.md for extensive FIM notes
-
-    local repo_name = self:get_repo_name()
-
-    -- TODO! confirm qwen2.5coder has trailing \n after repo_name
-    local repo_prompt = self.sentinel_tokens.repo_name .. repo_name .. "\n"
-    local context_file_prompt = self.sentinel_tokens.file_sep .. "nvim-recent-yanks.txt\n"
-    if self.current_context.yanks ~= "" then
-        context_file_prompt = context_file_prompt .. "\n" .. self.current_context.yanks .. "\n\n"
-    end
-
-    -- * recent edits
-    -- local recent_changes = "Here are some recent lines that were edited by the user: "
-    -- for _, change in pairs(current_context.edits) do
-    --     local str = string.format("Line %d, Column %d: %s", change.lnum, change.col, change.line)
-    --     -- todo include line/col or not?
-    --     -- todo include file?
-    --     recent_changes = recent_changes .. "\n" .. str
-    -- end
-    -- raw_prompt = recent_changes .. "\n\n" .. raw_prompt
-
-    local file_level_fim_prompt = self:get_file_level_fim_prompt()
-
-    -- PRN is this a better way to get filename?
-    -- local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(CURRENT_BUFFER), ":t")
-    local current_file_path = self:get_current_file_path()
-
-    if current_file_path == nil then
-        -- i.e. if :new and before first :w (save)
-        -- for now just leave filename blank?
-        --  or, maybe mark it as new?
-        --   can I deterine filetype using some heuristic or other metadata?
-        --   should I mark it "new"
-        log:warn("current_file_name is nil")
-        current_file_path = ""
-    end
-
-    -- confirmed: starcoder2 adds \n after filepath
-    local fim_file = self.sentinel_tokens.file_sep .. current_file_path .. "\n"
-        .. file_level_fim_prompt
-    -- WARNING: anything after <|fim_middle|> is seen as part of the completion!
-
-    return repo_prompt .. context_file_prompt .. fim_file
-    -- return repo_prompt .. fim_file
-    -- return file_level_fim_prompt
 end
 
 function OllamaFimBackend.process_sse(data)
