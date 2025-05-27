c935cfade70673db448421452e810f2af6303abc
Wes Higbee <wes.mcclure@gmail.com>
2025-05-26 22:20:44 -0500

    and voilà

--- lua/ask-openai/helpers/test_setup.lua
+++ lua/ask-openai/helpers/test_setup.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function M.test_setup_add_devtools_to_package_path()
+function M.add_devtools_to_package_path()
     local plugin_path = os.getenv("HOME") .. "/repos/github/g0t4/devtools.nvim/lua/"
     package.path = package.path .. ";" .. plugin_path .. "?.lua"
 end
