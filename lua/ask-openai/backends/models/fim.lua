6032f7bb4ec620c028d03fb9fae6f691045769c6
Wes Higbee <wes.mcclure@gmail.com>
2025-05-27 10:26:41 -0500

    mellum sentinels

--- lua/ask-openai/backends/models/fim.lua
+++ lua/ask-openai/backends/models/fim.lua
@@ -19,6 +19,55 @@ M.qwen25coder = {
     },
 }
 
+M.mellum = {
+    -- https://huggingface.co/JetBrains/Mellum-4b-base/blob/main/special_tokens_map.json
+    -- FYI very close to starcoder2
+    sentinel_tokens = {
+
+        -- FIM
+        fim_middle = "<fim_middle>",
+        fim_prefix = "<fim_prefix>",
+        fim_suffix = "<fim_suffix>",
+        fim_pad = "<fim_pad>",
+        filename = "<filename>",
+        reponame = "<reponame>",
+
+        -- roles:
+        system = "<system>",
+        slash_system = "</system>",
+        assistant = "<assistant>",
+        slash_assistant = "</assistant>",
+        user = "<user>",
+        slash_user = "</user>",
+
+        think = "<think>",
+        slash_think = "</think>",
+
+        gh_stars = "<gh_stars>",
+
+        -- PRN was base trained on commit messages? or were these just reserved for future use?
+        commit_after = "<commit_after>",
+        commit_before = "<commit_before>",
+        commit_msg = "<commit_msg>",
+
+        issue_start = "<issue_start>",
+        issue_comment = "<issue_comment>",
+        issue_closed = "<issue_closed>",
+
+        jupyter_start = "<jupyter_start>",
+        jupyter_code = "<jupyter_code>",
+        jupyter_text = "<jupyter_text>",
+        jupyter_output = "<jupyter_output>",
+        empty_output = "<empty_output>",
+
+        bos_token = "<|endoftext|>",
+        eos_token = "<|endoftext|>",
+        pad_token = "<|endoftext|>",
+        unk_token = "<|endoftext|>",
+
+    },
+}
+
 M.starcoder2 = {
     -- PRN did starcoder v1 have diff special tokens?
     sentinel_tokens = {
