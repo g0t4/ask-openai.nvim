582dcff37112b725b88081d90f15b5bafc8c8219
Wes Higbee <wes.mcclure@gmail.com>
2025-05-27 10:06:23 -0500

    add StarCoder2 special tokens

--- lua/ask-openai/backends/models/fim.lua
+++ lua/ask-openai/backends/models/fim.lua
@@ -6,6 +6,7 @@ local M = {}
 
 M.qwen25coder = {
     sentinel_tokens = {
+        -- https://huggingface.co/Qwen/Qwen2.5-Coder-14B-Instruct/blob/main/tokenizer_config.json
         fim_prefix = "<|fim_prefix|>",
         fim_middle = "<|fim_middle|>",
         fim_suffix = "<|fim_suffix|>",
@@ -16,6 +17,55 @@ M.qwen25coder = {
         im_end = "<|im_end|>",
         -- endoftext = "<|endoftext|>"
     },
+}
+
+M.StarCoder2 = {
+    sentinel_tokens = {
+        -- https://huggingface.co/bigcode/starcoder2-15b/blob/main/special_tokens_map.json
+        fim_prefix = "<fim_prefix>",
+        fim_middle = "<fim_middle>",
+        fim_suffix = "<fim_suffix>",
+        fim_pad = "<fim_pad>",
+        endoftext = "<|endoftext|>",
+        repo_name = "<repo_name>",
+        file_sep = "<file_sep>",
+        issue_start = "<issue_start>",
+        issue_comment = "<issue_comment>",
+        issue_closed = "<issue_closed>",
+        jupyter_start = "<jupyter_start>",
+        jupyter_text = "<jupyter_text>",
+        jupyter_code = "<jupyter_code>",
+        jupyter_output = "<jupyter_output>",
+        jupyter_script = "<jupyter_script>",
+        empty_output = "<empty_output>",
+        code_to_intermediate = "<code_to_intermediate>",
+        intermediate_to_code = "<intermediate_to_code>",
+
+
+        pr_start = "<pr>",
+        pr_status = "<pr_status>",
+        pr_is_merged = "<pr_is_merged>",
+        pr_base = "<pr_base>",
+        pr_file = "<pr_file>",
+        pr_base_code = "<pr_base_code>",
+        pr_diff = "<pr_diff>",
+        pr_diff_hunk = "<pr_diff_hunk>",
+        pr_comment = "<pr_comment>",
+
+        pr_event_id = "<pr_event_id>",
+        pr_review = "<pr_review>",
+        pr_review_state = "<pr_review_state>",
+        pr_review_comment = "<pr_review_comment>",
+        pr_in_reply_to_review_id = "<pr_in_reply_to_review_id>",
+        pr_in_reply_to_comment_id = "<pr_in_reply_to_comment_id>",
+
+        pr_diff_hunk_comment_line = "<pr_diff_hunk_comment_line>",
+
+        -- "<NAME>",
+        -- "<EMAIL>",
+        -- "<KEY>",
+        -- "<PASSWORD>"
+    }
 }
 
 return M
