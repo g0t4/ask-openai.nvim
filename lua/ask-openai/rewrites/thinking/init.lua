0200dd8c8d75b6b77474aa61684a14d1afc27d32
Wes Higbee <wes.mcclure@gmail.com>
2025-05-31 01:56:02 -0500

    use param, not module 'global'

--- lua/ask-openai/rewrites/thinking/init.lua
+++ lua/ask-openai/rewrites/thinking/init.lua
@@ -5,11 +5,11 @@ function M.set_thinking_tag_and_patterns(thinking_tag)
     -- FYI - == match shortest possible sequence (thus we can find first full closinng tag afterwards while skipping partial tags)
     --    also %s%S helps match everything possible, including newlines
     --    :h lua-patternitem
-    M.thinking_open_and_close_tags = "^%s*<" .. M.thinking_tag .. ">[%s%S]-</" .. M.thinking_tag .. ">"
+    M.thinking_open_and_close_tags = "^%s*<" .. thinking_tag .. ">[%s%S]-</" .. thinking_tag .. ">"
     -- PRN add () around all of the thinking tag text so I can extract it to paste a comment on demand
 
     -- TODO explore detecting and stripping or showing animation when thinking tag isn't closed yet
-    M.thinking_open_tag_only = "^%s*<" .. M.thinking_tag .. ">[^<]*"
+    M.thinking_open_tag_only = "^%s*<" .. thinking_tag .. ">[^<]*"
 end
 
 M.set_thinking_tag_and_patterns("think")
