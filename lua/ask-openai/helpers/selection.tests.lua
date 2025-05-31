1558e1e528628198e515eebef28dd9c2c820ceeb
Wes Higbee <wes.mcclure@gmail.com>
2025-05-31 12:04:48 -0500

    compose away some of test setup

--- lua/ask-openai/helpers/selection.tests.lua
+++ lua/ask-openai/helpers/selection.tests.lua
@@ -3,23 +3,38 @@ local Selection = require('ask-openai.helpers.selection')
 local should = require('devtools.tests.should')
 
 describe("get_visual_selection()", function()
-    -- TODO! get tests of this in place using plenary... with a real buffer
     -- TODO! vet if issue w/ trailing char left after accept... is it in the selection logic
-    it("no selection is empty", function()
-        local buffer_number = vim.api.nvim_create_buf(false, true)
-        vim.api.nvim_buf_set_lines(buffer_number, 0, -1, false, { "a" })
-        -- select current line using line-wise
-        local selection = Selection._get_visual_selection(buffer_number)
-        assert(selection:is_empty())
-    end)
 
+    local test_buffer_number = 0
+    local function buffer_has_lines(lines)
+        test_buffer_number = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_lines(test_buffer_number, 0, -1, false, lines)
+    end
+
+    local function get_selection()
+        return Selection._get_visual_selection(test_buffer_number)
+    end
 
     describe("linewise", function()
-        it("one line selected, only one in buffer", function()
-            -- vim.execute [[execute 'normal! <M-v>']]
-        end)
-        it("one line selected, subset of buffer", function()
-            -- vim.execute [[execute 'normal! <M-v>']]
+        describe("only one line", function()
+            it("no selection is empty", function()
+                buffer_has_lines({ "foo the bar" })
+                local selection = get_selection()
+                assert(selection:is_empty())
+            end)
+
+            it("one line selected, only one in buffer", function()
+                buffer_has_lines({ "foo the bar" })
+                local selection = get_selection()
+                -- local expected = { start_row = 1, end_row = 2 }
+                -- should.equal(selection, expected)
+
+
+                -- vim.execute [[execute 'normal! <M-v>']]
+            end)
+            it("one line selected, subset of buffer", function()
+                -- vim.execute [[execute 'normal! <M-v>']]
+            end)
         end)
 
         it("multiline selection, subset of buffer", function()
