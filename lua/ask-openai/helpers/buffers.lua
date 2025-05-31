e3f22ddc65f33bf6404afe162ee487ae1094b52c
Wes Higbee <wes.mcclure@gmail.com>
2025-05-31 10:29:17 -0500

    split out Selection type

--- lua/ask-openai/helpers/buffers.lua
+++ lua/ask-openai/helpers/buffers.lua
@@ -1,45 +1,6 @@
-local log = require("ask-openai.prediction.logger").predictions() -- TODO rename to just ask-openai logger in general
+local log = require("ask-openai.prediction.logger").predictions()
+local Selection = require("ask-openai.helpers.selection")
 local M = {}
-
-local Selection = {}
-function Selection:new(selected_lines, start_line_1indexed, start_col_1indexed, end_line_1indexed, end_col_1indexed)
-    local obj = {
-        original_text = vim.fn.join(selected_lines, "\n"),
-        start_line_1indexed = start_line_1indexed,
-        start_col_1indexed = start_col_1indexed,
-        end_line_1indexed = end_line_1indexed,
-        end_col_1indexed = end_col_1indexed,
-    }
-    setmetatable(obj, self)
-    self.__index = self
-    return obj
-end
-
-function Selection:is_empty()
-    return self.original_text == nil or self.original_text == ""
-end
-
-function Selection:to_str(as_0indexed)
-    as_0indexed = as_0indexed or false
-    if as_0indexed then
-        return
-            "Selection: 0-indexed start(line=" .. (self.start_line_1indexed - 1)
-            .. ",col=" .. (self.start_col_1indexed - 1)
-            .. ") end(line=" .. (self.end_line_1indexed - 1)
-            .. ",col=" .. (self.end_col_1indexed - 1)
-            .. ") (" .. self.original_text .. ")"
-    end
-    return
-        "Selection: 1-indexed start(line=" .. self.start_line_1indexed
-        .. ",col=" .. self.start_col_1indexed
-        .. ") end(line=" .. self.end_line_1indexed
-        .. ",col=" .. self.end_col_1indexed
-        .. ") (" .. self.original_text .. ")"
-end
-
-function Selection:log_info(as_0indexed)
-    log:info(self:to_str(as_0indexed))
-end
 
 function M.get_visual_selection()
     -- FYI getpos returns a byte index, getcharpos() returns a char index (prefer it)
---
+++ lua/ask-openai/helpers/selection.lua
@@ -0,0 +1,44 @@
+local log = require("ask-openai.prediction.logger").predictions()
+
+local Selection = {}
+
+function Selection:new(selected_lines, start_line_1indexed, start_col_1indexed, end_line_1indexed, end_col_1indexed)
+    local obj = {
+        original_text = vim.fn.join(selected_lines, "\n"),
+        start_line_1indexed = start_line_1indexed,
+        start_col_1indexed = start_col_1indexed,
+        end_line_1indexed = end_line_1indexed,
+        end_col_1indexed = end_col_1indexed,
+    }
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+function Selection:is_empty()
+    return self.original_text == nil or self.original_text == ""
+end
+
+function Selection:to_str(as_0indexed)
+    as_0indexed = as_0indexed or false
+    if as_0indexed then
+        return
+            "Selection: 0-indexed start(line=" .. (self.start_line_1indexed - 1)
+            .. ",col=" .. (self.start_col_1indexed - 1)
+            .. ") end(line=" .. (self.end_line_1indexed - 1)
+            .. ",col=" .. (self.end_col_1indexed - 1)
+            .. ") (" .. self.original_text .. ")"
+    end
+    return
+        "Selection: 1-indexed start(line=" .. self.start_line_1indexed
+        .. ",col=" .. self.start_col_1indexed
+        .. ") end(line=" .. self.end_line_1indexed
+        .. ",col=" .. self.end_col_1indexed
+        .. ") (" .. self.original_text .. ")"
+end
+
+function Selection:log_info(as_0indexed)
+    log:info(self:to_str(as_0indexed))
+end
+
+return Selection
