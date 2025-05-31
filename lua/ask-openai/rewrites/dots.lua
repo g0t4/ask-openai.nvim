3484494c63388d8be92a340f3e6f2b6dcdd7ce93
Wes Higbee <wes.mcclure@gmail.com>
2025-05-31 01:35:30 -0500

    :AskRewrite please turn this into a local M = {} ... return M style module

--- lua/ask-openai/rewrites/dots.lua
+++ lua/ask-openai/rewrites/dots.lua
@@ -1,14 +1,17 @@
-dots_module = {
-    dots = "",
-    count = 0,
-    still_thinking = function()
-        dots_module.count = dots_module.count + 1
-        if dots_module.count % 4 == 0 then
-            dots_module.dots = dots_module.dots .. "."
-        end
-        if dots_module.count > 120 then
-            dots_module.dots = ""
-            dots_module.count = 0
-        end
-    end,
-}
+local M = {}
+
+M.count = 0
+M.dots = ""
+
+function M.still_thinking(self)
+    self.count = self.count + 1
+    if self.count % 4 == 0 then
+        self.dots = self.dots .. "."
+    end
+    if self.count > 120 then
+        self.dots = ""
+        self.count = 0
+    end
+end
+
+return M
