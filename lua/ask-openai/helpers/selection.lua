38ee395f312b439643a0f3f3f809054ed96c6261
Wes Higbee <wes.mcclure@gmail.com>
2025-05-31 10:49:37 -0500

    move to stroing in 0 indexed as that's what I use

--- lua/ask-openai/helpers/selection.lua
+++ lua/ask-openai/helpers/selection.lua
@@ -2,6 +2,10 @@ local log = require("ask-openai.prediction.logger").predictions()
 
 ---@class Selection
 ---@field original_text string
+---@field start_line_0indexed integer
+---@field start_col_0indexed integer
+---@field end_line_0indexed integer
+---@field end_col_0indexed integer
 local Selection = {}
 
 function Selection:new(selected_lines, start_line_1indexed, start_col_1indexed, end_line_1indexed, end_col_1indexed)
@@ -9,9 +13,13 @@ function Selection:new(selected_lines, start_line_1indexed, start_col_1indexed, 
         original_text = vim.fn.join(selected_lines, "\n"),
         -- FYI these are all private, will have accessors ultimatley to get 0 or 1 based?
         start_line_1indexed = start_line_1indexed,
+        start_line_0indexed = start_line_1indexed - 1,
         start_col_1indexed = start_col_1indexed,
+        start_col_0indexed = start_col_1indexed - 1,
         end_line_1indexed = end_line_1indexed,
+        end_line_0indexed = end_line_1indexed - 1,
         end_col_1indexed = end_col_1indexed,
+        end_col_0indexed = end_col_1indexed - 1,
     }
     setmetatable(obj, self)
     self.__index = self
@@ -27,10 +35,10 @@ function Selection:to_str(as_0indexed)
     as_0indexed = as_0indexed or false
     if as_0indexed then
         return
-            "Selection: 0-indexed start(line=" .. (self.start_line_1indexed - 1)
-            .. ",col=" .. (self.start_col_1indexed - 1)
-            .. ") end(line=" .. (self.end_line_1indexed - 1)
-            .. ",col=" .. (self.end_col_1indexed - 1)
+            "Selection: 0-indexed start(line=" .. (self.start_line_0indexed)
+            .. ",col=" .. (self.start_col_0indexed)
+            .. ") end(line=" .. (self.end_line_0indexed)
+            .. ",col=" .. (self.end_col_0indexed)
             .. ") (" .. self.original_text .. ")"
     end
     return
