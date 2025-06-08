61c8c2da1cbb370f2cbe2c29e081f70dcb0e330b
Wes Higbee <wes.mcclure@gmail.com>
2025-06-08 16:09:15 -0500

    dont need

--- lua/ask-openai/prediction/backends/infill-llama-cpp.lua
+++
@@ -1,4 +0,0 @@
--- TODO try llama.cpp's /infill endpoint => any better feature wise / perf wise?
--- is it better at templating the FIM?
---    builds own prompt
---    IIUC only works with qwen2.5-coder currently?
