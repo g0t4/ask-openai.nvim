a2d8590929398194142ee2a40e62d61828ed734f
Wes Higbee <wes.mcclure@gmail.com>
2025-06-09 04:48:17 -0500

    manual example of ring_buffer.lua file context

--- tests/manual_prompting/qwen2_5coder/edits/ring_buffer.py
+++ tests/manual_prompting/qwen2_5coder/edits/ring_buffer.py
@@ -3,10 +3,13 @@ from show import show_completion_for
 # python3 -m qwen2_5coder.edits.ring_buffer
 
 # this is gonna be context to see if it will pay attention
-ring_buffer_file = "local RingBuffer = {}\nRingBuffer.__index = RingBuffer\n\nfunction RingBuffer.new(size)\n    return setmetatable({\n        size = size,\n        data = {},\n        head = 0,\n        count = 0,\n    }, RingBuffer)\nend\n\nfunction RingBuffer:push(item)\n    self.head = (self.head % self.size) + 1\n    self.data[self.head] = item\n    if self.count < self.size then\n        self.count = self.count + 1\n    end\nend\n\nfunction RingBuffer:items()\n    local items = {}\n    for i = 1, self.count do\n        local index = ((self.head - i + self.size) % self.size) + 1\n        table.insert(items, self.data[index])\n    end\n    return items\nend"
+ring_buffer_file = "<|file_sep|>ring.lua\nlocal RingBuffer = {}\nRingBuffer.__index = RingBuffer\n\nfunction RingBuffer.new(size)\n    return setmetatable({\n        size = size,\n        data = {},\n        head = 0,\n        count = 0,\n    }, RingBuffer)\nend\n\nfunction RingBuffer:push(item)\n    self.head = (self.head % self.size) + 1\n    self.data[self.head] = item\n    if self.count < self.size then\n        self.count = self.count + 1\n    end\nend\n\nfunction RingBuffer:items()\n    local items = {}\n    for i = 1, self.count do\n        local index = ((self.head - i + self.size) % self.size) + 1\n        table.insert(items, self.data[index])\n    end\n    return items\nend"
+
+# FYI this doesn't include a require yet... lets just see if it can figure it out anyways
+ring_current_01_minimal = "<|file_sep|>context.lua\n<|fim_prefix|>local edits = R<|fim_suffix|><|fim_middle|>"
 
 request_body = {
-    "prompt": "<|repo_name|>maths\n<|file_sep|>calc.lua\n<|fim_prefix|>local M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\n<|fim_suffix|>\n\n\nreturn M<|fim_middle|>",
+    "prompt": "<|repo_name|>nvim\n" + ring_buffer_file + ring_current_01_minimal,
     "options": {
         "num_ctx": 1024
     },
