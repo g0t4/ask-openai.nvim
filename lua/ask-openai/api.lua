34b24e9461f63b9f0bf57a6013f7da405e9ca0f2
Wes Higbee <wes.mcclure@gmail.com>
2025-06-13 15:04:13 -0500

    and now I think I don't want the commands, I see why these should be up to the end user... b/c for example I have my own streamdeck button that uses the API... and I'd rather not pollute my commands in nvim... especially b/c I have many I use like AskRewrite... so I guess other way is to drop Ask prefix but no to that too

--- lua/ask-openai/api.lua
+++ lua/ask-openai/api.lua
@@ -26,16 +26,16 @@ end
 
 function M._setup()
     -- FYI underscore in _setup is to indicate this is internal use only, won't hurt if users call it again though
-
-    vim.api.nvim_create_user_command('AskTogglePredictions', M.toggle_predictions, {})
-
-    vim.api.nvim_create_user_command('AskStatus', function()
-        if M.are_predictions_enabled() then
-            print('Ask predictions enabled')
-        else
-            print('Ask predictions disabled')
-        end
-    end, {})
+    --
+    -- vim.api.nvim_create_user_command('AskTogglePredictions', M.toggle_predictions, {})
+    --
+    -- vim.api.nvim_create_user_command('AskStatus', function()
+    --     if M.are_predictions_enabled() then
+    --         print('Ask predictions enabled')
+    --     else
+    --         print('Ask predictions disabled')
+    --     end
+    -- end, {})
 end
 
 return M
