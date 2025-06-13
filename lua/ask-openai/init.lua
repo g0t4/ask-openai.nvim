0a5c86777e98d98a7b5654dce3eff7ed12b9663f
Wes Higbee <wes.mcclure@gmail.com>
2025-06-13 13:13:34 -0500

    rearrange into a config dir, yes this feels much better for naming local

--- lua/ask-openai/init.lua
+++ lua/ask-openai/init.lua
@@ -173,7 +173,7 @@ function _module.setup(options)
     require("ask-openai.rewrites.inline").setup()
     require("ask-openai.questions.ask").setup()
     require("ask-openai.tools.mcp").setup()
-    require("ask-openai.local_config").setup()
+    require("ask-openai.config.local").setup()
 end
 
 return _module
