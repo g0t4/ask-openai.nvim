8e2a2cf786b56b38bda6b9c26a2cfa6edb4376b4
Wes Higbee <wes.mcclure@gmail.com>
2025-06-14 18:45:23 -0500

    warn if file not found, return no lines

--- lua/ask-openai/prediction/context/ctags.lua
+++ lua/ask-openai/prediction/context/ctags.lua
@@ -12,6 +12,10 @@ end
 ---@param file_path string
 ---@return string[]
 function M.read_file_lines(file_path)
+    if vim.fn.filereadable(file_path) == 0 then
+        vim.notify("read_file_lines failed to read: " .. tostring(file_path) .. " does not exist!")
+        return {}
+    end
     local lines = {}
     for line in io.lines(file_path) do
         table.insert(lines, line)
