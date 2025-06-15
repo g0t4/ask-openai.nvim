7168e262c3caabf8474c13463edb17fa21ab14f8
Wes Higbee <wes.mcclure@gmail.com>
2025-06-14 22:14:05 -0500

    idea for project level context source... in a file for now, I can paste in w/e I want.. no fancy need to do anything other than tack on the file

---
+++ lua/ask-openai/prediction/context/project.lua
@@ -0,0 +1,7 @@
+-- how about a long term, persistent, EXPLICIT set of context..
+--  things I copy and say these should always be relevant for this project,
+--  maybe start with a file (per project)...  like tags
+--  could even aggregate these for dependencies too... like tags (ctags)
+
+
+
