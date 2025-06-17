ccdb18d11115184312eb21405eebd8b08d2bf8be
Wes Higbee <wes.mcclure@gmail.com>
2025-06-16 23:46:37 -0500

    notes

--- lua/ask-openai/prediction/context/imports.lua
+++ lua/ask-openai/prediction/context/imports.lua
@@ -4,9 +4,15 @@ local require_parser = require("devtools.ts.require_parser")
 local M = {}
 
 function M.get_imports()
-    -- require_parser.get_static_requires_lua()
-    -- TODO WAIT... can I just find the imports in a file (at the top of the file) and show them when I'm editing a file and w/in its top lines?
-    --  take top lines until first blank, up to 10 lines per file?
+    -- TODO WAIT... can I just find the imports in a file (at the top of the file)
+    -- and show them when I'm editing a file and w/in its top lines?
+    -- maybe for open files only? or buffer files?
+    -- and/or take top lines until first blank, up to 10 lines per file?
+
+    -- FOR EXAMPLE:
+    --  local log = require("ask-openai.prediction.logger").predictions()
+    -- FYI could just grep files and find a way to flag them as parsed until changed
+    -- add some thing like ctags to do this, maybe even use ctags plugins/extension?
 end
 
 return M
