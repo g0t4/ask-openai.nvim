ff941636f895745faf0c45e8b8423d8ad5002382
Wes Higbee <wes.mcclure@gmail.com>
2025-06-29 18:50:00 -0500

    unused now that I moved to LSP

--- lua/ask-openai/rag/fim.lua
+++ lua/ask-openai/rag/fim.lua
@@ -54,44 +54,4 @@ function M.query_rag_via_lsp(document_prefix, document_suffix, callback)
     return _client_request_ids, _cancel_all_requests
 end
 
-function M.query_rag_first(document_prefix, document_suffix, callback)
-    if not is_rag_indexed_workspace then
-        callback(nil)
-        return
-    end
-
-    local sock
-    sock = vim.fn.sockconnect("tcp", "localhost:9999", {
-        on_data = function(_, data)
-            if data == nil then
-                log:info("nil data, aborting...")
-                return
-            end
-            -- log:info("raw rag response", vim.inspect(data))
-            data = table.concat(data, "\n")
-            if data == "" then
-                -- TODO verify if emtpy indeed is when I want to close the socket?
-                --   I've always received it last, but I can't find docs about on_data to find out when its called
-                --   i.e. is "" a signal that the server disconnected?
-                log:info("empty data, closing socket...")
-                vim.fn.chanclose(sock)
-                return
-            end
-            local response = vim.fn.json_decode(data)
-            local rag_matches = response.matches or {}
-            -- log:trace("rag_matches", vim.inspect(rag_matches))
-            callback(rag_matches)
-        end,
-        rpc = false,
-    })
-
-    local query = fim_concat(document_prefix, document_suffix)
-    local message = {
-        text = query,
-        current_file = files.get_current_file_relative_path(),
-    }
-    local json = vim.fn.json_encode(message)
-    vim.fn.chansend(sock, json .. "\n")
-end
-
 return M
