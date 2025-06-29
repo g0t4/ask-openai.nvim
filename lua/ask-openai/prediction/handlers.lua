5631e9732740047e67d4cd8dd27a4889d3b087d7
Wes Higbee <wes.mcclure@gmail.com>
2025-06-29 14:08:05 -0500

    rename

--- lua/ask-openai/prediction/handlers.lua
+++ lua/ask-openai/prediction/handlers.lua
@@ -3,7 +3,7 @@ local M = {}
 local Prediction = require("ask-openai.prediction.prediction")
 local CurrentContext = require("ask-openai.prediction.context")
 local ansi = require("ask-openai.prediction.ansi")
-local rag = require("ask-openai.rag.context")
+local rag = require("ask-openai.rag.fim")
 
 
 local OllamaFimBackend = require("ask-openai.prediction.backends.ollama")
