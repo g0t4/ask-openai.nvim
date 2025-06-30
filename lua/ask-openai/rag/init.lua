99967ccfdefc269fd8daa10c350affe06e86ec31
Wes Higbee <wes.mcclure@gmail.com>
2025-06-30 00:53:53 -0500

    yes this is better... move the testing lsp config here!

--- lua/ask-openai/rag/init.lua
+++ lua/ask-openai/rag/init.lua
@@ -1,6 +1,26 @@
 local M = {}
 
 function M.setup()
+    local lspconfig = require("lspconfig")
+    local configs = require("lspconfig.configs")
+
+    if not configs.ask_language_server then
+        configs.ask_language_server = {
+            default_config = {
+                cmd = {
+                    os.getenv("HOME") .. "/repos/github/g0t4/ask-openai.nvim/.venv/bin/python",
+                    "-m",
+                    "lsp.server",
+                },
+                cmd_cwd = os.getenv("HOME") .. "/repos/github/g0t4/ask-openai.nvim/lua/ask-openai/rag",
+                filetypes = { "lua" },
+                root_dir = require("lspconfig.util").root_pattern(".git", "."),
+
+            },
+        }
+    end
+
+    lspconfig.ask_language_server.setup({})
     -- TODO try moving lspconfig here
 end
 
