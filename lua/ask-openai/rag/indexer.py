43374ea0b8890d51e7f790dbabedbed28e14a4e0
Wes Higbee <wes.mcclure@gmail.com>
2025-07-09 02:17:21 -0500

    inline var

--- lua/ask-openai/rag/indexer.py
+++ lua/ask-openai/rag/indexer.py
@@ -281,9 +281,8 @@ class IncrementalRAGIndexer:
                 chunks = self.build_file_chunks(file_path, stat.hash)
                 updated_chunks_by_file[file_path_str] = chunks
 
-        what = paths.deleted
         message = "[bold]Deleted chunks:"
-        self.pretty_print(message, what)
+        self.pretty_print(message, paths.deleted)
         print("[bold]Updated chunks:")
         pprint(updated_chunks_by_file)
         print()
