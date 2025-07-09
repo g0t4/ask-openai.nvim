f210e5c5298e7107f6e7efb7c8a14065f63550b6
Wes Higbee <wes.mcclure@gmail.com>
2025-07-09 11:54:00 -0500

    stop using root logger

--- lua/ask-openai/rag/lsp/server.py
+++ lua/ask-openai/rag/lsp/server.py
@@ -14,6 +14,8 @@ from pygls import uris
 
 from .logs import logging
 
+logger = logging.getLogger("ask-rag")
+
 server = LanguageServer("ask_language_server", "v0.1")
 
 @server.feature(types.INITIALIZE)
@@ -21,15 +23,15 @@ def on_initialize(ls: LanguageServer, params: types.InitializeParams):
     global root_fs_path
     root_uri = params.root_uri  # ., root_uri='file:///Users/wesdemos/repos/github/g0t4/ask-openai.nvim'
     if root_uri is None:
-        logging.error(f"aborting on_initialize b/c missing client workspace root_uri {root_uri}")
+        logger.error(f"aborting on_initialize b/c missing client workspace root_uri {root_uri}")
         raise ValueError("root_uri is None")
-    logging.info(f"root_uri {root_uri}")
+    logger.info(f"root_uri {root_uri}")
     fs_path = uris.to_fs_path(root_uri)
     if fs_path is None:
-        logging.error(f"aborting on_initialize b/c missing client workspace fspath {fs_path}")
+        logger.error(f"aborting on_initialize b/c missing client workspace fspath {fs_path}")
         raise ValueError("fspath is None")
     root_fs_path = Path(fs_path)
-    logging.info(f"fspath {root_fs_path}")
+    logger.info(f"fspath {root_fs_path}")
 
 @server.feature(types.INITIALIZED)
 def on_initialized(server):
@@ -44,24 +46,24 @@ def on_initialized(server):
 # TODO!!!! :
 @server.feature(types.TEXT_DOCUMENT_DID_SAVE)
 def doc_saved(params: types.DidSaveTextDocumentParams):
-    logging.info(f"didSave: {params}")
+    logger.info(f"didSave: {params}")
 
 @server.feature(types.WORKSPACE_DID_CHANGE_WATCHED_FILES)
 def on_watched_files_changed(params: types.DidChangeWatchedFilesParams):
     #   workspace/didChangeWatchedFiles # when files changed outside of editor... i.e. nvim will detect someone else edited a file in the workspace (another nvim instance, maybe CLI tool, etc)
-    logging.info(f"didChangeWatchedFiles: {params}")
+    logger.info(f"didChangeWatchedFiles: {params}")
 
 #   # didOpen/Close are for real time change basically which I don't need, at least not for RAG (not yet?)
 
 @server.feature(types.TEXT_DOCUMENT_DID_OPEN)
 def doc_opened(params: types.DidOpenTextDocumentParams):
     # FYI just for testing purposes
-    logging.info(f"didOpen: {params}")
+    logger.info(f"didOpen: {params}")
 
 # @server.feature(types.TEXT_DOCUMENT_DID_CHANGE)
 # def doc_changed(params: types.DidChangeTextDocumentParams):
 #     # https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_didChange
-#     logging.info(f"didChange: {params}")
+#     logger.info(f"didChange: {params}")
 #     # TODO is this the only event? is this the right one?
 #     #  TODO what if I want to update the actual store? maybe I should have one process for that globally?
 #     #   FYI should not run "in-band" with the current RAG query (if any)... just let the current index serve current requests... not likely gonna be much different anyways! esp if just typing chars!
@@ -88,23 +90,23 @@ def context_query(params: ExecuteCommandParams):
     #  ! on didOpen track open files, didClose track closed... so you always KNOW WHAT IS OPEN!!!
     #
     # if params is None or params[0] is None:
-    #     logging.error(f"aborting ask.context.query b/c missing params {params}")
+    #     logger.error(f"aborting ask.context.query b/c missing params {params}")
     #     return
     # args = params[0]
-    logging.info("ask.context.query: %s", params)
+    logger.info("ask.context.query: %s", params)
     # return rag.handle_query(args)
     raise RuntimeError("Not Implemented!")
 
 @server.command("ask.rag.fim.query")
 def rag_query(ls: LanguageServer, params: ExecuteCommandParams):
     if params is None or params[0] is None:
-        logging.error(f"aborting ask.rag.fim.query b/c missing params {params}")
+        logger.error(f"aborting ask.rag.fim.query b/c missing params {params}")
         return
 
     # PRN cache last N rag queries? would help to regen another completion but maybe not that common?
 
     args = params[0]
-    logging.info("Query: %s", args)
+    logger.info("Query: %s", args)
     return rag.handle_query(args)
 
 server.start_io()
