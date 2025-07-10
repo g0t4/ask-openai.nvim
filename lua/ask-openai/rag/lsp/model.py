7be612fedf99dcc2c5158f6bdf956895c1b95926
Wes Higbee <wes.mcclure@gmail.com>
2025-07-09 23:21:44 -0500

    wrap the model so I can add some helper methods

--- lua/ask-openai/rag/lsp/model.py
+++ lua/ask-openai/rag/lsp/model.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from .logs import get_logger
 
 logger = get_logger(__name__)
@@ -5,7 +6,12 @@ logger = get_logger(__name__)
 with logger.timer("importing sentence transformers"):
     from sentence_transformers import SentenceTransformer
 
+@dataclass
+class ModelWrapper:
+    model: SentenceTransformer
+
 # TODO try Alibaba-NLP/gte-base-en-v1.5 ...  for the embeddings model
 model_name = "intfloat/e5-base-v2"
 with logger.timer(f"Load model {model_name}"):
-    model = SentenceTransformer(model_name)
+    st_model = SentenceTransformer(model_name)
+    model = ModelWrapper(st_model)
