db32ace0088141b79c791b1db339d2107614ced5
Wes Higbee <wes.mcclure@gmail.com>
2025-07-14 03:54:41 -0500

    continue to explore relationships to better understand embeddings

--- lua/ask-openai/rag/lsp/notes/maths.py
+++ lua/ask-openai/rag/lsp/notes/maths.py
@@ -1,20 +1,28 @@
+import numpy as np
 from rich import print
 import math
 from assertpy import assert_that
 
 vec = [1, 3, -5]
 
-magnitude = lambda vector: math.sqrt(sum([c*c for c in vector]))
+magnitude = lambda vector: math.sqrt(sum([c * c for c in vector]))
 
 assert_that(magnitude(vec)).is_close_to(5.916, tolerance=0.001)
 
 # %%
 
-
 vec_np = np.array(vec)
 print(vec_np)
 
+# use dot product relationship to magnitude to compute magnitude:
+dot_prod = vec_np.dot(vec_np.T)
+print(dot_prod)
+mag = np.sqrt(dot_prod)
+print(mag)
 
+# if does not blow up then its true:
+# mag^2 == dot_prod
+assert_that(math.pow(mag, 2)).is_equal_to(dot_prod)
 
 # %%
 
@@ -25,4 +33,3 @@ plt.xlim(-1, 3)
 plt.ylim(-1, 3)
 plt.grid()
 plt.show()
-
