ac5ebdd1900142902cf2aa51d1adcf0b6110b765
Wes Higbee <wes.mcclure@gmail.com>
2025-07-15 13:19:32 -0500

    put back device so I can test on cuda

--- lua/ask-openai/rag/lsp/notes/transformers_qwen3.py
+++ lua/ask-openai/rag/lsp/notes/transformers_qwen3.py
@@ -32,7 +32,7 @@ device = torch.device(
 )
 
 tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen3-Embedding-0.6B', padding_side='left')
-model = AutoModel.from_pretrained('Qwen/Qwen3-Embedding-0.6B').to("cpu")
+model = AutoModel.from_pretrained('Qwen/Qwen3-Embedding-0.6B').to(device)
 
 logger.info(f"{model.device=}")
 
