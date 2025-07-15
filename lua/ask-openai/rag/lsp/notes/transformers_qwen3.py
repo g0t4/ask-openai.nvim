004dd04229cf01528d1810a3462e2c24b5d7f7bc
Wes Higbee <wes.mcclure@gmail.com>
2025-07-15 13:18:40 -0500

    oh crap, MPS is fucking up the calculations, first three embeddings are nan?! and switch to CPU and it works fine?!

--- lua/ask-openai/rag/lsp/notes/transformers_qwen3.py
+++ lua/ask-openai/rag/lsp/notes/transformers_qwen3.py
@@ -32,7 +32,7 @@ device = torch.device(
 )
 
 tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen3-Embedding-0.6B', padding_side='left')
-model = AutoModel.from_pretrained('Qwen/Qwen3-Embedding-0.6B').to(device)
+model = AutoModel.from_pretrained('Qwen/Qwen3-Embedding-0.6B').to("cpu")
 
 logger.info(f"{model.device=}")
 
