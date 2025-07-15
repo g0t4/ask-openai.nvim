8b609c535d7b8a8e939184f81019b8da74907201
Wes Higbee <wes.mcclure@gmail.com>
2025-07-14 21:33:49 -0500

    inline variable

--- lua/ask-openai/rag/lsp/notes/transformers_qwen3.py
+++ lua/ask-openai/rag/lsp/notes/transformers_qwen3.py
@@ -62,8 +62,7 @@ def encode(input_texts):
     batch_dict.to(model.device)
     outputs = model(**batch_dict)
     embeddings = last_token_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
-    embeddings = F.normalize(embeddings, p=2, dim=1)
-    return embeddings
+    return F.normalize(embeddings, p=2, dim=1)
 
 
 
