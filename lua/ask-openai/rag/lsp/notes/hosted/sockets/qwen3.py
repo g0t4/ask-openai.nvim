512f1c5a10f2c0432790a529931188470433ec07
Wes Higbee <wes.mcclure@gmail.com>
2025-07-16 17:44:20 -0500

    remove prints

--- lua/ask-openai/rag/lsp/notes/hosted/sockets/qwen3.py
+++ lua/ask-openai/rag/lsp/notes/hosted/sockets/qwen3.py
@@ -15,19 +15,10 @@ logger = get_logger(__name__)
 def last_token_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
 
     left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
-    # prints are for checking left/right padding for what might be going wrong on MPS:
-    print(f'{attention_mask[:, -1]=}')
-    print(f'{attention_mask[:, -1].sum()=}')
-    print(f'{attention_mask.shape=}')
-    print(f'{attention_mask.shape[0]=}')
-    print(f'{left_padding=}')
     if left_padding:
-        print("LEFT")
         result = last_hidden_states[:, -1]
-        print(f'{result=}')
         return result
     else:
-        print("NOT LEFT")
         sequence_lengths = attention_mask.sum(dim=1) - 1
         batch_size = last_hidden_states.shape[0]
         return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
@@ -74,28 +65,16 @@ def encode(input_texts):
             return_tensors="pt",
         )
         batch_args.to(model.device)
-        # again, prints were for troubleshooting padding issues:
-        print("batch_args:")
-        for key, value in batch_args.items():
-            print(f'  {key}: {value}')
-            if hasattr(value, "shape"):
-                print(f'  shape: {value.shape}')
-        # dump decoded to verify correct padding/tokenization:
-        print("\ndecoded:")
-        decoded_inputs = tokenizer.batch_decode(batch_args["input_ids"])
-        for i, input_text in enumerate(decoded_inputs):
-            print(f'{i}: {input_text}')
-        print()
-        #
         outputs = model(**batch_args)
-        print(f'{outputs=}')
         embeddings = last_token_pool(outputs.last_hidden_state, batch_args['attention_mask'])
-        print(f'{embeddings=}')
         norm = F.normalize(embeddings, p=2, dim=1).cpu().numpy()
-        print(f'{norm=}')
         return norm
 
-def main():
+def test_known_embeddings():
+    # ! test the model config above produces correct embeddings for pre-canned examples
+    # ! taken from the Qwen3 README (had published values)
+    # TODO! can I find more published examples for a test suite to validate my config is correct?
+    # TODO! on startup run some of these tests?
 
     # Each query must come with a one-sentence instruction that describes the task
     task = 'Given a web search query, retrieve relevant passages that answer the query'
@@ -109,21 +88,17 @@ def main():
         "Gravity is a force that attracts two bodies towards each other. It gives weight to physical objects and is responsible for the movement of planets around the sun.",
     ]
     input_texts = queries + documents
-    # prints for padding checks:
-    for i, text in enumerate(input_texts):
-        print(f'{i}: {len(text)=}')
 
     embeddings = encode(input_texts)
-    print(embeddings)
 
     query_embeddings = embeddings[:2]  # first two are queries
     passage_embeddings = embeddings[2:]  # last two are documents
     actual_scores = (query_embeddings @ passage_embeddings.T)
-    print(f'{actual_scores=}')
     from numpy.testing import assert_array_almost_equal
     expected_scores = [[0.7645568251609802, 0.14142508804798126], [0.13549736142158508, 0.5999549627304077]]
     assert_array_almost_equal(actual_scores, expected_scores, decimal=3)
+    print(f'{actual_scores=}')
     print(f'{expected_scores=}')
 
 if __name__ == "__main__":
-    main()
+    test_known_embeddings()
