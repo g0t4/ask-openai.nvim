1a80ba1b5549fbfbdd6a2cc7bda265e46d33682b
Wes Higbee <wes.mcclure@gmail.com>
2025-07-17 01:41:18 -0500

    notes

--- lua/ask-openai/rag/lsp/model_qwen3_remote.py
+++ lua/ask-openai/rag/lsp/model_qwen3_remote.py
@@ -13,15 +13,19 @@ def _encode(texts):
     import numpy as np
     from lsp.remote.comms import EmbedClient
 
-    # TODO! Batchsize
+    # FYI for now lets leave batch_size at 8?
+    # TODO capture some sequence length distribution data so I can see how variable it is
+    #   and if small batch size would help to avoid padding for longest sequence in a bigger batch?
     def batched_encode(texts, batch_size=8):
-        # for now, let's open a new client PER batch
-        #  can add long-lived connection if need be but lets asssume this is plenty fast
-        #   would require server to handle more than one at a time too
+        # PRN? allow multiple encodes per connection! right now server closes after one!
+        # can add longer-lived connection (beyond this batch)
+        #   BUT only if timing shows its worthwhile
+        #   btw ok to let client complete an entire batch, that won't require server to handle multiple connections
+        #     only need multi connection handling IF I allow longer-lived connections (like for entire life of LS/indexer)
         all_vecs = []
         total = len(texts)
         for i in range(0, total, batch_size):
-            # TODO allow multiple encodes per connection! right now server closes!
+            # FYI right now this is a new connection PER batch
             with EmbedClient() as client:
                 logger.info(f"    batch {i}-{i+batch_size} of {total}")
                 batch = texts[i:i + batch_size]
