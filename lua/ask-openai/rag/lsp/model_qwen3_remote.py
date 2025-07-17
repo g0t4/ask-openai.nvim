6c67388b77378951d4e6c2920add0c58d971f181
Wes Higbee <wes.mcclure@gmail.com>
2025-07-17 01:47:40 -0500

    copy over known embeddings test case ... I really should make this reusable

--- lua/ask-openai/rag/lsp/model_qwen3_remote.py
+++ lua/ask-openai/rag/lsp/model_qwen3_remote.py
@@ -56,6 +56,35 @@ def get_shape() -> int:
     shape = sample_vec.shape[1]
     return shape
 
+def get_detailed_instruct(task_description: str, query: str) -> str:
+    # *** INSTRUCTION!
+    return f'Instruct: {task_description}\nQuery:{query}'
+
 def test_known_embeddings():
-    # TODO
-    pass
+
+    task = 'Given a web search query, retrieve relevant passages that answer the query'
+    queries = [
+        get_detailed_instruct(task, 'What is the capital of China?'),
+        get_detailed_instruct(task, 'Explain gravity'),
+    ]
+    # No need to add instruction for retrieval documents
+    documents = [
+        "The capital of China is Beijing.",
+        "Gravity is a force that attracts two bodies towards each other. It gives weight to physical objects and is responsible for the movement of planets around the sun.",
+    ]
+    input_texts = queries + documents
+    # prints for padding checks:
+    for i, text in enumerate(input_texts):
+        print(f'{i}: {len(text)=}')
+
+    embeddings = _encode(input_texts)
+    print(embeddings)
+
+    query_embeddings = embeddings[:2]  # first two are queries
+    passage_embeddings = embeddings[2:]  # last two are documents
+    actual_scores = (query_embeddings @ passage_embeddings.T)
+    print(f'{actual_scores=}')
+    from numpy.testing import assert_array_almost_equal
+    expected_scores = [[0.7645568251609802, 0.14142508804798126], [0.13549736142158508, 0.5999549627304077]]
+    assert_array_almost_equal(actual_scores, expected_scores, decimal=3)
+    print(f'{expected_scores=}')
