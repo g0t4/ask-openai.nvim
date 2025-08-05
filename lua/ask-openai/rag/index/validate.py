a4b487ff813f6496da534fd68d5bf0a22d183293
Wes Higbee <wes.mcclure@gmail.com>
2025-08-05 17:45:52 -0500

    renamed

--- lua/ask-openai/rag/index/validate.py
+++ lua/ask-openai/rag/index/validate.py
@@ -6,7 +6,7 @@ import faiss
 import numpy as np
 
 # usage:
-#   python3 -m index.inspecter $(_repo_root)/.rag
+#   python3 -m index.validate $(_repo_root)/.rag
 
 logger = get_logger(__name__)
 logging_fwk_to_console(level="DEBUG")
@@ -15,8 +15,8 @@ rag_dir = Path(sys.argv[1])
 dataset = load_prior_data(rag_dir, "py")
 
 print(f"{dataset.index.ntotal=}")
-print(f"{dataset.chunks_by_file.keys()=}")
-print(f"{dataset.stat_by_path.keys()=}")
+# print(f"{dataset.chunks_by_file.keys()=}")
+# print(f"{dataset.stat_by_path.keys()=}")
 
 ids = faiss.vector_to_array(dataset.index.id_map)
 
