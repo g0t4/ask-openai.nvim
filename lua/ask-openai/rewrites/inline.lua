37809b800b028b7c664278c82d42887f083dd1dc
Wes Higbee <wes.mcclure@gmail.com>
2025-08-05 04:16:51 -0500

    get rid of redundant new line - also found its way into prompt for RAG context query

--- lua/ask-openai/rewrites/inline.lua
+++ lua/ask-openai/rewrites/inline.lua
@@ -195,7 +195,7 @@ function M.stream_from_ollama(user_prompt, code, file_name)
     local user_prompt = context.cleaned_prompt
     local code_context = ""
     if code ~= nil and code ~= "" then
-        code_context = "\n Here is my code from " .. file_name
+        code_context = "Here is my code from " .. file_name
             .. ":\n" .. code
     else
         code_context = "I am working on this file: " .. file_name
