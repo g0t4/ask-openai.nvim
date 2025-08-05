7e130f851268ab3f0bc198d45c20d6bd2aacfb5d
Wes Higbee <wes.mcclure@gmail.com>
2025-08-05 15:57:53 -0500

    wow RAG+FIM did all three of these... for me in a new file!

--- lua/ask-openai/rag/index/inspecter.py
+++ lua/ask-openai/rag/index/inspecter.py
@@ -8,3 +8,8 @@ logging_fwk_to_console(level="DEBUG")
 
 rag_dir = Path(sys.argv[1])
 dataset = load_prior_data(rag_dir, "py")
+
+print(f"{dataset.index.ntotal=}")
+print(f"{dataset.chunks_by_file.keys()=}")
+print(f"{dataset.stat_by_path.keys()=}")
+
