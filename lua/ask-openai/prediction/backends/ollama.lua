2797a069ad769a84035041524897819762e6dd4b
Wes Higbee <wes.mcclure@gmail.com>
2025-08-06 02:20:11 -0500

    renames + back to qwen2.5-coder for now

--- lua/ask-openai/prediction/backends/ollama.lua
+++ lua/ask-openai/prediction/backends/ollama.lua
@@ -5,19 +5,25 @@ local meta = require("ask-openai.backends.models.meta")
 local files = require("ask-openai.helpers.files")
 
 
--- FYI! ONLY ONE AT A TIME (all false == /api/generate)
-local use_llama_server_completions = false -- qwen2.5-coder (w/ spec dec!)
-local use_ollama_api_completions = false
-local use_openaicompat_chat_completions = true
-
-
+-- * primary models I am testing (keep notes in MODELS.notes.md)
+local use_model = "qwen2.5-coder:7b-instruct-q8_0"
+-- local use_model = "gpt-oss:20b"
+-- local use_model = "qwen3-coder:30b-a3b-q8_0"
+--
 -- * llama-server (llama-cpp)
--- local url = "http://ollama:8012/completions" -- qwen2.5-coder w/ raw prompt uses this
--- local url = "http://ollama:8012/chat/completions"
+local url = "http://ollama:8012/completions" -- raw prompt: qwen2.5-coder(llama-server)
+-- local url = "http://ollama:8012/chat/completions" -- gpt-oss(llama-server, not working yet)
 -- * ollama
--- local url = "http://ollama:11434/api/generate" -- ollama serve
--- local url = "http://ollama:11434/api/chat"
-local url = "http://ollama:11434/v1/chat/completions"
+-- local url = "http://ollama:11434/api/generate" -- raw prompt: qwen2.5-coder(ollama)
+-- local url = "http://ollama:11434/api/chat" -- gpt-oss(ollama works)
+-- local url = "http://ollama:11434/v1/chat/completions" -- gpt-oss(ollama works)
+--
+-- * parser toggles
+--   (make based on url/model so not have to explicitly config too)
+local endpoint_ollama_api_generate = string.match(url, "/api/generate$")
+local endpoint_ollama_api_chat = string.match(url, "/api/chat$")
+local endpoint_llama_server_proprietary_completions = string.match(url, ":8012/completions$")
+local endpoint_openaicompat_chat_completions = string.match(url, "v1/chat/completions$")
 
 ---@class OllamaFimBackend
 ---@field prefix string
@@ -66,54 +72,9 @@ end
 
 function OllamaFimBackend:body_for()
     local body = {
-
-        -- https://huggingface.co/collections/JetBrains/mellum-68120b4ae1423c86a2da007a
-        -- model = "huggingface.co/JetBrains/Mellum-4b-base-gguf", -- no language specific fine tuning
-        -- model = "huggingface.co/JetBrains/Mellum-4b-sft-python-gguf", -- ** did better with Lua than base!
-        -- kotlin exists but no gguf on hf yet:
-        --   https://huggingface.co/JetBrains/Mellum-4b-sft-kotlin
-        -- TODO add in other fine tunes for languages as released
-
-        -- FYI set of possible models for demoing impact of fine tune
-        -- qwen2.5-coder:7b-base-q8_0  -- ** shorter responses, more "EOF" focused
-        -- qwen2.5-coder:14b-base-q8_0 -- ** shorter responses, more "EOF" focused
-        -- qwen2.5-coder:7b-instruct-q8_0 -- DO NOT USE instruct
-        -- model = "qwen2.5-coder:7b-base-q8_0", -- ** favorite
-        --
-        -- model is NOT ACTUALLY USED when hosting llama-server
-        -- model = "huggingface.co/unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M",
-        -- model = "qwen3-coder:30b-a3b-fp16", -- ** TODO TRY THIS ONE
-        -- # TODO optimal params? any new updates for llama-server that would help?
-        -- llama-server -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M --host 0.0.0.0 --port 8012 --batch-size 2048 --ubatch-size 2048 --flash-attn --n-gpu-layers 99
-        -- TODO params.n_ctx = 0;
-        -- REMEMBER just host the model in llama-server, it only runs one
-
-
-        model = "gpt-oss:20b",
-
-        -- starcoder2:15b-instruct-v0.1-q8_0                      a11b58c111d9    16 GB     6 weeks ago
-        -- starcoder2:15b-q8_0                                    95f55571067f    16 GB     6 weeks ago
-        -- starcoder2:7b-fp16                                     f0643097e171    14 GB     6 weeks ago
-        -- starcoder2:3b-q8_0                                     003abcecad23    3.2 GB    6 weeks ago
-        -- starcoder2:7b-q8_0                                     d76878e96d8a    7.6 GB    6 weeks ago
-        -- model = "starcoder2:7b-q8_0",
-
-        -- codellama:7b-code-q8_0 -- shorter too
-        -- codellama:7b-instruct-q8_0 -- longer too
-        -- codellama:7b-python-q8_0 -- doesn't do well with FIM (spits out FIM tokens text as if not recognized)... also not sure it supports FIM based on reading docs only code/instruct are mentioned for FIM support)
-        -- model = "codellama:7b-code-q8_0",
-
-        -- llama3.1:8b-text-q8_0 -- weird, generated some "code"/text in this file that wasn't terrible!... verbose
-        -- llama3.1:8b-instruct-q8_0
-        -- model = "llama3.1:8b-instruct-q8_0",
-        -- https://github.com/meta-llama/codellama/blob/main/llama/generation.py#L496
-
-        -- model = "codestral:22b-v0.1-q4_K_M",
-
-        -- ** FAST MoE
-        -- model = "deepseek-coder-v2:16b-lite-base-q8_0", -- *** 217 TPS! WORKS GOOD!
-        -- model = "deepseek-coder-v2:16b-lite-base-fp16", -- FITS! and its still fast (MoE)
-
+        -- FYI! keep model notes in MODELS.notes.md
+        -- for llama-server this is only to select the right prompt/chat builder below
+        model = use_model,
 
         raw = true, -- bypass templates (only /api/generate, not /v1/completions)
 
@@ -255,11 +216,11 @@ function OllamaFimBackend.process_sse(data)
         if success and parsed then
             -- vim.print(parsed)
             local parsed_chunk
-            if use_llama_server_completions then
+            if endpoint_llama_server_proprietary_completions then
                 parsed_chunk, done, done_reason = parse_llama_cpp_server(parsed)
-            elseif use_openaicompat_chat_completions then
+            elseif endpoint_openaicompat_chat_completions then
                 parsed_chunk, done, done_reason, thinking = parse_sse_oai_chat_completions(parsed)
-            elseif use_ollama_api_completions then
+            elseif endpoint_ollama_api_chat then
                 parsed_chunk, done, done_reason = parse_sse_ollama_chat(parsed)
             else
                 parsed_chunk, done, done_reason = parse_ollama_api_generate(parsed)
