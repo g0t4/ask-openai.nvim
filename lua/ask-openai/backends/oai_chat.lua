22044fec634e0b3f22a14972ab3fcf12c327a8ac
Wes Higbee <wes.mcclure@gmail.com>
2025-08-07 03:42:00 -0500

    finish moving notes out, add some about why I even want/need oai_* middleends

--- lua/ask-openai/backends/oai_chat.lua
+++ lua/ask-openai/backends/oai_chat.lua
@@ -9,61 +9,13 @@ end
 
 M.terminate = curl.terminate
 
--- *** output shape
---   FYI largely the same as for /v1/completions, except the message/delta under choices
---
---  created, id, model, object, system_fingerprint, usage
---  choices:
---    finish_reason:
---    index:
---    logprobs:
---
---    # stream only:
---    # https://platform.openai.com/docs/api-reference/chat-streaming
---    delta:
---      content: string
---      role: string
---      refusal: string
---      tool_calls: (formerly function_calls) ** vllm/ollama may use function_calls
---        function:
---          arguments:
---          name:
---        id:
---        type:
---        FYI means single tool calls dont span deltas? though IIAC still can do multiple across deltas
---
---    # sync only:
---    message:
---      refusal: string
---      content: string
---      role: string
---      annotations: [objects]
---      audio:
---      tool_calls: (same as in delta)
-
--- *** ollama /v1/chat/completions
--- {"id":"chatcmpl-209","object":"chat.completion.chunk","created":1743021818,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":"."},"finish_reason":null}]}
--- {
---   "id": "chatcmpl-209",
---   "object": "chat.completion.chunk",
---   "created": 1743021818,
---   "model": "qwen2.5-coder:7b-instruct-q8_0",
---   "system_fingerprint": "fp_ollama",
---   "choices": [
---     {
---       "index": 0,
---       "delta": {
---         "role": "assistant",
---         "content": "."
---       },
---       "finish_reason": null
---     }
---   ]
--- }
--- {"id":"chatcmpl-209","object":"chat.completion.chunk","created":1743021818,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":""},"finish_reason":"stop"}]}
-
--- * differs vs /v1/completions endpoint
 function M.parse_choice(choice)
+    -- TODO PUSH this entire class out into rewrites (only spot used)
+    --  above is just a URL contant  (and body is chat/completions specific too so this is all DUMB, nothing is swappable)
+    --  two primary points of difference:
+    --    body/url of request
+    --    SSE parser to get content / done / done_reason / reasoning/thinking ...
+
     -- TODO reasoning if already extracted?
     -- choice.delta.reasoning?/thinking? ollama splits this out, IIUC LM Studio does too... won't work if using harmony format with gpt-oss and its not parsed
 
--- lua/ask-openai/backends/oai_chat.notes.md
+++ lua/ask-openai/backends/oai_chat.notes.md
@@ -20,3 +20,59 @@
 -- tool_choice (none, auto, required) -- whether or not the model can/should use tools
 -- tools
 
+
+
+
+## ** output shape
+--   ******* FYI largely the same as for /v1/completions, except the message/delta under choices
+--
+--  created, id, model, object, system_fingerprint, usage
+--  choices:
+--    finish_reason:
+--    index:
+--    logprobs:
+--
+--    # stream only:
+--    # https://platform.openai.com/docs/api-reference/chat-streaming
+--    delta:
+--      content: string
+--      role: string
+--      refusal: string
+--      tool_calls: (formerly function_calls) ** vllm/ollama may use function_calls
+--        function:
+--          arguments:
+--          name:
+--        id:
+--        type:
+--        FYI means single tool calls dont span deltas? though IIAC still can do multiple across deltas
+--
+--    # sync only:
+--    message:
+--      refusal: string
+--      content: string
+--      role: string
+--      annotations: [objects]
+--      audio:
+--      tool_calls: (same as in delta)
+
+-- *** ollama /v1/chat/completions
+-- {"id":"chatcmpl-209","object":"chat.completion.chunk","created":1743021818,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":"."},"finish_reason":null}]}
+-- {
+--   "id": "chatcmpl-209",
+--   "object": "chat.completion.chunk",
+--   "created": 1743021818,
+--   "model": "qwen2.5-coder:7b-instruct-q8_0",
+--   "system_fingerprint": "fp_ollama",
+--   "choices": [
+--     {
+--       "index": 0,
+--       "delta": {
+--         "role": "assistant",
+--         "content": "."
+--       },
+--       "finish_reason": null
+--     }
+--   ]
+-- }
+-- {"id":"chatcmpl-209","object":"chat.completion.chunk","created":1743021818,"model":"qwen2.5-coder:7b-instruct-q8_0","system_fingerprint":"fp_ollama","choices":[{"index":0,"delta":{"role":"assistant","content":""},"finish_reason":"stop"}]}
+
