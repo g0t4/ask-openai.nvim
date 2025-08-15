83b5444b675e57d22cc34c8fac0374e623ae433d
Wes Higbee <wes.mcclure@gmail.com>
2025-08-15 13:42:40 -0500

    duplicate update test...  will use this to create new repro test

--- lua/ask-openai/rag/indexer_tests.py
+++ lua/ask-openai/rag/indexer_tests.py
@@ -219,6 +219,94 @@ class TestBuildIndex(unittest.TestCase):
         self.assertEqual(len(files), 2)
         self.assertEqual(index.ntotal, 4)
 
+    def test_update_reproduce_duplicated_vector_IDs(self):
+        self.trash_path(self.dot_rag_dir)
+        # * recreate source directory with initial files
+        self.trash_path(self.tmp_source_code_dir)
+        self.tmp_source_code_dir.mkdir(exist_ok=True, parents=True)
+
+        def copy_file(src, dest):
+            (self.tmp_source_code_dir / dest).write_text((self.test_cases / src).read_text())
+
+        copy_file("numbers.30.txt", "numbers.lua")  # 30 lines, 2 chunks
+        copy_file("unchanged.lua.txt", "unchanged.lua")  # 31 lines, 2 chunks
+
+        # * build initial index
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper)
+        indexer.build_index(language_extension="lua")
+
+        # * check counts
+        chunks_by_file = self.get_chunks_by_file()
+        files = self.get_files()
+        index = self.get_vector_index()
+        #
+        self.assertEqual(len(files), 2)
+        #
+        self.assertEqual(len(chunks_by_file), 2)  # 2 files
+        first_file_chunks = chunks_by_file[str(self.tmp_source_code_dir / "numbers.lua")]
+        second_file_chunks = chunks_by_file[str(self.tmp_source_code_dir / "unchanged.lua")]
+        self.assertEqual(len(first_file_chunks), 2)  # 2 chunks
+        self.assertEqual(len(second_file_chunks), 2)
+        #
+        self.assertEqual(index.ntotal, 4)
+
+        # * update a file and rebuild
+        copy_file("numbers.50.txt", "numbers.lua")  # 50 lines, 3 chunks (starts = 1-20, 16-35, 31-50)
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper)  # BTW recreate so no shared state (i.e. if cache added)
+        indexer.build_index(language_extension="lua")
+
+        # * check counts
+        chunks_by_file = self.get_chunks_by_file()
+        files = self.get_files()
+        index = self.get_vector_index()
+        #
+        self.assertEqual(len(chunks_by_file), 2)
+        #
+        first_file_chunks = chunks_by_file[str(self.tmp_source_code_dir / "numbers.lua")]
+        second_file_chunks = chunks_by_file[str(self.tmp_source_code_dir / "unchanged.lua")]
+        self.assertEqual(len(first_file_chunks), 3)
+        self.assertEqual(len(second_file_chunks), 2)
+        #
+        self.assertEqual(len(files), 2)
+        self.assertEqual(index.ntotal, 5)
+
+        # * delete a file and rebuild
+        (self.tmp_source_code_dir / "numbers.lua").unlink()
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper)
+        indexer.build_index(language_extension="lua")
+        #
+        chunks_by_file = self.get_chunks_by_file()
+        files = self.get_files()
+        index = self.get_vector_index()
+        #
+        self.assertEqual(len(chunks_by_file), 1)
+        #
+        only_file_chunks = chunks_by_file[str(self.tmp_source_code_dir / "unchanged.lua")]
+        self.assertEqual(len(only_file_chunks), 2)
+        #
+        self.assertEqual(len(files), 1)
+        self.assertEqual(index.ntotal, 2)
+
+        # * add a file
+        # FYI car.lua.txt was designed to catch issues with overlap (32 lines => 0 to 20, 15 to 35, but NOT 30 to 50 b/c only overlap exists so the next chunk has nothing unique in its non-overlapping segment) so maybe use a diff input file... if this causes issues here (move car.lua to a new test then)
+        copy_file("car.lua.txt", "car.lua")
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper)
+        indexer.build_index(language_extension="lua")
+        #
+        chunks_by_file = self.get_chunks_by_file()
+        files = self.get_files()
+        index = self.get_vector_index()
+        #
+        self.assertEqual(len(chunks_by_file), 2)
+        #
+        first_file_chunks = chunks_by_file[str(self.tmp_source_code_dir / "unchanged.lua")]
+        second_file_chunks = chunks_by_file[str(self.tmp_source_code_dir / "car.lua")]
+        self.assertEqual(len(first_file_chunks), 2)
+        self.assertEqual(len(second_file_chunks), 2)
+        #
+        self.assertEqual(len(files), 2)
+        self.assertEqual(index.ntotal, 4)
+
     def test_update_file_from_language_server(self):
         self.trash_path(self.dot_rag_dir)
         # * recreate source directory with initial files
