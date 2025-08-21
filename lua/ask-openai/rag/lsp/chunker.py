3eb310c4f14629ba39f0439e19bba4aeb6619c08
Wes Higbee <wes.mcclure@gmail.com>
2025-08-20 20:20:54 -0500

    add a helper to look at captured node heirarchies for building signatures

--- lua/ask-openai/rag/lsp/chunker.py
+++ lua/ask-openai/rag/lsp/chunker.py
@@ -118,7 +118,7 @@ def build_line_range_chunks_from_lines(path: Path, file_hash: str, lines: list[s
                 end_column0=None,
                 type=chunk_type,
                 file_hash=file_hash,
-                signature="", # TODO
+                signature="",  # TODO
             )
 
     return list(iter_chunks())
@@ -168,6 +168,17 @@ def build_ts_chunks_from_source_bytes(path: Path, file_hash: str, source_bytes: 
     with logger.timer('parse_ts ' + str(path)):
         tree = parser.parse(source_bytes)
 
+    def debug_dump_children(node):
+        # print all nodes for specific
+
+        def dump(node, prefix):
+            print(prefix + node.type)
+            prefix = prefix + "  "
+            for child in node.children:
+                dump(child, prefix)
+
+        dump(node, "")
+
     def debug_uncollected_node(node):
         # use node type filter to find specific nodes
         if node.type.find('function') <= 0:
@@ -192,6 +203,7 @@ def build_ts_chunks_from_source_bytes(path: Path, file_hash: str, source_bytes: 
             # FOR lua functions, grab --- triple dash comments before function (until blank line)
             nodes.append(node)
             collected_parent = True
+            debug_dump_children(node)
         elif node.type == "class_definition":
             # python
             nodes.append(node)
