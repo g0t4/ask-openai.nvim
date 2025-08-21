f809671d5d8844ecd679196fe1aef2a61e3ce41c
Wes Higbee <wes.mcclure@gmail.com>
2025-08-21 00:43:38 -0500

    inline variable

--- lua/ask-openai/rag/lsp/remote/client.py
+++ lua/ask-openai/rag/lsp/remote/client.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
 
     file_chunk = "local M = {}\nlocal init = require(\"ask-openai\")\nlocal config = require(\"ask-openai.config\")\n\n-- FYI uses can add commands if that's what they want, they have the API to do so:\n\nfunction M.enable_predictions()\n    config.local_share.set_predictions_enabled()\n    init.start_predictions()\nend\n\nfunction M.disable_predictions()\n    config.local_share.set_predictions_disabled()\n    init.stop_predictions()\nend\n\nfunction M.toggle_predictions()\n    if config.local_share.are_predictions_enabled() then\n        M.disable_predictions()\n    else"
     hello_world = "Hello world"
-    test_inputs = {'texts': scoring_texts}
 
     with logger.timer("Send embedding to server"):
         # intfloat/e5-base-v2 model timing:
@@ -37,7 +36,7 @@ if __name__ == "__main__":
         #   then my chunk (below)... holy F 21ms?! qwen3 full precision!
         #
         with EmbedClient() as client:
-            rx_embeddings = client.encode(test_inputs)
+            rx_embeddings = client.encode({'texts': scoring_texts})
 
     if not rx_embeddings:
         exit(-1)
