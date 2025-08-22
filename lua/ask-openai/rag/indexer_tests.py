4bfcf92b9f576ea2be4110994a7b75f9ba542057
Wes Higbee <wes.mcclure@gmail.com>
2025-08-22 10:26:33 -0500

    replace model_st with qwen3 remote inference in indexer_tests... - some weird issue with swig blowing up if I just remove model_st - so, first I am replacing all tests with a model_wrapper2 that work with it - so I can find what is left and fix it last that doesn't work with model_wrapper2 - all committed test changes here are working (tests pass)

--- lua/ask-openai/rag/indexer_tests.py
+++ lua/ask-openai/rag/indexer_tests.py
@@ -11,6 +11,7 @@ import rich
 from indexer import IncrementalRAGIndexer
 from lsp.chunker import RAGChunkerOptions
 from lsp import model_st as model_wrapper
+from lsp import model_qwen3_remote as model_wrapper2
 from lsp.storage import load_chunks_by_file, load_file_stats_by_file
 
 class TestBuildIndex(unittest.TestCase):
@@ -46,7 +47,7 @@ class TestBuildIndex(unittest.TestCase):
 
         # * recreate index
         self.trash_path(self.dot_rag_dir)
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.indexer_src_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.indexer_src_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
         indexer.build_index(language_extension="lua")
 
         # * chunks
@@ -102,7 +103,7 @@ class TestBuildIndex(unittest.TestCase):
         index = self.get_vector_index()
 
         self.assertEqual(index.ntotal, 3)
-        self.assertEqual(index.d, 768)
+        self.assertEqual(index.d, 1024)
         # rich.print(f'{index=}')
         # rich.print(f'{index.metric_type=}')
         # rich.print(f'{index.metric_arg=}')
@@ -152,7 +153,7 @@ class TestBuildIndex(unittest.TestCase):
         copy_file("unchanged.lua.txt", "unchanged.lua")  # 31 lines, 2 chunks
 
         # * build initial index
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
         indexer.build_index(language_extension="lua")
 
         # * check counts
@@ -172,7 +173,7 @@ class TestBuildIndex(unittest.TestCase):
 
         # * update a file and rebuild
         copy_file("numbers.50.txt", "numbers.lua")  # 50 lines, 3 chunks (starts = 1-20, 16-35, 31-50)
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())  # BTW recreate so no shared state (i.e. if cache added)
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())  # BTW recreate so no shared state (i.e. if cache added)
         indexer.build_index(language_extension="lua")
 
         # * check counts
@@ -192,7 +193,7 @@ class TestBuildIndex(unittest.TestCase):
 
         # * delete a file and rebuild
         (self.tmp_source_code_dir / "numbers.lua").unlink()
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
         indexer.build_index(language_extension="lua")
         #
         chunks_by_file = self.get_chunks_by_file()
@@ -210,7 +211,7 @@ class TestBuildIndex(unittest.TestCase):
         # * add a file
         # FYI car.lua.txt was designed to catch issues with overlap (32 lines => 0 to 20, 15 to 35, but NOT 30 to 50 b/c only overlap exists so the next chunk has nothing unique in its non-overlapping segment) so maybe use a diff input file... if this causes issues here (move car.lua to a new test then)
         copy_file("car.lua.txt", "car.lua")
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
         indexer.build_index(language_extension="lua")
         #
         chunks_by_file = self.get_chunks_by_file()
@@ -240,7 +241,7 @@ class TestBuildIndex(unittest.TestCase):
         # copy_file("unchanged.lua.txt", "unchanged.lua")  # 31 lines, 2 chunks
 
         # * build initial index
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
         indexer.build_index(language_extension="lua")
 
         # * check counts
@@ -259,7 +260,7 @@ class TestBuildIndex(unittest.TestCase):
         self.assertEqual(index.ntotal, 2, "index.ntotal (num vectors) should be 1")
 
         copy_file("numbers.30.txt", "numbers.lua")
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
         indexer.build_index(language_extension="lua")
 
         # * check counts
@@ -274,7 +275,7 @@ class TestBuildIndex(unittest.TestCase):
 
         # * 3rd rebuild - useful for compare new index 1 (new index), index 2 and index 3
         #  don't really need this to validate problem but I find it helpful to diff the logs
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
         indexer.build_index(language_extension="lua")
 
         self.assertEqual(index.ntotal, 2, "index.ntotal (num vectors) should be 1")
@@ -298,11 +299,11 @@ class TestBuildIndex(unittest.TestCase):
         copy_file("unchanged.lua.txt", "unchanged.lua")  # 31 lines, 2 chunks
 
         # * build initial index
-        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        indexer = IncrementalRAGIndexer(self.dot_rag_dir, self.tmp_source_code_dir, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
         indexer.build_index(language_extension="lua")
 
         from lsp import rag
-        rag.load_model_and_indexes(self.dot_rag_dir, model_wrapper)
+        rag.load_model_and_indexes(self.dot_rag_dir, model_wrapper2)
 
         copy_file("numbers.50.txt", "numbers.lua")  # 50 lines, 3 chunks
         target_file_path = self.tmp_source_code_dir / "numbers.lua"
@@ -312,7 +313,7 @@ class TestBuildIndex(unittest.TestCase):
             # version=2,
             source=target_file_path.read_text(encoding="utf-8"),
         )
-        rag.update_file_from_pygls_doc(fake_lsp_doc, model_wrapper, RAGChunkerOptions.OnlyLineRangeChunks())
+        rag.update_file_from_pygls_doc(fake_lsp_doc, model_wrapper2, RAGChunkerOptions.OnlyLineRangeChunks())
 
         # * check counts
         datasets = rag.datasets
