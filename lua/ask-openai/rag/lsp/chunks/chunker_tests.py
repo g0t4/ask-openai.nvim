1c9858e7aff35dc42ebfce95cbb917c68f95d50a
Wes Higbee <wes.mcclure@gmail.com>
2025-08-23 21:09:58 -0500

    pytest

--- lua/ask-openai/rag/lsp/chunks/chunker_tests.py
+++ lua/ask-openai/rag/lsp/chunks/chunker_tests.py
@@ -29,29 +29,29 @@ class TestReadingFilesAndNewLines:
     def test_readlines_final_line_not_empty_without_newline(self):
         test_file = test_cases / "readlines" / "final_line_not_empty_without_newline.txt"
         lines = read_text_lines(test_file)
-        self.assertEqual(lines, ["1\n", "2\n", "3"])
+        assert lines == ["1\n", "2\n", "3"]
 
         chunks = build_chunks_from_file(test_file, "fake_hash", RAGChunkerOptions.OnlyLineRangeChunks())
         first_chunk = chunks[0]
-        self.assertEqual(first_chunk.text, "1\n2\n3")  # NO final \n
+        assert first_chunk.text == "1\n2\n3"  # NO final \n
 
     def test_readlines_final_line_not_empty_with_newline(self):
         test_file = test_cases / "readlines" / "final_line_not_empty_with_newline.txt"
         lines = read_text_lines(test_file)
-        self.assertEqual(lines, ["1\n", "2\n", "3\n"])
+        assert lines == ["1\n", "2\n", "3\n"]
 
         chunks = build_chunks_from_file(test_file, "fake_hash", RAGChunkerOptions.OnlyLineRangeChunks())
         first_chunk = chunks[0]
-        self.assertEqual(first_chunk.text, "1\n2\n3\n")
+        assert first_chunk.text == "1\n2\n3\n"
 
     def test_readlines_final_line_empty_with_newline(self):
         test_file = test_cases / "readlines" / "final_line_empty_with_newline.txt"
         lines = read_text_lines(test_file)
-        self.assertEqual(lines, ["1\n", "2\n", "3\n", "\n"])
+        assert lines == ["1\n", "2\n", "3\n", "\n"]
 
         chunks = build_chunks_from_file(test_file, "fake_hash", RAGChunkerOptions.OnlyLineRangeChunks())
         first_chunk = chunks[0]
-        self.assertEqual(first_chunk.text, "1\n2\n3\n\n")
+        assert first_chunk.text == "1\n2\n3\n\n"
 
 class TestLowLevel_LinesChunker:
     """
@@ -64,48 +64,48 @@ class TestLowLevel_LinesChunker:
 
     def test_build_line_range_chunks(self):
         lines = [str(x) + "\n" for x in range(0, 30)]  # 0\n1\n ... 29\n
-        self.assertEqual(len(lines), 30)  # FYI mostly as reminder range is not inclusive :)
+        assert len(lines) == 30  # FYI mostly as reminder range is not inclusive :)
         chunks = build_line_range_chunks_from_lines(Path("foo.txt"), "fake_hash", lines)
-        self.assertEqual(len(chunks), 2)
+        assert len(chunks) == 2
         chunk1 = chunks[0]
         expected_text1 = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n"
-        self.assertEqual(chunk1.text, expected_text1)
-        self.assertEqual(chunk1.start_line0, 0)
-        self.assertEqual(chunk1.end_line0, 19)
-        self.assertEqual(chunk1.start_column0, 0)
-        self.assertEqual(chunk1.end_column0, None)
-        self.assertEqual(chunk1.file, "foo.txt")
+        assert chunk1.text == expected_text1
+        assert chunk1.start_line0 == 0
+        assert chunk1.end_line0 == 19
+        assert chunk1.start_column0 == 0
+        assert chunk1.end_column0 == None
+        assert chunk1.file == "foo.txt"
         # chunk_id
         #   echo -n "foo.txt:lines:0-19:fake_hash" | sha256sum | cut -c -16
-        self.assertEqual(chunk1.id, "fc78caf765f98c08")
+        assert chunk1.id == "fc78caf765f98c08"
         #   bitmaths "0xfc78caf765f98c08 & 0x7FFFFFFFFFFFFFFF"
         #      FYI this case id_int the high bits are truncated
-        self.assertEqual(chunk1.id_int, "8969141821824928776")
-        self.assertEqual(chunk1.type, "lines")
-        self.assertEqual(chunk1.file_hash, "fake_hash")
+        assert chunk1.id_int == "8969141821824928776"
+        assert chunk1.type == "lines"
+        assert chunk1.file_hash == "fake_hash"
 
         chunk2 = chunks[1]
         expected_text2 = "15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n"
-        self.assertEqual(chunk2.text, expected_text2)
-        self.assertEqual(chunk2.start_line0, 15)
-        self.assertEqual(chunk2.end_line0, 29)  # 30 lines total, last has line 29 which is on file line 28
-        self.assertEqual(chunk2.start_column0, 0)
-        self.assertEqual(chunk2.end_column0, None)
-        self.assertEqual(chunk2.file, "foo.txt")
+        assert chunk2.text == expected_text2
+        assert chunk2.start_line0 == 15
+        assert chunk2.end_line0 == 29  # 30 lines total, last has line 29 which is on file line 28
+        assert chunk2.start_column0 == 0
+        assert chunk2.end_column0 == None
+        assert chunk2.file == "foo.txt"
         # chunk_id
         #   echo -n "foo.txt:lines:15-29:fake_hash" | sha256sum | cut -c -16
-        self.assertEqual(chunk2.id, "5f0546bff37a52e6")
+        assert chunk2.id == "5f0546bff37a52e6"
         #   bitmaths "0x5f0546bff37a52e6 & 0x7FFFFFFFFFFFFFFF"
         #     FYI no high_bits to truncate in this case
-        self.assertEqual(chunk2.id_int, "6846956598724285158")
-        self.assertEqual(chunk2.type, "lines")
-        self.assertEqual(chunk2.file_hash, "fake_hash")
+        assert chunk2.id_int == "6846956598724285158"
+        assert chunk2.type == "lines"
+        assert chunk2.file_hash == "fake_hash"
 
     def test_last_chunk_has_one_line_past_overlap__thus_is_kept(self):
         #   chunk1: 0=>19
         #   chunk2: 15=>20 # has one line past overlap, so keep chunk
         lines = [str(x) + "\n" for x in range(0, 21)]  # 0\n1\n ... 20\n
-        self.assertEqual(len(lines), 21)
+        assert len(lines) == 21
         chunks = build_line_range_chunks_from_lines(Path("foo.txt"), "fake_hash", lines)
         assert [(c.start_line0, c.end_line0) for c in chunks] == [(0, 19), (15, 20)]
 
@@ -114,7 +114,7 @@ class TestLowLevel_LinesChunker:
         #   chunk1: 0=>19
         #   chunk2: 15=>19 # skip as its only overlap
         lines = [str(x) + "\n" for x in range(0, 20)]  # 0\n1\n ... 19\n
-        self.assertEqual(len(lines), 20)
+        assert len(lines) == 20
         chunks = build_line_range_chunks_from_lines(Path("foo.txt"), "fake_hash", lines)
         assert [(c.start_line0, c.end_line0) for c in chunks] == [(0, 19)]
 
@@ -122,45 +122,45 @@ class TestTreesitterPythonChunker:
 
     def test_two_functions_py(self):
         chunks = _ts_chunks_from_file_with_fake_hash(test_cases_ts / "two_functions.py", RAGChunkerOptions.OnlyTsChunks())
-        self.assertEqual(len(chunks), 2)
+        assert len(chunks) == 2
 
         first_chunk = chunks[0]
         expected_func1_chunk_text = "def func1():\n    return 1"  # TODO new line between two funcs? how about skip that?
-        self.assertEqual(first_chunk.text, expected_func1_chunk_text)
+        assert first_chunk.text == expected_func1_chunk_text
 
         second_chunk = chunks[1]
         expected_func2_chunk_text = "def func2():\n    return 2"
-        self.assertEqual(second_chunk.text, expected_func2_chunk_text)
+        assert second_chunk.text == expected_func2_chunk_text
 
     def test_nested_functions_py(self):
         chunks = _ts_chunks_from_file_with_fake_hash(test_cases_ts / "nested_functions.py", RAGChunkerOptions.OnlyTsChunks())
-        self.assertEqual(len(chunks), 2)
+        assert len(chunks) == 2
 
         first_chunk = chunks[0]
         expected_first_chunk = "def f():\n\n    def g():\n        return 42"
-        self.assertEqual(first_chunk.text, expected_first_chunk)
+        assert first_chunk.text == expected_first_chunk
 
         second_chunk = chunks[1]
         expected_second_chunk = "def g():\n        return 42"
-        self.assertEqual(second_chunk.text, expected_second_chunk)
+        assert second_chunk.text == expected_second_chunk
         # TODO how do I want to handle nesting? maybe all in one if its under a token count?
         # and/or index nested too?
 
     def test_dataclass_py(self):
         chunks = _ts_chunks_from_file_with_fake_hash(test_cases_ts / "dataclass.py", RAGChunkerOptions.OnlyTsChunks())
-        self.assertEqual(len(chunks), 1)
+        assert len(chunks) == 1
 
         first_chunk = chunks[0]
         class_text = """class Customer():
     id: int
     name: str
     email: str"""
-        self.assertEqual(first_chunk.text, class_text)
+        assert first_chunk.text == class_text
 
     def test_class_with_functions_py(self):
         chunks = _ts_chunks_from_file_with_fake_hash(test_cases_ts / "class_with_functions.py", RAGChunkerOptions.OnlyTsChunks())
 
-        self.assertEqual(len(chunks), 5)
+        assert len(chunks) == 5
         self.maxDiff = None
 
         first_chunk = chunks[0]
@@ -180,22 +180,22 @@ class TestTreesitterPythonChunker:
 
     def __str__(self):
         return f'Person({self.first_name}, {self.last_name}, {self.dob})'"""
-        self.assertEqual(first_chunk.text, class_text)
+        assert first_chunk.text == class_text
 
-        self.assertEqual(chunks[1].text, """def __init__(self, first_name, last_name, dob):
+        assert chunks[1].text == """def __init__(self, first_name, last_name, dob):
         self.first_name = first_name
         self.last_name = last_name
-        self.dob = dob""")
+        self.dob = dob"""
 
-        self.assertEqual(chunks[2].text, """def say_hi(self):
-        return f'Hi, {self.first_name} {self.last_name}!'""")
+        assert chunks[2].text == """def say_hi(self):
+        return f'Hi, {self.first_name} {self.last_name}!'"""
 
-        self.assertEqual(chunks[3].text, """def is_of_age(self):
+        assert chunks[3].text == """def is_of_age(self):
         current_year = datetime.now().year
-        return (current_year - self.dob.year) >= 18""")
+        return (current_year - self.dob.year) >= 18"""
 
-        self.assertEqual(chunks[4].text, """def __str__(self):
-        return f'Person({self.first_name}, {self.last_name}, {self.dob})'""")
+        assert chunks[4].text == """def __str__(self):
+        return f'Person({self.first_name}, {self.last_name}, {self.dob})'"""
 
     # def WIP_test_ts_toplevel_query_py(self):
     #     from tree_sitter_languages import get_parser, get_language
