552a94509e51ac93e3cd50a2ffbf596c4e708ffd
Wes Higbee <wes.mcclure@gmail.com>
2025-08-30 02:48:10 -0500

    spike idea for wrapping some vim lower level APIs

--- lua/ask-openai/helpers/selection.lua
+++ lua/ask-openai/helpers/selection.lua
@@ -65,6 +65,81 @@ function Selection:log_info(message)
     log:info(message, self:to_str())
 end
 
+-- * GetPos
+
+-- Map <leader>b to print the cursor position.
+vim.keymap.set({ 'n', 'v' }, '<leader>b', function()
+    vim.print(GetPos.SelectionRange_Line1Col1())
+end, { desc = "Print cursor position (getpos)" })
+
+
+
+_G.GetPos = {}
+
+--- getpos(".")
+function GetPos.CursorPosition_Line1Col1()
+    return GetPos.Line1Col1(".")
+end
+
+function GetPos.OtherEndOfSelection_Line1Col1()
+    -- if not in visual mode, this will return CursorPosition (same as ".")
+    return GetPos.Line1Col1("v")
+end
+
+---@class GetPosSelectionRange
+---@field start_line_b1 integer
+---@field start_col_b1 integer
+---@field end_line_b1 integer
+---@field end_col_b1 integer
+_G.GetPosSelectionRange = {}
+
+
+---Returns the selection range in 1‑indexed line/column coordinates.
+---The order is always start → end regardless of cursor direction.
+---@return GetPosSelectionRange
+function GetPos.SelectionRange_Line1Col1()
+    local dot_line_base1, dot_col_base1 = GetPos.Line1Col1(".")
+    local v_line_base1, v_col_base1 = GetPos.Line1Col1("v")
+
+    local start_line, start_col, end_line, end_col
+    if dot_line_base1 < v_line_base1 or (dot_line_base1 == v_line_base1 and dot_col_base1 <= v_col_base1) then
+        return {
+            start_line_b1 = dot_line_base1,
+            start_col_b1  = dot_col_base1,
+            end_line_b1   = v_line_base1,
+            end_col_b1    = v_col_base1,
+        }
+    else
+        return {
+            start_line_b1 = v_line_base1,
+            start_col_b1  = v_col_base1,
+            end_line_b1   = dot_line_base1,
+            end_col_b1    = dot_col_base1,
+        }
+    end
+end
+
+function GetPos.LastLineOfBuffer_Line1Col1()
+    return GetPos.Line1Col1("$")
+end
+
+function GetPos.LastVisibleLine_Line1Col1()
+    return GetPos.Line1Col1("w$")
+end
+
+function GetPos.FirstVisibleLine_Line1Col1()
+    return GetPos.Line1Col1("w0")
+end
+
+---@param expr string
+function GetPos.Line1Col1(expr)
+    -- FYI offset has to do with virtualedit (when cursor is allowed to stop on non-char positions)
+    local bufnr, lnum_base1, col_base1, offset = unpack(vim.fn.getpos(expr))
+    return lnum_base1, col_base1
+end
+
+-- * end GetPos
+
 function Selection._get_visual_selection_for_window_id(window_id)
     local current_mode = vim.fn.mode()
     local last_visualmode = vim.fn.visualmode()
