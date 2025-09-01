26c26239832306a9fa602d034229f798c42c1a9c
Wes Higbee <wes.mcclure@gmail.com>
2025-09-01 03:10:59 -0500

    split out FIMPerformance module

---
+++ lua/ask-openai/prediction/fim_performance.lua
@@ -0,0 +1,74 @@
+local ansi = require("ask-openai.prediction.ansi")
+local log = require("ask-openai.logs.logger").predictions()
+
+---@class FIMPerformance
+---@field time_to_first_token_ms? number
+---@field rag_duration_ms? number
+---@field total_duration_ms? number
+local FIMPerformance = {}
+FIMPerformance.__index = FIMPerformance
+
+function FIMPerformance:new()
+    self._prediction_start_time_ns = get_time_in_ns()
+
+    self._rag_start_time_ns = nil
+    self.rag_duration_ms = nil
+
+    self.time_to_first_token_ms = nil
+
+    self.total_duration_ms = nil
+    return self
+end
+
+function FIMPerformance:token_arrived()
+    if self.time_to_first_token_ms ~= nil then
+        return
+    end
+    self.time_to_first_token_ms = get_elapsed_time_in_rounded_ms(self._prediction_start_time_ns)
+end
+
+function FIMPerformance:rag_started()
+    if self._rag_start_time_ns ~= nil then
+        error("rag called a second time, aborting...")
+    end
+    self._rag_start_time_ns = get_time_in_ns()
+end
+
+function FIMPerformance:rag_done()
+    if self.rag_duration_ms ~= nil then
+        error("rag_done called a second time, timings might be wrong, aborting...")
+    end
+    if self._rag_start_time_ns == nil then
+        error("rag_done called before rag_started, aborting...")
+    end
+    self.rag_duration_ms = get_elapsed_time_in_rounded_ms(self._rag_start_time_ns)
+end
+
+function FIMPerformance:overall_done()
+    if self.total_duration_ms ~= nil then
+        error("completed called a second time, timings might be wrong, aborting...")
+    end
+    self.total_duration_ms = get_elapsed_time_in_rounded_ms(self._prediction_start_time_ns)
+
+    local message = string.format(
+        "FIMPerformance - %s %s %s",
+        "TTFT: " .. ansi.underline(self.time_to_first_token_ms .. " ms"),
+        "RAG: " .. ansi.underline(self.rag_duration_ms .. " ms"),
+        "TOTAL: " .. ansi.underline(self.total_duration_ms .. " ms")
+    )
+    log:info(message)
+
+    return message
+end
+
+function FIMPerformance:TTFT_minus_RAG_ms()
+    if self.time_to_first_token_ms == nil then
+        return nil
+    end
+    if self.rag_duration_ms == nil then
+        return nil
+    end
+    return self.time_to_first_token_ms - self.rag_duration_ms
+end
+
+return FIMPerformance
--- lua/ask-openai/prediction/handlers.lua
+++ lua/ask-openai/prediction/handlers.lua
@@ -4,6 +4,7 @@ local Prediction = require("ask-openai.prediction.prediction")
 local ansi = require("ask-openai.prediction.ansi")
 local rag_client = require("ask-openai.rag.client")
 local api = require("ask-openai.api")
+local FIMPerformance = require("ask-openai.prediction.fim_performance")
 require("devtools.performance")
 
 local OllamaFimBackend = require("ask-openai.prediction.backends.llama")
@@ -103,76 +104,6 @@ local function get_prefix_suffix()
         end
     end
     return document_prefix, document_suffix
-end
-
----@class FIMPerformance
----@field time_to_first_token_ms? number
----@field rag_duration_ms? number
----@field total_duration_ms? number
-local FIMPerformance = {}
-FIMPerformance.__index = FIMPerformance
-
-function FIMPerformance:new()
-    self._prediction_start_time_ns = get_time_in_ns()
-
-    self._rag_start_time_ns = nil
-    self.rag_duration_ms = nil
-
-    self.time_to_first_token_ms = nil
-
-    self.total_duration_ms = nil
-    return self
-end
-
-function FIMPerformance:token_arrived()
-    if self.time_to_first_token_ms ~= nil then
-        return
-    end
-    self.time_to_first_token_ms = get_elapsed_time_in_rounded_ms(self._prediction_start_time_ns)
-end
-
-function FIMPerformance:rag_started()
-    if self._rag_start_time_ns ~= nil then
-        error("rag called a second time, aborting...")
-    end
-    self._rag_start_time_ns = get_time_in_ns()
-end
-
-function FIMPerformance:rag_done()
-    if self.rag_duration_ms ~= nil then
-        error("rag_done called a second time, timings might be wrong, aborting...")
-    end
-    if self._rag_start_time_ns == nil then
-        error("rag_done called before rag_started, aborting...")
-    end
-    self.rag_duration_ms = get_elapsed_time_in_rounded_ms(self._rag_start_time_ns)
-end
-
-function FIMPerformance:overall_done()
-    if self.total_duration_ms ~= nil then
-        error("completed called a second time, timings might be wrong, aborting...")
-    end
-    self.total_duration_ms = get_elapsed_time_in_rounded_ms(self._prediction_start_time_ns)
-
-    local message = string.format(
-        "FIMPerformance - %s %s %s",
-        "TTFT: " .. ansi.underline(self.time_to_first_token_ms .. " ms"),
-        "RAG: " .. ansi.underline(self.rag_duration_ms .. " ms"),
-        "TOTAL: " .. ansi.underline(self.total_duration_ms .. " ms")
-    )
-    log:info(message)
-
-    return message
-end
-
-function FIMPerformance:TTFT_minus_RAG_ms()
-    if self.time_to_first_token_ms == nil then
-        return nil
-    end
-    if self.rag_duration_ms == nil then
-        return nil
-    end
-    return self.time_to_first_token_ms - self.rag_duration_ms
 end
 
 function M.ask_for_prediction()
