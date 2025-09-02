75a3d2f1f37c17f192eb096d57c9918d1a08fcdf
Wes Higbee <wes.mcclure@gmail.com>
2025-09-02 14:44:16 -0500

    test cursor in middle of line, splits cursor char into suffix successfully

--- lua/ask-openai/prediction/prefix_suffix_tests.lua
+++ lua/ask-openai/prediction/prefix_suffix_tests.lua
@@ -58,17 +58,18 @@ describe("get_prefix_suffix", function()
                 assert.equal("line 4\nline 5\nline 6", suffix)
             end)
 
-            -- it("cursor is in middle of line", function()
-            --     local bufnr = new_buffer_with_lines(seven_lines)
-            --     local line_base1 = 4 -- 'line 4'
-            --     local col_base0 = 3 -- 3 is the 'e' in 'line'
-            --     vim.api.nvim_win_set_cursor(0, { line_base1, col_base0 })
-            --
-            --     local take_lines = 2
-            --     local prefix, suffix = ps.get_prefix_suffix(take_lines)
-            --
-            --     assert.equal("", prefix)
-            -- end)
+            it("cursor is in middle of line", function()
+                local bufnr = new_buffer_with_lines(seven_lines)
+                local line_base1 = 4 -- 'line 4'
+                local col_base0 = 3 -- 3 is the 'e' in 'line'
+                vim.api.nvim_win_set_cursor(0, { line_base1, col_base0 })
+
+                local take_lines = 2
+                local prefix, suffix = ps.get_prefix_suffix(take_lines)
+
+                assert.equal("line 2\nline 3\nlin", prefix)
+                assert.equal("e 4\nline 5\nline 6", suffix)
+            end)
         end)
 
         describe("cursor line is not empty", function()
