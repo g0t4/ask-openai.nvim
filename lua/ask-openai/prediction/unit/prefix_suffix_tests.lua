646725d0b963552cccbf1075e3a5473d071d5eda
Wes Higbee <wes.mcclure@gmail.com>
2025-09-02 11:17:15 -0500

    consolidate all prefix_suffix tests

--- lua/ask-openai/prediction/unit/prefix_suffix_tests.lua
+++ lua/ask-openai/prediction/unit/prefix_suffix_tests.lua
@@ -1,5 +1,7 @@
+local assert = require 'luassert'
 require("ask-openai.helpers.test_setup").modify_package_path()
 require("ask-openai.helpers.buffer_testing")
+
 local ps = require("ask-openai.prediction.prefix_suffix")
 
 describe("get_prefix_suffix", function()
@@ -26,5 +28,64 @@ describe("get_prefix_suffix", function()
 
         assert.equal("line 1\nline 2\nline 3\n", prefix)
         assert.equal("line 4\nline 5\nline 6\nline 7", suffix)
+    end)
+end)
+
+
+local ps = require("ask-openai.prediction.prefix_suffix")
+
+describe("get_line_range", function()
+    it("allowed lines both before/after are within document", function()
+        local current_row = 40
+        local allow_lines = 10
+        local total_rows = 100
+
+        local first_row, last_row = ps.get_line_range(current_row, allow_lines, total_rows)
+
+        assert.equals(30, first_row)
+        assert.equals(50, last_row)
+    end)
+
+    it("current line is less than allowed lines, adds before's overflow to last_row", function()
+        local current_row = 4
+        local allow_lines = 10
+        local total_rows = 100
+        local first_row, last_row = ps.get_line_range(current_row, allow_lines, total_rows)
+        assert.equals(0, first_row)
+        -- 6 extra overflow lines from before, 10+6==16
+        --    16+4 = 20
+        assert.equals(20, last_row)
+    end)
+
+    it("current line is greater than total_lines - allow_lines, adds after's overflow to first_row", function()
+        local current_row = 95
+        local allow_lines = 10
+        local total_rows = 100
+
+        local first_row, last_row = ps.get_line_range(current_row, allow_lines, total_rows)
+        -- local first_row, last_row = handlers.get_line_range(current_row, allow_lines, total_rows)
+        -- 5 extra overflow lines from after, 10+5==15
+        -- 95-15==80
+        assert.equals(80, first_row)
+        assert.equals(100, last_row)
+    end)
+
+    it("total rows is less than allowed lines in both directions, takes all lines", function()
+        local current_row = 4
+        local allow_lines = 10
+        local total_rows = 8
+        local first_row, last_row = ps.get_line_range(current_row, allow_lines, total_rows)
+        assert.equals(0, first_row)
+        assert.equals(8, last_row)
+    end)
+
+    it("current_row + allow_lines == total_rows", function()
+        -- arguably redundant, boundary condition
+        local current_row = 90
+        local allow_lines = 10
+        local total_rows = 100
+        local first_row, last_row = ps.get_line_range(current_row, allow_lines, total_rows)
+        assert.equals(80, first_row)
+        assert.equals(100, last_row)
     end)
 end)
