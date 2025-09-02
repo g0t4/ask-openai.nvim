acdd34b3a56c1d47c379f3199fe2e9a12299be9d
Wes Higbee <wes.mcclure@gmail.com>
2025-09-02 11:05:19 -0500

    first test of get_prefix_suffix

--- lua/ask-openai/prediction/unit/prefix_suffix_tests.lua
+++ lua/ask-openai/prediction/unit/prefix_suffix_tests.lua
@@ -1,19 +1,31 @@
-local a = require("plenary.async")
-local tests = require("plenary.busted")
-local assert = require 'luassert'
-local match = require 'luassert.match'
-
-require("ask-openai.rx.tests-setup")
+require("ask-openai.helpers.test_setup").modify_package_path()
+require("ask-openai.helpers.buffer_testing")
 
 local handlers = require("ask-openai.prediction.handlers")
 
 describe("get_prefix_suffix", function()
-    local lines_1_to_30 = {}
-    for i = 1, 30 do
-        lines_1_to_30[i] = "line " .. i
+    local function create_lines(num_lines)
+        local lines = {}
+        for i = 1, num_lines do
+            lines[i] = "line " .. i
+        end
+        return lines
     end
 
-    it("", function()
-        new_buffer_with_lines(lines_1_to_30)
+    local seven_lines = {
+        "line 1", "line 2", "line 3", "line 4", "line 5",
+        "line 6", "line 7"
+    }
+
+    it("splits prefix and suffix", function()
+        local bufnr = new_buffer_with_lines(seven_lines)
+        local line_base1 = 4 -- 4th line
+        local col_base0 = 0 -- cursor in first col
+        vim.api.nvim_win_set_cursor(0, { line_base1, col_base0 })
+
+        local prefix, suffix = get_prefix_suffix(bufnr)
+
+        assert.equal("line 1\nline 2\nline 3\n", prefix)
+        assert.equal("line 4\nline 5\nline 6\nline 7", suffix)
     end)
 end)
