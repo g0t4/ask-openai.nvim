c78ccdcdb571f2e76a5c4e4922ff6c9df6fe3df3
Wes Higbee <wes.mcclure@gmail.com>
2025-09-02 09:31:12 -0500

    return bufnr from load_lines

--- lua/ask-openai/helpers/buffer_testing.lua
+++ lua/ask-openai/helpers/buffer_testing.lua
@@ -1,9 +1,8 @@
-local test_buffer_number = 0
 function load_lines(lines)
-    test_buffer_number = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_lines(test_buffer_number, 0, -1, false, lines)
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
 
-    local win = vim.api.nvim_open_win(test_buffer_number, true, {
+    local win = vim.api.nvim_open_win(bufnr, true, {
         relative = 'editor',
         width = 80,
         height = 10,
@@ -12,7 +11,7 @@ function load_lines(lines)
         style = 'minimal',
     })
     vim.api.nvim_set_current_win(win)
-
+    return bufnr
     -- FYI not setting cursor before commands, let the tests handle reliably setting cursor
     -- vim.api.nvim_win_set_cursor(win, { 1, 0 })
 end
