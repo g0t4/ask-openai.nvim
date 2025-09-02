82444c0f1bf6b7406f1d4ee91aaf4cc7348b7b54
Wes Higbee <wes.mcclure@gmail.com>
2025-09-02 09:31:27 -0500

    use load_lines in new prefix_suffix tests

--- lua/ask-openai/prediction/unit/prefix_suffix_tests.lua
+++ lua/ask-openai/prediction/unit/prefix_suffix_tests.lua
@@ -8,7 +8,12 @@ require("ask-openai.rx.tests-setup")
 local handlers = require("ask-openai.prediction.handlers")
 
 describe("get_prefix_suffix", function()
-    it("", function()
+    local lines_1_to_30 = {}
+    for i = 1, 30 do
+        lines_1_to_30[i] = "line " .. i
+    end
 
+    it("", function()
+        load_lines(lines_1_to_30)
     end)
 end)
