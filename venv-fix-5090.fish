6f90f5d197600441081e1aa25334a9eba8765d1e
Wes Higbee <wes.mcclure@gmail.com>
2025-09-02 21:46:08 -0500

    install flash-attn w/o deps is only way

--- venv-fix-5090.fish
+++
@@ -1,2 +0,0 @@
-# TODO put all in here? or just uv sync first?
-uv pip install   --index-url https://download.pytorch.org/whl/cu128 torch torchvision torchaudio
---
+++ venv-fix-buil21.fish
@@ -0,0 +1,3 @@
+#!/usr/bin/env fish
+# uv pip install   --index-url https://download.pytorch.org/whl/cu128 torch torchvision torchaudio
+uv pip install flash-attn einops --no-build-isolation --no-deps
