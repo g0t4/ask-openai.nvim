352a5db50463fa5c593346201dd401ec8c14770b
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 13:03:00 -0500

    notes

--- lua/ask-openai/rag/lsp/inference/server/qwen3_embeddings.py
+++ lua/ask-openai/rag/lsp/inference/server/qwen3_embeddings.py
@@ -46,8 +46,8 @@ def dump_device_memory_stats(message: str = ""):
     # if reserved > 12:
     #     re-ranker is causing the explosion in cache...
     #     however it also appears to be caching with FIM at least partly?
-    #     how about have rerank send hotpath signal after all docs re-ranked?
-    #     if that fails, consider a threshold to clean up here?
+    #     sending signal hotpath in batch rerank queries now
+    #     consider a threshold to clean up here?
     #     OR set use_cache = False entirely for re-ranker
     #     torch.cuda.empty_cache()
 
