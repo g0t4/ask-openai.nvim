d625b9bc075e4026c6dc45d4e55c8c9b53f25c11
Wes Higbee <wes.mcclure@gmail.com>
2025-09-20 03:57:57 -0500

    rename

--- lua/ask-openai/rag/lsp/chunks/chunker_tests.py
+++ lua/ask-openai/rag/lsp/chunks/chunker_tests.py
@@ -20,7 +20,7 @@ set_root_dir(repo_root)
 # z rag
 # ptw lsp/chunker_tests.py -- --capture=tee-sys
 
-def _treesitter_chunks_from_file_with_fake_hash(path: Path, options: RAGChunkerOptions) -> list[Chunk]:
+def build_test_chunks(path: Path, options: RAGChunkerOptions) -> list[Chunk]:
     return build_ts_chunks_from_source_bytes(path, "fake_hash", path.read_bytes(), options)
 
 class TestReadingFilesAndNewLines:
@@ -123,7 +123,7 @@ class TestLowLevel_LinesChunker:
 class TestTreesitterPythonChunker:
 
     def test_two_functions_py(self):
-        chunks = _treesitter_chunks_from_file_with_fake_hash(test_cases_python / "two_functions.py", RAGChunkerOptions.OnlyTsChunks())
+        chunks = build_test_chunks(test_cases_python / "two_functions.py", RAGChunkerOptions.OnlyTsChunks())
         assert len(chunks) == 2
 
         first_chunk = chunks[0]
@@ -135,7 +135,7 @@ class TestTreesitterPythonChunker:
         assert second_chunk.text == expected_func2_chunk_text
 
     def test_nested_functions_py(self):
-        chunks = _treesitter_chunks_from_file_with_fake_hash(test_cases_python / "nested_functions.py", RAGChunkerOptions.OnlyTsChunks())
+        chunks = build_test_chunks(test_cases_python / "nested_functions.py", RAGChunkerOptions.OnlyTsChunks())
         assert len(chunks) == 2
 
         first_chunk = chunks[0]
@@ -149,7 +149,7 @@ class TestTreesitterPythonChunker:
         # and/or index nested too?
 
     def test_dataclass_py(self):
-        chunks = _treesitter_chunks_from_file_with_fake_hash(test_cases_python / "dataclass.py", RAGChunkerOptions.OnlyTsChunks())
+        chunks = build_test_chunks(test_cases_python / "dataclass.py", RAGChunkerOptions.OnlyTsChunks())
         assert len(chunks) == 1
 
         first_chunk = chunks[0]
@@ -160,7 +160,7 @@ class TestTreesitterPythonChunker:
         assert first_chunk.text == class_text
 
     def test_class_with_functions_py(self):
-        chunks = _treesitter_chunks_from_file_with_fake_hash(test_cases_python / "class_with_functions.py", RAGChunkerOptions.OnlyTsChunks())
+        chunks = build_test_chunks(test_cases_python / "class_with_functions.py", RAGChunkerOptions.OnlyTsChunks())
 
         assert len(chunks) == 5
         self.maxDiff = None
@@ -260,7 +260,7 @@ class TestTreesitterTypescriptChunker:
     # ptw lsp/chunks/chunker* -- --capture=tee-sys -k TestTreesitterTypescriptChunker
 
     def test_functions(self):
-        chunks = _treesitter_chunks_from_file_with_fake_hash(test_cases_typescript / "calc.ts", RAGChunkerOptions.OnlyTsChunks())
+        chunks = build_test_chunks(test_cases_typescript / "calc.ts", RAGChunkerOptions.OnlyTsChunks())
         assert len(chunks) == 4
 
         add_chunk = chunks[0]
@@ -297,7 +297,7 @@ class TestTreesitterTypescriptChunker:
         #       * do not want body in the signature!
         #       * works the same as function_definition in python
 
-        chunks = _treesitter_chunks_from_file_with_fake_hash(test_cases_typescript / "calc.ts", RAGChunkerOptions.OnlyTsChunks())
+        chunks = build_test_chunks(test_cases_typescript / "calc.ts", RAGChunkerOptions.OnlyTsChunks())
         add_chunk = chunks[0]
         assert add_chunk.signature == "function add(a: number, b: number): number"
 
