1751c3a385b6024f2e81e307e6c502203bfb9b70
Wes Higbee <wes.mcclure@gmail.com>
2025-09-21 18:59:37 -0500

    cleanup old notes

--- lua/ask-openai/rewrites/ExtmarksSet.lua
+++ lua/ask-openai/rewrites/ExtmarksSet.lua
@@ -4,13 +4,6 @@
 local ExtmarksSet = {}
 ExtmarksSet.__index = ExtmarksSet
 
--- * ExtmarksSet - a collection of extmarks, by Namespace!
---  maybe:
---   predition_marks = buffer:extmarks(prediction_ns)
---   prediction_marks:get(mark_id, {})
---   prediction_marks:set(mark_id, {})
---   prediction_marks:clear_all()
-
 ---@param buffer_number integer
 ---@param ns string
 ---@return ExtmarksSet
@@ -18,14 +11,6 @@ function ExtmarksSet:new(buffer_number, ns)
     self = setmetatable({}, ExtmarksSet)
     self.buffer_number = buffer_number
     self.namespace_id = vim.api.nvim_create_namespace(ns)
-
-    -- PRN mark_ids is tentative, not sure it needs stored in a list
-    --  instead, might have an Extmark class that encapsulates the mark_id
-    --  that consumers can hold a ref to and use to update it
-    -- self.mark_ids = {}
-    --  OR store Extmark objects in a list?
-    --  self.extmarks = {}
-
     return self
 end
 
@@ -47,13 +32,8 @@ end
 --- @param opts { start_line:integer, start_col:integer, ... } # nvim_buf_set_extmark options
 ---@return integer mark_id # useful if you didn't provide a mark_id and want the generated id
 function ExtmarksSet:set(mark_id, opts)
-    -- PRN how about create helpers for different types of marks that I commonly use?
-    --   then can make params for required args? and map to opts
-
-    -- PRN pass in opts too?
     opts.id = mark_id
 
-    -- TODO is it useful to pass start line/col in opts?
     local start_line = opts.start_line or 0
     local start_col = opts.start_col or 0
     opts.start_line = nil
@@ -70,9 +50,7 @@ function ExtmarksSet:highlight_lines(opts)
     assert(opts.id, 'must provide an id')
     assert(opts.hl_group, 'must provide an hl_group')
     assert(opts.start_line, 'must provide a start_line')
-    -- assert(opts.start_col, "must provide a start_col")
     assert(opts.end_line, 'must provide an end_line')
-    -- assert(opts.end_col, "must provide an end_col")
 
     local start_line = opts.start_line or 0
     local start_col = opts.start_col or 0
