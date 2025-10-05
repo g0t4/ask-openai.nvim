8bad93693a95bf6a9035512c54c9ed5a2c165d13
Wes Higbee <wes.mcclure@gmail.com>
2025-10-04 22:13:34 -0500

    renames

--- lua/ask-openai/backends/sse/stream/parser_tests.lua
+++ lua/ask-openai/backends/sse/stream/parser_tests.lua
@@ -16,10 +16,10 @@ describe("data-only events", function()
     end
 
     describe("single data value in single write", function()
-        local blank_line_markers = { "\n\n", "\r\n\r\n", "\r\r" }
-        for _, marker in ipairs(blank_line_markers) do
-            local write1 = 'data: data_value1' .. marker
-            describe(escape_newlines(marker), function()
+        local blank_line_types = { "\n\n", "\r\n\r\n", "\r\r" }
+        for _, blank_line in ipairs(blank_line_types) do
+            local write1 = 'data: data_value1' .. blank_line
+            describe(escape_newlines(blank_line), function()
                 before_each(function()
                     parser:write(write1)
                 end)
