2172e018bacfdd1fb723da855a80587b8ea43f66
Wes Higbee <wes.mcclure@gmail.com>
2025-10-23 03:45:57 -0500

    notes fixing venv tonight with torch==2.8.0 (2.9 no workee)

--- venv-fix-buil21.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-# uv pip install   --index-url https://download.pytorch.org/whl/cu128 torch torchvision torchaudio
-uv pip install flash-attn einops --no-build-isolation --no-deps
-
-# ***! NOW USE THIS:
-uv sync --extra build21
