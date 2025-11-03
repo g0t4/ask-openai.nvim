b3b1ab03a3eeef85aeecd8ce628a0ad57aa1aa32
Wes Higbee <wes.mcclure@gmail.com>
2025-11-03 09:26:33 -0600

    turn on logging during tests, temporarily

--- lua/ask-openai/rag/lsp/chunks/uncovered_tests.py
+++ lua/ask-openai/rag/lsp/chunks/uncovered_tests.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from tree_sitter_language_pack import get_parser
 
 from lsp.chunks.uncovered import _debug_uncovered_nodes
+from lsp.logs import logging_fwk_to_console
+
+logging_fwk_to_console("INFO")
 
 class TestUncoveredNodes():
 
