ec6e59b81ae72797c5265cefdedb66f7fbddbff6
Wes Higbee <wes.mcclure@gmail.com>
2025-11-03 09:13:34 -0600

    add tests of uncovered logger

---
+++ lua/ask-openai/rag/lsp/chunks/uncovered_tests.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from tree_sitter_language_pack import get_parser
+
+from lsp.chunks.uncovered import _debug_uncovered_nodes
+
+class TestUncoveredNodes():
+
+    def test_uncovered(self):
+        lua_parser = get_parser('lua')
+        tree = lua_parser.parse(bytes('function a() return 1 end', 'utf8'))
+        _debug_uncovered_nodes(tree, bytes('function a() return 1 end', 'utf8'), [], Path('foo.lua'))
+        assert 1 == 1
+
+    # TODO flesh out tests of returning the uncovered code
