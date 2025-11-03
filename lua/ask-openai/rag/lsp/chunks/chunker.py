975796c80f18f34173d53370ff118e8b9021e2d8
Wes Higbee <wes.mcclure@gmail.com>
2025-11-03 01:05:45 -0600

    extract variable

--- lua/ask-openai/rag/lsp/chunks/chunker.py
+++ lua/ask-openai/rag/lsp/chunks/chunker.py
@@ -269,7 +269,8 @@ def build_ts_chunks_from_source_bytes(path: Path, file_hash: str, source_bytes: 
 
         source_lines = source_bytes.splitlines()
 
-        uncovered_line_numbers = [line_number for line_number in range(len(source_lines)) if line_number not in covered_line_numbers]
+        all_line_numbers = range(len(source_lines))
+        uncovered_line_numbers = [line_number for line_number in all_line_numbers if line_number not in covered_line_numbers]
 
         if uncovered_line_numbers:
             logger_uncovered.debug("[bold on red] *********************** Uncovered lines *********************** [/]  ")
