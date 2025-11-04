4b6b80c217e2da7b65eb1df30f97b363f71207ba
Wes Higbee <wes.mcclure@gmail.com>
2025-11-04 02:40:43 -0600

    use __slots__

--- lua/ask-openai/rag/lsp/chunks/identified.py
+++ lua/ask-openai/rag/lsp/chunks/identified.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from tree_sitter import Node
 
-@dataclass
+@dataclass(slots=True)
 class IdentifiedChunk:
     # i.e. when primary has doc_comments/annotations/decorators before it, these are then siblings and there is not single node
     sibling_nodes: list[Node]
