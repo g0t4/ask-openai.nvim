5295392309d35ee501204f7819961464bdfccaf7
Wes Higbee <wes.mcclure@gmail.com>
2025-11-04 10:22:09 -0600

    move notes outside of decorator detector

--- lua/ask-openai/rag/lsp/chunks/ts/py.py
+++ lua/ask-openai/rag/lsp/chunks/ts/py.py
@@ -5,13 +5,6 @@ def attach_decorators(node, sibling_nodes: list) -> None:
         return
 
     # TODO any other constraints?
-    # FYI I feel like this should be some sort of explorer or visitor that can look back at nodes before a node and decide what to include and under what conditions?
-    #  and can I generalize this to more than just python decorators?
-    #  c# Attributes: [Serializable], [TestMethod], attached to members or classes, runtime accessible.
-    #  c++ Attributes: [[nodiscard]], [[deprecated]], attached to members or classes, runtime accessible
-    #      Doxygen comments: /// or /** ... */
-    #  rust Attributes: #[derive(Debug)], #[test], etc. - compiler metadata on functions, structs, modules.
-    #       Doc comments: /// or //!.
 
     sibling_nodes.insert(0, prev)
     attach_decorators(prev, sibling_nodes)
