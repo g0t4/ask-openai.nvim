2fb7ccc6e3db1b317a7550a465f7cf6678498adb
Wes Higbee <wes.mcclure@gmail.com>
2025-11-04 17:41:18 -0600

    simplify

--- lua/ask-openai/rag/lsp/chunks/uncovered.py
+++ lua/ask-openai/rag/lsp/chunks/uncovered.py
@@ -154,12 +154,7 @@ def _debug_uncovered_nodes(tree: Tree, source_bytes: bytes, chunks: list[Identif
         #  i.e. immediately obvious why we get leading and trailing \n in specific situations
         #  run the myriad of test cases in uncovered_tests and then look at the output w.r.t. this debug section
 
-        t_merged = [TroubleshootNode(
-            interval=m,
-            text=source_bytes[m.lower:m.upper].decode("utf-8", errors="replace"),
-            type="merged_covered",
-        ) for m in merged_covered_spans]
-
+        t_merged = [create_span_troubleshooter(source_bytes, m) for m in merged_covered_spans]
         t_uncovered = [code.troubleshoot_node() for code in uncovered_code]
 
         # troubleshoots = t_uncovered + t_covered # show unmerged covered spans
