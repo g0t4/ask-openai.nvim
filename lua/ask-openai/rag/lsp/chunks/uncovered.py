567bf9b65cc7a5fc290e397aa35b77af6a5d9368
Wes Higbee <wes.mcclure@gmail.com>
2025-11-04 17:23:45 -0600

    rename

--- lua/ask-openai/rag/lsp/chunks/uncovered.py
+++ lua/ask-openai/rag/lsp/chunks/uncovered.py
@@ -120,22 +120,22 @@ def _debug_uncovered_nodes(tree: Tree, source_bytes: bytes, chunks: list[Identif
         assert span.right == P.Bound.CLOSED
         # FYI logic below assumes open/closed (use assertions for now to ensure that reality)
         #  slice below treats end as not-inclusive, thus matches open/closed
-        start_base0: int = span.lower
-        end_base0: int = span.upper
+        start_byte_base0: int = span.lower
+        end_byte_base0: int = span.upper
 
-        text = source_bytes[start_base0:end_base0].decode("utf-8", errors="replace")
+        text = source_bytes[start_byte_base0:end_byte_base0].decode("utf-8", errors="replace")
 
         # FYI I am not computing column offsets, for uncovered code purposes I think that's fine for now b/c...
         # - this is only going to be for sliding window "fallback" chunker which is 100% fine to cover a smidge extra
         # - I might even cover X lines around window too so columns on the start/end line don't matter
-        start_line_base1 = source_bytes[:start_base0].count(b"\n") + 1
+        start_line_base1 = source_bytes[:start_byte_base0].count(b"\n") + 1
         end_line_base1 = start_line_base1 + text.count("\n")
         code = UncoveredCode(
             text=text,
             start_line_base1=start_line_base1,
             end_line_base1=end_line_base1,
-            start_byte_base0=start_base0,
-            end_byte_base0=end_base0,
+            start_byte_base0=start_byte_base0,
+            end_byte_base0=end_byte_base0,
         )
         uncovered_code.append(code)
 
