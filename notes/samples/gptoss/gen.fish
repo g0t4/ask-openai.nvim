4268e32df75d9295e0042388dd63d96256340227
Wes Higbee <wes.mcclure@gmail.com>
2025-11-07 03:47:58 -0600

    fix missing <|end|>

--- notes/samples/gptoss/gen.fish
+++ notes/samples/gptoss/gen.fish
@@ -26,8 +26,6 @@ echo '{
   "max_tokens": 80,
   "stream": false
 }' | curl --fail-with-body -sSL --no-buffer http://build21:8013/apply-template -d @- \
-    | string replace --regex "^data: (\[DONE\])*" "" \
-    # null entries for records w/o choices[0].delta.content
     | jq > test.apply-template.json
 
 # %%
