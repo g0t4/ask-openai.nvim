7e7262d67931247c8f1a377011c6852107d4315c
Wes Higbee <wes.mcclure@gmail.com>
2025-11-07 03:31:33 -0600

    use /apply-template in llama-server to see how a message history renders to a prompt (in this case harmony for gptoss)

--- notes/samples/gptoss/gen.fish
+++ notes/samples/gptoss/gen.fish
@@ -17,3 +17,22 @@ echo '{
     | string replace --regex "^data: (\[DONE\])*" "" \
     # null entries for records w/o choices[0].delta.content
     | jq --compact-output >test.full.json
+
+# %%
+
+# render chat template to raw prompt
+echo '{
+  "messages": [ { "role": "user", "content": "test" } ],
+  "max_tokens": 80,
+  "stream": false
+}' | curl --fail-with-body -sSL --no-buffer http://build21:8013/apply-template -d @- \
+    | string replace --regex "^data: (\[DONE\])*" "" \
+    # null entries for records w/o choices[0].delta.content
+    | jq > test.apply-template.json
+
+# %%
+
+# send the rendered prompt
+
+curl --fail-with-body -sSL --no-buffer http://build21:8013/v1/completions \
+    -d @test.apply-template.json | jq
