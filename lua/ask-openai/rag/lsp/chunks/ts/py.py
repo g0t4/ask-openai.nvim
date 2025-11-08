bc6e66a2b64cc6578ce07a821c8af9579bb3bb5c
Wes Higbee <wes.mcclure@gmail.com>
2025-11-07 21:50:05 -0600

    remove tail recursion

--- lua/ask-openai/rag/lsp/chunks/ts/py.py
+++ lua/ask-openai/rag/lsp/chunks/ts/py.py
@@ -1,10 +1,9 @@
 def attach_decorators(node, accumulated_decorators: list) -> None:
-    prev = node.prev_sibling
-    prev_is_decorator = prev and prev.type == "decorator"
-    if not prev_is_decorator:
-        return
+    while True:
+        prev = node.prev_sibling
+        prev_is_decorator = prev and prev.type == "decorator"
+        if not prev_is_decorator:
+            return
 
-    # TODO any other constraints?
-
-    accumulated_decorators.insert(0, prev)
-    attach_decorators(prev, accumulated_decorators)
+        accumulated_decorators.insert(0, prev)
+        node = prev
