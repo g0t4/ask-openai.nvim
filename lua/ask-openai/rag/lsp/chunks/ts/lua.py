7d45e9495539370ec9cd2dad60b0c262325de4c9
Wes Higbee <wes.mcclure@gmail.com>
2025-11-07 21:20:48 -0600

    review

--- lua/ask-openai/rag/lsp/chunks/ts/lua.py
+++ lua/ask-openai/rag/lsp/chunks/ts/lua.py
@@ -3,26 +3,19 @@ def attach_doc_comments(node, sibling_nodes: list) -> None:
     prev_is_doc_comment = prev and prev.type == "comment"
     if not prev_is_doc_comment:
         return
-    # TODO ensure not blank line between (comment's end_point will have line # that is 2 less than node's start_point=>line)
 
-        # Ensure there is no blank line between the comment and the node
-    # The comment's end line should be exactly one line before the node's start line
+    # * Ensure no blank line between the comment and the node
     comment_end_line = prev.end_point[0]
     node_start_line = node.start_point[0]
     if comment_end_line != node_start_line - 1:
         return
-    # # Check for any other non-comment nodes between the comment and the current node
-    # # If there are any such nodes, the comment should not be attached
-    # sibling = prev.prev_sibling
-    # while sibling:
-    #     # If we encounter a non-whitespace, non-comment node before reaching a blank line, abort
-    #     if sibling.type not in ("comment", "blank"):
-    #         return
-    #     # Stop if we encounter a blank line (i.e., a node that represents a line break)
-    #     if sibling.type == "blank":
-    #         return
-    #     sibling = sibling.prev_sibling
-    # # If we made it here
 
     sibling_nodes.insert(0, prev)
+
+    # TODO remove tail recursion using smth like what gptoss120b suggested:
+    # sibling = prev.prev_sibling
+    # while sibling:
+    #     if sibling.type not in ("comment"):
+    #         return
+    #     sibling = sibling.prev_sibling
     attach_doc_comments(prev, sibling_nodes)
