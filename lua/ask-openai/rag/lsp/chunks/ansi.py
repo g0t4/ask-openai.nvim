55196aba28e5ab69f629893c5e62516a17a4e45c
Wes Higbee <wes.mcclure@gmail.com>
2025-11-12 09:25:25 -0600

    split out ansi constants for reuse

---
+++ lua/ask-openai/rag/lsp/chunks/ansi.py
@@ -0,0 +1,24 @@
+# * ANSI colors
+# foreground:
+BLACK = "\x1b[30m"
+RED = "\x1b[31m"
+GREEN = "\x1b[32m"
+YELLOW = "\x1b[33m"
+BLUE = "\x1b[34m"
+MAGENTA = "\x1b[35m"
+CYAN = "\x1b[36m"
+WHITE = "\x1b[37m"
+# background:
+BLACKBG = "\x1b[40m"
+REDBG = "\x1b[41m"
+GREENBG = "\x1b[42m"
+YELLOWBG = "\x1b[43m"
+BLUEBG = "\x1b[44m"
+MAGENTABG = "\x1b[45m"
+CYANBG = "\x1b[46m"
+WHITEBG = "\x1b[47m"
+#
+RESET = "\x1b[0m"
+BOLD = "\x1b[1m"
+ITALIC = "\x1b[3m"
+UNDERLINE = "\x1b[4m"
--- lua/ask-openai/rag/lsp/chunks/uncovered.py
+++ lua/ask-openai/rag/lsp/chunks/uncovered.py
@@ -9,34 +9,10 @@ from tree_sitter import Tree
 from lsp.chunks.identified import IdentifiedChunk
 from lsp.logs import get_logger
 from io import StringIO
+from lsp.chunks.ansi import *
 
 logger_uncovered = get_logger(__name__)
 # logger_uncovered.setLevel(logging.DEBUG)
-
-# * ANSI colors
-# foreground:
-BLACK = "\x1b[30m"
-RED = "\x1b[31m"
-GREEN = "\x1b[32m"
-YELLOW = "\x1b[33m"
-BLUE = "\x1b[34m"
-MAGENTA = "\x1b[35m"
-CYAN = "\x1b[36m"
-WHITE = "\x1b[37m"
-# background:
-BLACKBG = "\x1b[40m"
-REDBG = "\x1b[41m"
-GREENBG = "\x1b[42m"
-YELLOWBG = "\x1b[43m"
-BLUEBG = "\x1b[44m"
-MAGENTABG = "\x1b[45m"
-CYANBG = "\x1b[46m"
-WHITEBG = "\x1b[47m"
-#
-RESET = "\x1b[0m"
-BOLD = "\x1b[1m"
-ITALIC = "\x1b[3m"
-UNDERLINE = "\x1b[4m"
 
 class TroubleshootCodeInterval(NamedTuple):
     interval: P.Interval
