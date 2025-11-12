6c14e622f3da6847f08786773fdf98da61ddde9f
Wes Higbee <wes.mcclure@gmail.com>
2025-11-12 12:28:45 -0600

    typescript decorators in chunking

--- lua/ask-openai/rag/tests/test_cases/treesitter/typescript/decorators.ts
+++ lua/ask-openai/rag/tests/test_cases/treesitter/typescript/decorators.ts
@@ -1,10 +1,18 @@
-//
-// TODO like with python we have decorators
+function sealed(constructor: Function) {
+    Object.seal(constructor);
+    Object.seal(constructor.prototype);
+};
+
+function log(target: any) {};
+
+@sealed
+class SimpleTest {
+    @log
+    greet(name: string): string {
+        return `Hello, ${name}`;
+    };
+};
+
+
 // decorators cannot be put on:
 //    enums, type aliases, interfaces
-// TODO class
-// TODO functions? (proposed?)
-
-// TODO like with lua, we also have docstrings (preceding a member)
-// TODO scenarios?
-
