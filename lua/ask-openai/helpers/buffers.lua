3c19f0a532c0fe007170e4921df6ed5d2d6b6663
Wes Higbee <wes.mcclure@gmail.com>
2025-11-14 09:30:22 -0600

    move to helpers I already had that it noted!

--- lua/ask-openai/helpers/buffers.lua
+++ lua/ask-openai/helpers/buffers.lua
@@ -7,6 +7,20 @@ function M.get_current_buffer_entire_text()
     return table.concat(lines, "\n")
 end
 
+---@return table<string, string>
+function M.all()
+    local result = {}
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_option(bufnr, "buflisted") then
+            local name = vim.api.nvim_buf_get_name(bufnr)
+            if name == "" then name = tostring(bufnr) end
+            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+            result[name] = table.concat(lines, "\n")
+        end
+    end
+    return result
+end
+
 function M.dump_last_seletion()
     local selection = Selection.get_visual_selection_for_current_window()
     print(vim.inspect(selection))
