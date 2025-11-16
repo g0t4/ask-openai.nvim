74d5d84b81b8180b1b3d835f4670bbee45023fd2
Wes Higbee <wes.mcclure@gmail.com>
2025-11-16 02:03:27 -0600

    good scenarios to test

--- lua/ask-openai/backends/models/gptoss/parse_tests.lua
+++ lua/ask-openai/backends/models/gptoss/parse_tests.lua
@@ -1,5 +1,3 @@
-
-
 -- add tests for each interesting example in the cookbook:
 --   https://cookbook.openai.com/articles/openai-harmony
 --   FOCUS on model response scenarios, for parsing (model output side)...
@@ -9,3 +7,55 @@
 --  {"properties":{"items":{"type":"array","description":"entries on the shopping list","items":{"type":"string"}}},"type":"object"}<|end|><|start|>user<|message|>I need to buy coffee, soda and eggs<|end|><|start|>assistant
 
 
+-- TODO write a streaming SSE parser first.. can slice up the following full example into SSE like style
+--   keep in mind key tokens are atomic (not split across SSE boundaries b/c only one token each: <|start|>,<|end|>,etc
+
+-- * GOOD practice for parsing a series!
+-- end_to_end is from: https://cookbook.openai.com/articles/openai-harmony#system-message-format
+end_to_end = [[
+<|start|>system<|message|>You are ChatGPT, a large language model trained by OpenAI.
+Knowledge cutoff: 2024-06
+Current date: 2025-06-28
+
+Reasoning: high
+
+# Valid channels: analysis, commentary, final. Channel must be included for every message.
+Calls to these tools must go to the commentary channel: 'functions'.<|end|><|start|>developer<|message|># Instructions
+
+Use a friendly tone.
+
+# Tools
+
+## functions
+
+namespace functions {
+
+// Gets the location of the user.
+type get_location = () => any;
+
+// Gets the current weather in the provided location.
+type get_current_weather = (_: {
+// The city and state, e.g. San Francisco, CA
+location: string,
+format?: "celsius" | "fahrenheit", // default: celsius
+}) => any;
+
+// Gets the current weather in the provided list of locations.
+type get_multiple_weathers = (_: {
+// List of city and state, e.g. ["San Francisco, CA", "New York, NY"]
+locations: string[],
+format?: "celsius" | "fahrenheit", // default: celsius
+}) => any;
+
+} // namespace functions<|end|><|start|>user<|message|>What is the weather like in SF?<|end|><|start|>assistant<|channel|>analysis<|message|>Need to use function get_current_weather.<|end|><|start|>assistant<|channel|>commentary to=functions.get_current_weather <|constrain|>json<|message|>{"location":"San Francisco"}<|call|><|start|>functions.get_current_weather to=assistant<|channel|>commentary<|message|>{"sunny": true, "temperature": 20}<|end|><|start|>assistant
+]]
+
+
+preamble_example = [[
+<|channel|>analysis<|message|>{long chain of thought}<|end|><|start|>assistant<|channel|>commentary<|message|>**Action plan**:
+1. Generate an HTML file
+2. Generate a JavaScript for the Node.js server
+3. Start the server
+---
+Will start executing the plan step by step<|end|><|start|>assistant<|channel|>commentary to=functions.generate_file<|constrain|>json<|message|>{"template": "basic_html", "path": "index.html"}<|call|>
+]]
