e77157a982388bfb5cb17f8309067348a841de89
Wes Higbee <wes.mcclure@gmail.com>
2025-11-17 00:41:05 -0600

    rename

--- lua/ask-openai/questions/ask.lua
+++ lua/ask-openai/questions/ask.lua
@@ -424,11 +424,11 @@ function M.curl_exited_successfully()
         end
         M.clear_undos()
 
-        M.call_tools()
+        M.call_requested_tools_for_the_model()
     end)
 end
 
-function M.call_tools()
+function M.call_requested_tools_for_the_model()
     for _, message in ipairs(M.thread.last_request.response_messages or {}) do
         for _, tool_call in ipairs(message.tool_calls) do
             -- log:jsonify_compact_trace("tool:", tool_call)
