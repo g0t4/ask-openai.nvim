85646020b8ebd31d8dc018d76f969f15cabb77ab
Wes Higbee <wes.mcclure@gmail.com>
2025-11-18 09:51:53 -0600

    try splitting apart accumulated from chat message to specialize each?

---
+++ lua/ask-openai/questions/chat/accumulated.lua
@@ -0,0 +1,139 @@
+local log = require('ask-openai.logs.logger').predictions()
+local ansi = require('ask-openai.prediction.ansi')
+
+---@class AccumulatedMessage
+---@field role? string
+---@field index integer -- must be kept and sent back with thread
+---@field content? string
+---@field _verbatim_content? string -- hack for  <tool_call>... leaks (can be removed if fixed)
+---@field reasoning_content? string
+---@field finish_reason? string|vim.NIL -- FYI use get_finish_reason() for clean value (vim.NIL => nil)
+---@field tool_call_id? string
+---@field name? string
+---@field tool_calls ToolCall[] -- empty if none
+local AccumulatedMessage = {}
+
+--- FYI largely a marker interface as well, don't need to actually use this ctor
+---@return ChatMessage
+function AccumulatedMessage:new(role, content)
+    self = setmetatable({}, { __index = AccumulatedMessage })
+    self.role = role
+    self.content = content
+    self.finish_reason = nil
+    self.tool_calls = {} -- empty == None (enforce invariant)
+    return self
+end
+
+function AccumulatedMessage:new_tool_response(call_result_object_not_json, tool_call_id, name)
+    -- FYI see NOTES.md for "fix" => removed `|tojson` from jinja template for message.content
+    self = AccumulatedMessage:new("tool", vim.json.encode(call_result_object_not_json))
+
+    self.tool_call_id = tool_call_id
+    self.name = name
+    return self
+end
+
+function AccumulatedMessage:user(content)
+    return AccumulatedMessage:new("user", content)
+end
+
+function AccumulatedMessage:assistant(content)
+    return AccumulatedMessage:new("assistant", content)
+end
+
+function AccumulatedMessage:new_system_message(content)
+    return AccumulatedMessage:new("system", content)
+end
+
+function AccumulatedMessage:add_tool_call_requests(call_request)
+    -- ONLY clone fields on the original call request from the model
+    local new_call = {
+        id = call_request.id,
+        index = call_request.index,
+        type = call_request.type,
+        ["function"] = {
+            name = call_request["function"].name,
+            arguments = call_request["function"].arguments,
+        }
+    }
+    table.insert(self.tool_calls, new_call)
+end
+
+---@return string
+function AccumulatedMessage:dump_text()
+    local lines = {
+        ansi.white_bold(self.role .. ":") .. " " .. tostring(self.content or ""),
+    }
+    -- include fields not explicitly in the template above
+    for key, v in pairs(self) do
+        if key ~= "__index" and key ~= "role" and key ~= "content" then
+            local color_key = ansi.yellow(key)
+            local line = string.format("%s: %s", color_key, vim.inspect(v))
+            table.insert(lines, line)
+        end
+    end
+    return table.concat(lines, "\n")
+end
+
+---@enum FINISH_REASONS
+AccumulatedMessage.FINISH_REASONS = {
+    LENGTH = "length",
+    STOP = "stop",
+    TOOL_CALLS = "tool_calls",
+    -- observed finish_reason values: "tool_calls", "stop", "length", null (not string, a literal null JSON value)
+    -- vim.NIL (still streaming) => b/c of JSON value of null (not string, but literal null in the JSON)
+
+    -- FYI find finish_reason observed values:
+    --   grep --no-filename -o '"finish_reason":[^,}]*' **/* 2>/dev/null | sort | uniq
+    -- "finish_reason":"length"
+    -- "finish_reason":"stop"
+    -- "finish_reason":"tool_calls"
+    -- "finish_reason":null
+}
+
+---Returns the finish reason, cleanup when not set (i.e. nil instead of vim.NIL)
+---@return FINISH_REASONS?
+function AccumulatedMessage:get_finish_reason()
+    if self.finish_reason == vim.NIL then
+        return nil
+    end
+    return self.finish_reason
+end
+
+---@return boolean
+function AccumulatedMessage:is_still_streaming()
+    return self.finish_reason == nil or self.finish_reason == vim.NIL
+end
+
+AccumulatedMessage.LIFECYCLE = {
+    -- FYI I merged two concepts: message from model + managing requested tool_call object(s)
+    -- streaming -> rx finish_reason=stop/length -> finished
+    -- streaming -> rx finish_reason=tool_calls -> pending_tool_call -> calling -> rx results -> finished (tool call done)
+
+    STREAMING = "streaming", -- server is sending message (streaming SSEs)
+    FINISHED = "finished", -- server is done sending the message
+
+    -- * tool call related
+    -- PENDING_TOOL_CALL = "pending_tool_call", -- next the client will call the tool (add this only IF NEEDED)
+    TOOL_CALLING = "tool_calling", -- client is calling the tool, waiting for it to complete
+    TOOLS_DONE = "tool_called", -- tool finished (next message will send results to server for a new "TURN" in chat history)
+}
+
+function AccumulatedMessage:get_lifecycle_step()
+    -- TODO try using this to simplify consumer logic... i.e. in streaming chat window  message/tool formatters/summarizers
+    if self:is_still_streaming() then
+        return AccumulatedMessage.LIFECYCLE.STREAMING
+    end
+    local finish_reason = self:get_finish_reason()
+    if finish_reason == AccumulatedMessage.FINISH_REASONS.TOOL_CALLS then
+        -- IIRC tool_calls are parsed before FINISHED state... so just check all are complete (or not)
+        for _, call in ipairs(self.tool_calls) do
+            if not call:is_done() then
+                return AccumulatedMessage.LIFECYCLE.TOOL_CALLING
+            end
+        end
+    end
+    return AccumulatedMessage.LIFECYCLE.FINISHED
+end
+
+return AccumulatedMessage
