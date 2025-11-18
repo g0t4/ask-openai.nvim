6d082d2a21fd7c00d6f6e23b6566b3ac2007cb34
Wes Higbee <wes.mcclure@gmail.com>
2025-11-18 17:57:08 -0600

    b/c I like punishing myself :)

--- lua/ask-openai/backends/openai_schema.lua
+++ lua/ask-openai/backends/openai_schema.lua
@@ -77,27 +77,27 @@
 
 --- * Message Types (by Role)
 
----@class OpenAIChatCompletion_Input_SystemMessage
+---@class OpenAIChatCompletion_Input_BaseMessage
+---@field role string
+---@field content OpenAIChatCompletion_Input_MessageContent - btw role="user" has a few extra item times in table[]
+
+---@class OpenAIChatCompletion_Input_BaseNamedMessage
+---@field name? string - NOT using (so far)
+
+---@class OpenAIChatCompletion_Input_SystemMessage : OpenAIChatCompletion_Input_BaseNamedMessage
 ---@field role string - "system"
----@field content OpenAIChatCompletion_Input_MessageContent
----@field name? string - NOT using (so far)
 
----@class OpenAIChatCompletion_Input_DeveloperMessage
+---@class OpenAIChatCompletion_Input_DeveloperMessage : OpenAIChatCompletion_Input_BaseNamedMessage
 ---@field role string - "developer"
----@field content OpenAIChatCompletion_Input_MessageContent
----@field name? string - NOT using (so far)
 
----@class OpenAIChatCompletion_Input_UserMessage
+---@class OpenAIChatCompletion_Input_UserMessage : OpenAIChatCompletion_Input_BaseNamedMessage
 ---@field role string - "user"
----@field content OpenAIChatCompletion_Input_MessageContent - FTR has a few extra item types if using table[] which I am not using
----@field name? string - NOT using (so far)
 
 --- For returning tool call results to the model (assistant)
----@class OpenAIChatCompletion_Input_ToolResultMessage
+---  FTR no name field
+---@class OpenAIChatCompletion_Input_ToolResultMessage : OpenAIChatCompletion_Input_BaseMessage
 ---@field role string - "tool"
----@field content OpenAIChatCompletion_Input_MessageContent
 ---@field tool_call_id string
-
 
 --- I'm ONLY USING string
 --- I am not using table[] of {type,text|refusal} - all message roles have these two (and then user role has a few more item types)
@@ -107,11 +107,9 @@
 
 -- * Assistant Message Type (has extras like tool calls)
 
----@class OpenAIChatCompletion_Input_AssistantMessage
+---@class OpenAIChatCompletion_Input_AssistantMessage : OpenAIChatCompletion_Input_BaseNamedMessage
 ---@field role string - "assistant"
----@field content OpenAIChatCompletion_Input_MessageContent
 ---@field tool_calls OpenAIChatCompletion_Input_AssistantMessageToolCallRequest[]
----@field name? string - NOT using (so far)
 ---
 ---@field audio any - NOT using
 ---@field refusal? string - NOT using
