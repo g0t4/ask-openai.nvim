ba538da8b0fa9c03d9ac1ecf27c4dbd78a367c1d
Wes Higbee <wes.mcclure@gmail.com>
2025-11-18 10:30:45 -0600

    holy crap how did I miss this!? I fucked up the system prompt... which means it was never being added to gptoss (and others) wow... for gptoss that meant these instructions never made it to the developer message, not that they are critical (yet) but still wow wes

--- lua/ask-openai/questions/ask.lua
+++ lua/ask-openai/questions/ask.lua
@@ -98,7 +98,7 @@ The semantic_grep tool:
 
     ---@type ChatMessage[]
     local messages = {
-        ChatMessage:new("system prompt", system_prompt),
+        ChatMessage:new("system", system_prompt),
     }
 
     -- TODO add this back and optional RAG?
