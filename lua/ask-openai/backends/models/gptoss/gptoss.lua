e118a83ecf1146877d0b05df08a488aae436dfc2
Wes Higbee <wes.mcclure@gmail.com>
2025-11-19 10:23:54 -0600

    rename

--- lua/ask-openai/backends/models/gptoss/gptoss.lua
+++ lua/ask-openai/backends/models/gptoss/gptoss.lua
@@ -1,7 +1,16 @@
 --
+-- TODO LATER move this and the TxChatMessage model out of questions, it can and is used everywhere
+
+--
 -- TODO ?? decouple TxChatMessage from rendering and parsing (create a seam)
 --  * add gptoss raw renderer/parser
 --    you already have non-thinking renderer
 --    and parser is pretty much one message at a time (not that complex)
 --    well tested if you go this route
 --  * keep a generic messages renderer/parser
+
+
+
+
+
+
