ccb103291d8d4839f986f95477cb4b1670ab54e0
Wes Higbee <wes.mcclure@gmail.com>
2025-11-20 01:29:07 -0600

    spike idea for cursor controller - very crude idea! I will definitely be rearranging/refactoring this if I keep it

---
+++ lua/ask-openai/prediction/cursor_controller.lua
@@ -0,0 +1,48 @@
+---@class CursorController
+---@field window_id integer
+local CursorController = {}
+CursorController.__index = CursorController
+
+---@param window_id integer? # defaults to current window (0)
+---@return CursorController
+function CursorController:new(window_id)
+    local self = setmetatable({}, CursorController)
+    self.window_id = window_id or 0
+    return self
+end
+
+---Calculate new cursor position after inserting `text` at the current cursor.
+---Handles multiline text and returns both 0- and 1-indexed coordinates.
+---@param inserted_lines string[]
+---@param cursor CursorInfo
+---@return CursorInfo
+function CursorController:calc_new_position(cursor, inserted_lines)
+    local new_line_base0 = cursor.line_base0
+    local new_col_base0 = cursor.col_base0
+
+    if #inserted_lines == 1 then
+        new_col_base0 = cursor.col_base0 + #inserted_lines[1]
+    else
+        -- minus 1 because first line is cursor line (IOTW already moved to it)
+        move_down_lines = #inserted_lines - 1
+        new_line_base0 = cursor.line_base0 + move_down_lines
+
+        new_col_base0 = #inserted_lines[#inserted_lines] -- to last inserted character on the last line
+    end
+
+    return {
+        line_base0 = new_line_base0,
+        line_base1 = new_line_base0 + 1,
+        col_base0 = new_col_base0,
+        col_base1 = new_col_base0 + 1,
+    }
+end
+
+---@param cursor CursorInfo
+---@param inserted_lines string[]
+function CursorController:move_cursor_after_insert(cursor, inserted_lines)
+    local new_cursor = self:calc_new_position(cursor, inserted_lines)
+    vim.api.nvim_win_set_cursor(self.window_id, { new_cursor.line_base1, new_cursor.col_base0 }) -- (1,0)-indexed
+end
+
+return CursorController
