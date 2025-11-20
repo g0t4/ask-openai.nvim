7e00c0262129ca68780d7f46ceee06bc50b65dad
Wes Higbee <wes.mcclure@gmail.com>
2025-11-20 05:22:05 -0600

    rearrange

--- lua/ask-openai/prediction/cursor_controller.lua
+++ lua/ask-openai/prediction/cursor_controller.lua
@@ -19,12 +19,14 @@ end
 function CursorController:calc_new_position(cursor, inserted_lines)
     local new_line_base0, new_col_base0
     if #inserted_lines == 1 then
-        new_col_base0 = cursor.col_base0 + #inserted_lines[1]
         new_line_base0 = cursor.line_base0
+
+        new_col_base0 = cursor.col_base0 + #inserted_lines[1]
     else
         -- minus 1 because first line is cursor line (IOTW already moved to it)
         move_down_lines = #inserted_lines - 1
         new_line_base0 = cursor.line_base0 + move_down_lines
+
         new_col_base0 = #inserted_lines[#inserted_lines] -- to last inserted character on the last line
     end
 
