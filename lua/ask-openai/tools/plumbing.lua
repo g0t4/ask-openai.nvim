3bb31fa55e2f17ee9e20bad680a5ecee8aa7af85
Wes Higbee <wes.mcclure@gmail.com>
2025-11-20 14:03:32 -0600

    notes

--- lua/ask-openai/tools/plumbing.lua
+++ lua/ask-openai/tools/plumbing.lua
@@ -7,6 +7,8 @@ local M = {}
 function M.create_tool_call_output_failure(description)
     log:error("tool_call plumbing failure: " .. description)
 
+    -- TODO review all of TOOLs pipeline for other spots to add this
+
     return {
         result = {
             isError = true,
@@ -19,6 +21,10 @@ function M.create_tool_call_output_failure(description)
             },
         },
     }
+
+    -- https://modelcontextprotocol.io/specification/2025-06-18/server/tools#error-handling
+    --   could use a "protocol error" though I'd have to patch the "error" through to the model
+    --   as long as the model gets the message, it doesn't really matter the format
 end
 
 return M
