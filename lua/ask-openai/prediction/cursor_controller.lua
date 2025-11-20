b9f1cbff57a016a51977965c4edcb0ceb10e45f2
Wes Higbee <wes.mcclure@gmail.com>
2025-11-20 05:10:11 -0600

    review

--- lua/ask-openai/prediction/cursor_controller.lua
+++ lua/ask-openai/prediction/cursor_controller.lua
@@ -17,17 +17,14 @@ end
 ---@param cursor CursorInfo
 ---@return CursorInfo
 function CursorController:calc_new_position(cursor, inserted_lines)
-    -- TODO I am itching to refactor setting these up here and then overriding in all but one case!
-    local new_line_base0 = cursor.line_base0
-    local new_col_base0 = cursor.col_base0
-
+    local new_line_base0, new_col_base0
     if #inserted_lines == 1 then
         new_col_base0 = cursor.col_base0 + #inserted_lines[1]
+        new_line_base0 = cursor.line_base0
     else
         -- minus 1 because first line is cursor line (IOTW already moved to it)
         move_down_lines = #inserted_lines - 1
         new_line_base0 = cursor.line_base0 + move_down_lines
-
         new_col_base0 = #inserted_lines[#inserted_lines] -- to last inserted character on the last line
     end
 
