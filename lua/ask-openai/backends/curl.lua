4ee6aaee9a3d9a0ac0d13874dbdab537ff27ccc4
Wes Higbee <wes.mcclure@gmail.com>
2025-11-21 03:05:37 -0600

    return early

--- lua/ask-openai/backends/curl.lua
+++ lua/ask-openai/backends/curl.lua
@@ -26,40 +26,41 @@ function get_extract_func(endpoint)
     --   3rd ExtractGeneratedTextFunction for non-openai /completions endpoint on llama-server
     --     => no sse.choice so I'd have to change how M.on_line_or_lines works to not assume sse.choices
     --     whereas with non-openai /completions it would just use top-level to get text (.content)
-
-    ---@type ExtractGeneratedTextFunction
-    local function extract_generated_text_v1_completions(choice)
-        --- * /v1/completions
-        if choice == nil or choice.text == nil then
-            -- just skip if no (first) choice or no text on it (i.e. last SSE is often timing only)
-            return ""
+    if endpoint == CompletionsEndpoints.v1_completions then
+        ---@type ExtractGeneratedTextFunction
+        return function(choice)
+            --- * /v1/completions
+            if choice == nil or choice.text == nil then
+                -- just skip if no (first) choice or no text on it (i.e. last SSE is often timing only)
+                return ""
+            end
+            return choice.text
         end
-        return choice.text
-    end
-
-    ---@type ExtractGeneratedTextFunction
-    local function extract_generated_text_v1_chat(choice)
-        --- * /v1/chat/completions
-        -- NOW I have access to request (url, body.model, etc) to be able to dynamically swap in the right SSE parser!
-        --   I could even add another function that would handle aggregating and transforming the raw response (i.e. for harmony) into aggregate views (i.e. of thinking and final responses), also trigger events that way
-        if choice == nil
-            or choice.delta == nil
-            or choice.delta.content == nil
-            or choice.delta.content == vim.NIL
-        then
-            return ""
-        end
-        return choice.delta.content
     end
 
     if endpoint == CompletionsEndpoints.v1_chat then
-        return extract_generated_text_v1_chat
-    elseif endpoint == CompletionsEndpoints.v1_completions then
-        return extract_generated_text_v1_completions
-    else
-        -- TODO CompletionsEndpoints.completions /completions for 3rd ExtractGeneratedTextFunction
-        error("Not yet implemented: " .. endpoint)
+        ---@type ExtractGeneratedTextFunction
+        return function(choice)
+            --- * /v1/chat/completions
+            -- NOW I have access to request (url, body.model, etc) to be able to dynamically swap in the right SSE parser!
+            --   I could even add another function that would handle aggregating and transforming the raw response (i.e. for harmony) into aggregate views (i.e. of thinking and final responses), also trigger events that way
+            if choice == nil
+                or choice.delta == nil
+                or choice.delta.content == nil
+                or choice.delta.content == vim.NIL
+            then
+                return ""
+            end
+            return choice.delta.content
+        end
     end
+
+    if endpoint == CompletionsEndpoints.completions then
+        error("TODO /completions endpoint's ExtractGeneratedTextFunction")
+    end
+
+    -- TODO CompletionsEndpoints.completions /completions for 3rd ExtractGeneratedTextFunction
+    error("Not yet implemented: " .. endpoint)
 end
 
 ---@class StreamingFrontend
