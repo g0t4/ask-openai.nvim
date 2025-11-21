a9189c90dcb1c1a01d26b9570beccfbee035d443
Wes Higbee <wes.mcclure@gmail.com>
2025-11-21 03:07:40 -0600

    local

--- lua/ask-openai/backends/curl.lua
+++ lua/ask-openai/backends/curl.lua
@@ -21,7 +21,7 @@ _G.CompletionsEndpoints = {
     v1_chat = "/v1/chat/completions",
 }
 ---@param endpoint CompletionsEndpoints
-function get_extract_generated_text_func(endpoint)
+local function get_extract_generated_text_func(endpoint)
     -- * /completions  CompletionsEndpoints.completions
     --   3rd ExtractGeneratedTextFunction for non-openai /completions endpoint on llama-server
     --     => no sse.choice so I'd have to change how M.on_line_or_lines works to not assume sse.choices
