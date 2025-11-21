431db8ac2241a3cf591a68cc88319bd2f2a980af
Wes Higbee <wes.mcclure@gmail.com>
2025-11-21 01:19:07 -0600

    more examples of llama-server completion vs /v1/completions (vs obviously /v1/chat/completions)

--- notes/samples/gptoss/gen.fish
+++ notes/samples/gptoss/gen.fish
@@ -34,17 +34,103 @@ echo '{
 
 cat test.apply-template.json | jq .prompt -r # show just the prompt
 
-# %% * raw prompt => /v1/completions
-
+# %% * raw prompt => /completions (NOT OpenAI compat)
 # send the rendered prompt
-curl --fail-with-body -sSL --no-buffer "$base_url/v1/completions" \
-    -d @test.apply-template.json | jq >test.raw.json
 
-# %%
+mkdir -p completions
 
+# FYI /completions == /completion
+curl --fail-with-body -sSL --no-buffer "$base_url/completions" \
+    -d @test.apply-template.json | jq > completions/raw.json
+
+# modify
+cat test.apply-template.json | jq 'add(.stream=true)'
+
+# streaming => flat SSEs => .content,.tokens,.stop (.index,.tokens_predicted,.tokens_evaluated)
+cat test.apply-template.json | jq 'add(.stream=true)' | \
+    curl --fail-with-body -sSL --no-buffer "$base_url/completions" -d @- \
+    | string replace --regex  "^data: " "" | jq >completions/raw.stream.json
+# {
+#   "index": 0,
+#   "content": "5",
+#   "tokens": [
+#     20
+#   ],
+#   "stop": false,
+#   "id_slot": -1,
+#   "tokens_predicted": 71,
+#   "tokens_evaluated": 72
+# }
 #
+# FYI 200002 is last token => <|return|> ... though, llama-server does not show the token <|return|> (maybe an option to do so?)
+
+
+# -d @-     ==> means STDIN => request body
+
+# %% * raw prompt => /v1/completions (OpenAI compat)
+
+mkdir -p v1_completions_oai
+
+curl --fail-with-body -sSL --no-buffer "$base_url/v1/completions" \
+    -d @test.apply-template.json | jq >v1_completions_oai/raw.json
+
+# streaming => .choices[0].text
+cat test.apply-template.json | jq 'add(.stream=true)' | \
+    curl --fail-with-body -sSL --no-buffer "$base_url/v1/completions" -d @- \
+    | string replace --regex  "^data: " "" | string replace --regex "\[DONE\]" "" \
+    | jq >v1_completions_oai/raw.stream.json
+# bulk of response is in:
+#
+#   "choices": [
+#     {
+#       "text": "",
+#       "index": 0,
+#       "logprobs": null,
+#       "finish_reason": null
+#     }
+#   ],
+#
+# SUPER COOL! .__verbose shows underlying /completions like format
+#    IIAC that is b/c /v1/completions simply transforms /completions format?
+#   "__verbose": {
+#     "index": 0,
+#     "content": "<|channel|>",
+#     "tokens": [
+#       200005
+#     ],
+#     "stop": false,
+#     "id_slot": -1,
+#     "tokens_predicted": 1,
+#     "tokens_evaluated": 72
+#   }
+#
+
 #
 # %% * NO THINK w/ pre-filled analysis channel
 
-curl --fail-with-body -sSL --no-buffer "$base_url/v1/completions" \
+curl --fail-with-body -sSL --no-buffer "$base_url/completions" \
     -d @./nothink/1-only-message.json | jq
+# "index": 0,
+# "content": "<|message|>Got it! If you have any questions or need assistance, just let me know. 😊",
+# "tokens": [],
+# "id_slot": 3,
+# "stop": true,
+# "model": "gpt-3.5-turbo",
+# "tokens_predicted": 20,
+# "tokens_evaluated": 99,
+
+# "truncated": false,
+# "stop_type": "eos",
+# "stopping_word": "",
+
+
+# %% * /props - verify config + chat template
+#  especially to verify ENV VARS / CLI ARGS applied as expected
+
+curl build21.lan:8013/props | jq .
+
+# drop chat_template (big item) so it is easier to see the rest:
+curl build21.lan:8013/props | jq 'del(.chat_template)'
+
+# only chat_template:
+curl build21.lan:8013/props | jq .chat_template
