7f2864ae016220d7c39a773821ee549733393511
Wes Higbee <wes.mcclure@gmail.com>
2025-11-21 00:01:36 -0600

    modify examples

--- notes/samples/gptoss/gen.fish
+++ notes/samples/gptoss/gen.fish
@@ -1,10 +1,14 @@
 #!/usr/bin/env fish
 
+set base_url 'http://build21:8013'
+
+# %% * /v1/chat/completions
+
 echo '{
   "messages": [ { "role": "user", "content": "test" } ],
   "max_tokens": 80,
   "stream": true
-}' | curl --fail-with-body -sSL --no-buffer http://build21:8013/v1/chat/completions -d @- \
+}' | curl --fail-with-body -sSL --no-buffer "$base_url/v1/chat/completions" -d @- \
     | string replace --regex "^data: (\[DONE\])*" "" \
     # null entries for records w/o choices[0].delta.content
     | jq --compact-output >test.stream.json
@@ -13,30 +17,32 @@ echo '{
   "messages": [ { "role": "user", "content": "test" } ],
   "max_tokens": 80,
   "stream": false
-}' | curl --fail-with-body -sSL --no-buffer http://build21:8013/v1/chat/completions -d @- \
+}' | curl --fail-with-body -sSL --no-buffer "$base_url/v1/chat/completions" -d @- \
     | string replace --regex "^data: (\[DONE\])*" "" \
     # null entries for records w/o choices[0].delta.content
     | jq --compact-output >test.full.json
 
-# %%
+# %% * /apply-template
 
 # render chat template to raw prompt
 echo '{
   "messages": [ { "role": "user", "content": "test" } ],
   "max_tokens": 80,
   "stream": false
-}' | curl --fail-with-body -sSL --no-buffer http://build21:8013/apply-template -d @- \
+}' | curl --fail-with-body -sSL --no-buffer "$base_url/apply-template" -d @- \
     | jq >test.apply-template.json
 
-# %%
+# %% * raw prompt => /v1/completions
 
 # send the rendered prompt
-
-curl --fail-with-body -sSL --no-buffer http://build21:8013/v1/completions \
+curl --fail-with-body -sSL --no-buffer "$base_url/v1/completions" \
     -d @test.apply-template.json | jq >test.raw.json
 
 # %%
 
-# * NO THINK w/ pre-filled analysis channel
-curl --fail-with-body -sSL --no-buffer http://build21:8013/v1/completions \
+#
+#
+# %% * NO THINK w/ pre-filled analysis channel
+
+curl --fail-with-body -sSL --no-buffer "$base_url/v1/completions" \
     -d @./nothink/1-only-message.json | jq
