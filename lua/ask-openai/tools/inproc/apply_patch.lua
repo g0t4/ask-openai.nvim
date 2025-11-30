c583049b0ea744022eaed83aedc748a67a4d75eb
Wes Higbee <wes.mcclure@gmail.com>
2025-11-30 17:43:44 -0600

    use type="string" ... I will update the jinja template to support this

--- lua/ask-openai/tools/inproc/apply_patch.lua
+++ lua/ask-openai/tools/inproc/apply_patch.lua
@@ -2,49 +2,20 @@
 ---@diagnostic disable-next-line: missing-fields
 local M = {}
 
----@type OpenAITool
+-- TODO extra instructions for system message (goes into developer message for gptoss)
+M.SystemMessageExplanation = {
+
+}
+
 M.ToolDefinition = {
-    -- tool definition based on gpt-oss repo:
-    --   https://github.com/g0t4/gpt-oss/blob/2cb651c/gpt_oss/chat.py#L101-L123
-    --     UGH... this looks like it wouldn't render with gpt-oss's template... unless the server does something to map parameters to parameters.properties (but then there's no name?)
-    --     OR, does the client in chat.py change things? It doesn't seem so b/c I see BaseModel (pydantic) which suggests these are verbatim passed to backend server
-    --      it is possible that OpenAI's server does some things to change what gets mapped in?
-    --      or is this chat app intended for say ollama backend? or llama-cpp... do either of these transform the single parameter case to work with gptoss template?
-    --        I guess what would it matter? it would have to be a named property for that template (jinja) to render it... so I can just set name myself
-    -- parameters={
-    --     "type": "string",
-    --     "description": "Formatted patch code",
-    --     "default": "*** Begin Patch\n*** End Patch\n",
-    -- }
-    --
-
-
-    -- FYI confirmed tools structure matches template expectations:
-    --   https://github.com/ggml-org/llama.cpp/blob/10e978015/models/templates/openai-gpt-oss-120b.jinja#L108-L149
-    --   notably expects tool.function for definition L112
+    -- FYI confirmed jinja expects tool.function: https://github.com/ggml-org/llama.cpp/blob/10e978015/models/templates/openai-gpt-oss-120b.jinja#L108-L149
     ["function"] = {
         description = "Patch a file",
         name = "apply_patch",
+        ---@diagnostic disable-next-line: missing-fields
         parameters = {
-            type = "object",
-            properties = {
-                -- FYI must have parameters.properties to render in gptoss120b template
-                --   so create one named property "patch"
-                -- TODO how about add my own explanation to developer message and not send apply_patch in tools? AFAICT this is just an update to the developer message on the server side?
-                --   TODO can you run the chat.py app and see what it is sending for this structure!
-                --   it almost looks like it won't render any properties in the jinja template for it!
-                patch = {
-                    -- TODO multiple patch files? the apply_patch.md file suggests can have multiple
-                    --    TODO is it all in one string value and I split them?
-                    --       btw apply_patch.py right now only takes one, but I could easily split on first line of each
-                    --    TODO or, should I accept an array of strings?
-                    --    TODO can I get any hint about what the tool looked like in training?
-                    type = "string",
-                    description = "Formatted patch code",
-                    default = "*** Begin Patch\n*** End Patch\n",
-                }
-            },
-            required = { "patch" }
+            type = "string",
+            -- FYI I had to patch the jinja template to support string only arg (otherwise it forces to use dict which means needless JSON wrapper)
         }
     },
     type = "function"
@@ -68,6 +39,8 @@ end
 --   why did OpenAI release a jinja template that doesn't even support using their apply_patch tool?!?!
 
 -- TODO any issues having two Tools sections in developer message (if I pass some tools via body.tools?)
+--   FYI I rendered this via: https://github.com/g0t4/gpt-oss/blob/017c732/gpt_oss/chat.py#L129-L133
+--     also https://github.com/g0t4/gpt-oss/blob/2cb651c/gpt_oss/chat.py#L101-L123
 local developer_message_apply_patch_tool_definition_only = [[
 # Tools
 
