8943ad5a72e2800ccb6b087cd1bd62a6f4ab891f
Wes Higbee <wes.mcclure@gmail.com>
2025-12-01 12:04:05 -0600

    todo add off level for ask questions

--- lua/ask-openai/questions/chat/thread.lua
+++ lua/ask-openai/questions/chat/thread.lua
@@ -39,6 +39,10 @@ end
 
 ---@return table body
 function ChatThread:next_curl_request_body()
+    -- TODO add support for FimReasoningLevel.off => do the same as I am doing in FIM by setting last message w/ role==assistant and the message can prefill to force no thinking
+    --   TODO see this in fim code:
+    --     local fixed_thoughts = HarmonyRawFimPromptBuilder.deep_thoughts_about_fim
+
     local body = {
         -- TODO! clone so I can manipulate messages sent back?
         messages = self.messages,
