3f6658404e3be4d302a9a59e10c805066e3e8220
Wes Higbee <wes.mcclure@gmail.com>
2026-01-02 00:17:44 -0600

    notes

--- lua/ask-openai/predictions/context/coc/context.lua
+++ lua/ask-openai/predictions/context/coc/context.lua
@@ -1,19 +1,20 @@
 -- TODO this is a reminder, put coc related context item providers here
---
---
 --  maybes:
---   vim.fn.CocAction('diagnosticInfo') [target]  ... suppposed to be able to get diagnostics for current cursor position (IIUC)
---      how do I pass target? is it a lnum/col?
---         :h coc-actions
---      add a /diag to slash context commands
---   CocAction('diagnosticList')
---      returns for entire workspace, can filter on bufnr/file (see learn/diagnostics.lua examples)
 --   CocAction('documentSymbols')
 --   CocAction('workspaceSymbols')
 
 local M = {}
 
 function M.setup()
+end
+
+function M.diagnostic_under_cursor()
+    -- :h coc-actions
+
+    -- slash commands?
+    -- /diagc[ursor] -- only diagnostics under cursor: ?? vim.fn.CocAction('diagnosticInfo', 'echo') ??
+    -- /diagw[orkspace] -- everything from CocAction('diagnosticList')
+    -- /diagf[ile] -- match on bufnr/file to filter CocAction('diagnosticList')
 end
 
 return M
