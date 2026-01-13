c4968c628214968b8779d5d0a550d1f3188de941
Wes Higbee <wes.mcclure@gmail.com>
2026-01-13 15:39:47 -0600

    use rust based apply_patch tool, it appears to be maintained and it has better output to confirm changes... and bonus, it's faster

--- lua/ask-openai/tools/inproc/apply_patch.lua
+++ lua/ask-openai/tools/inproc/apply_patch.lua
@@ -82,14 +82,16 @@ function M.call(parsed_args, callback)
         log:error("Original patch:\n" .. patch, vim.log.levels.ERROR)
     end
 
-
+    -- * python variant in gptoss repo:
     -- cat ~/repos/github/g0t4/gpt-oss/gpt_oss/tools/example-add.patch | ~/repos/github/g0t4/gpt-oss/.venv/bin/python3 ~/repos/github/g0t4/gpt-oss/gpt_oss/tools/apply_patch.py
-    local python = vim.fn.expand("~/repos/github/g0t4/gpt-oss/.venv/bin/python3")
-    local apply_patch_py = vim.fn.expand("~/repos/github/g0t4/gpt-oss/gpt_oss/tools/apply_patch.py")
+    -- local python = vim.fn.expand("~/repos/github/g0t4/gpt-oss/.venv/bin/python3")
+    -- local apply_patch_py = vim.fn.expand("~/repos/github/g0t4/gpt-oss/gpt_oss/tools/apply_patch.py")
+
+    local apply_patch_rs = vim.fn.expand("~/repos/github/openai/codex/codex-rs/target/release/apply_patch")
 
     -- PRN use async and get callback?
     -- FYI do not differentiate STDOUT/STDERR unless you can prove it fixes a problem with model performance
-    local result = vim.fn.system({ python, apply_patch_py }, patch)
+    local result = vim.fn.system({ apply_patch_rs }, patch)
     log:info("apply_patch - vim.v.shell_error", vim.v.shell_error)
 
     -- apply_patch tool behaviors:
