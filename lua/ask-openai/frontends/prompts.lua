579afe2617d80350e3eca0e2302572992bb01e14
Wes Higbee <wes.mcclure@gmail.com>
2026-01-18 10:27:37 -0600

    reword to user perspective

--- lua/ask-openai/frontends/prompts.lua
+++ lua/ask-openai/frontends/prompts.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.semantic_grep_header(rag_matches)
     return {
         "# Semantic Grep matches: " .. #rag_matches .. "\n",
-        "This is automatic context from my neovim AI tools. The user's request is used to query for relevant code. Only the top results are included. These may or may not be relevant."
+        "This is automatic context based on my request. These may or may not be relevant."
     }
 end
 
