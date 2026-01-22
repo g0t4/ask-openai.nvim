a82b861e25b7592a2ead72cd639f34bd78dce35a
Wes Higbee <wes.mcclure@gmail.com>
2026-01-21 21:51:14 -0600

    run vite helper

--- .config.fish
+++ .config.fish
@@ -1,1 +1,3 @@
 abbr ptw_apply_patch_multi 'ptw --clear lua/ask-openai/tools/inproc/apply_patch_multi_tests.py --  --capture=no --log-cli-level=INFO'
+
+abbr run_vite 'cd tools/chat_viewer_web; npm run dev'
