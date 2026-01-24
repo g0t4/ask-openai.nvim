931814ad3205244035dd34c519f97ac70eb08d54
Wes Higbee <wes.mcclure@gmail.com>
2026-01-23 19:33:43 -0600

    if matching a chunk that starts middle of a line, then prefill whitespace so its func signature is not de-indented

--- lua/ask-openai/frontends/prompts.lua
+++ lua/ask-openai/frontends/prompts.lua
@@ -25,6 +25,20 @@ function M.semantic_grep_user_message(rag_matches)
             ---@cast chunk LSPRankedMatch
             local file = chunk.file .. ":" .. chunk.start_line_base0 .. "-" .. chunk.end_line_base0
             local code_chunk = chunk.text
+
+            -- * add leading whitespace for non-zero start columns (ts chunks only, so far)
+            if chunk.start_column_base0 > 0 then
+                -- I noticed in some cases a treesitter matched function has non-zero start column and that shows as incorrectly indented in thread RAG matches
+                --   mostly wanted to avoid model being confused, especialy if multiple matches are from same file and the de-indented func signature (often all that is off like this)... is just slightly wrong but could it confuse the generated code?
+                --      could this be partially why I get poorly indented suggestions at times?
+                --   so, I am adding whitespace for now
+                --   FYI it would be fine to roll this back, most of the time it would just be incorrectly indented function signature, which isn't even invalid in python
+                -- TODO any material cases where the leading chars aren't actually whitespace?
+                local visible_ws = " "
+                code_chunk = string.rep(visible_ws, chunk.start_column_base0) .. code_chunk
+                -- TODO consider is col offset in bytes or chars? (see RAG preview for more on this)... i.e. with emoji or other unicode chars?
+            end
+
             table.insert(lines,
                 "## " .. file .. "\n"
                 .. code_chunk .. "\n"
