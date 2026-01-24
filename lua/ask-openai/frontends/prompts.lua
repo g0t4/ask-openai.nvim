a3f02977d4c7b86020711b4e3ed58be0a6cd2b0e
Wes Higbee <wes.mcclure@gmail.com>
2026-01-24 04:59:45 -0600

    fix for nil start_column (uncovered_code chunks)

--- lua/ask-openai/frontends/prompts.lua
+++ lua/ask-openai/frontends/prompts.lua
@@ -27,7 +27,8 @@ function M.semantic_grep_user_message(rag_matches)
             local code_chunk = chunk.text
 
             -- * add leading whitespace for non-zero start columns (ts chunks only, so far)
-            if chunk.start_column_base0 > 0 then
+            local start_col0 = chunk.start_column_base0
+            if start_col0 and start_col0 > 0 then
                 -- I noticed in some cases a treesitter matched function has non-zero start column and that shows as incorrectly indented in thread RAG matches
                 --   mostly wanted to avoid model being confused, especialy if multiple matches are from same file and the de-indented func signature (often all that is off like this)... is just slightly wrong but could it confuse the generated code?
                 --      could this be partially why I get poorly indented suggestions at times?
@@ -35,7 +36,7 @@ function M.semantic_grep_user_message(rag_matches)
                 --   FYI it would be fine to roll this back, most of the time it would just be incorrectly indented function signature, which isn't even invalid in python
                 -- TODO any material cases where the leading chars aren't actually whitespace?
                 local visible_ws = " "
-                code_chunk = string.rep(visible_ws, chunk.start_column_base0) .. code_chunk
+                code_chunk = string.rep(visible_ws, start_col0) .. code_chunk
                 -- TODO consider is col offset in bytes or chars? (see RAG preview for more on this)... i.e. with emoji or other unicode chars?
             end
 
