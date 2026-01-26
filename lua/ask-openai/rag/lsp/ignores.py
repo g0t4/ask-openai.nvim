3172039e94d9efdb807a7e08c258712fefd16eea
Wes Higbee <wes.mcclure@gmail.com>
2026-01-26 07:03:24 -0600

    create and cache gitignore_spec

--- lua/ask-openai/rag/lsp/ignores.py
+++ lua/ask-openai/rag/lsp/ignores.py
@@ -8,40 +8,44 @@ from lsp.config import Config
 
 logger = get_logger(__name__)
 
-# don't mark None by default, else pyright will be a PITA
-gitignore_spec: PathSpec
+def setup_ignores(fs_root_path) -> PathSpec:
+    # TODO why not just create this on first use! have helper to create/get it
+    gitignore_path = fs_root_path.joinpath(".gitignore")
 
-def setup_ignores(fs_root_path):
+    ignore_entries = set()
+    if gitignore_path.exists():
+        ignore_entries = set(gitignore_path.read_text().splitlines())
+
+    # ALWAYS exclude:
+    ignore_entries.update([
+        # focus on directories mostly, the languages you actually index can filter on file types implicitly (not indexed == ignored too)
+        ".git",
+        ".venv",
+        "__pycache__",
+        "node_modules",
+        "bower_components",
+        "iterm2env",
+
+        # files that are often committed but shouldn't ever be indexed:
+        "package-lock.json",
+        "uv.lock",  # PRN *.lock?
+        # ? other lock files?
+    ])
+    # TODO config.ignores load into gitignore spec!
+
+    return PathSpec.from_lines(GitWildMatchPattern, ignore_entries)
+
+gitignore_spec: PathSpec = None
+
+def get_gitignore_spec(fs_root_path):
     global gitignore_spec
+    # FYI always the same, so cache it... TECHNICALLY I SHOULD CACHE PER PATH
+    # TODO memoize the function
 
-    def _setup_gitignored() -> PathSpec:
-        # TODO why not just create this on first use! have helper to create/get it
-        gitignore_path = fs_root_path.joinpath(".gitignore")
+    if (gitignore_spec is None):
+        gitignore_spec = setup_ignores(fs_root_path)
 
-        ignore_entries = set()
-        if gitignore_path.exists():
-            ignore_entries = set(gitignore_path.read_text().splitlines())
-
-        # ALWAYS exclude:
-        ignore_entries.update([
-            # focus on directories mostly, the languages you actually index can filter on file types implicitly (not indexed == ignored too)
-            ".git",
-            ".venv",
-            "__pycache__",
-            "node_modules",
-            "bower_components",
-            "iterm2env",
-
-            # files that are often committed but shouldn't ever be indexed:
-            "package-lock.json",
-            "uv.lock",  # PRN *.lock?
-            # ? other lock files?
-        ])
-        # TODO config.ignores load into gitignore spec!
-
-        return PathSpec.from_lines(GitWildMatchPattern, ignore_entries)
-
-    gitignore_spec = _setup_gitignored()
+    return gitignore_spec
 
 IGNORED = True
 
@@ -71,4 +75,5 @@ def _is_gitignored(file_path: str | Path, fs_root_path):
     # relative path is needed for relative patterns that start without a wildcard
     rel_path = file_path.relative_to(fs_root_path)
 
-    return gitignore_spec.match_file(rel_path)
+    spec = get_gitignore_spec(fs_root_path)
+    return spec.match_file(rel_path)
