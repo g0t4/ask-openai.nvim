14253f6d70875fc44d6fa96fff69f1dc2dfcf9db
Wes Higbee <wes.mcclure@gmail.com>
2026-04-13 18:56:02 -0500

    bump up tokens for gptoss120b on cmdline suggests

--- lua/ask-openai/config/init.lua
+++ lua/ask-openai/config/init.lua
@@ -41,7 +41,7 @@ local default_options = {
 
     -- request parameters:
     model = "gpt-4o",
-    max_tokens = 200, -- maybe have a higher toggle if absolutely needed?
+    max_tokens = 2000, -- higher when using thinking models like gptoss120b
     -- PRN temperature
     -- in future, if add other ask helpers then I can move these into a nested table like copilot options
 
