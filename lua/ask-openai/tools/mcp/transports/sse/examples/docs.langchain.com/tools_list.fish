2ea20186571a45cbf23fbd9d6b1765ca06cdf49f
Wes Higbee <wes.mcclure@gmail.com>
2026-04-15 14:24:33 -0500

    w00t got SSE response now! with same capabilities info but this time in an SSE

--- lua/ask-openai/tools/mcp/transports/sse/examples/docs.langchain.com/tools_list.fish
+++ lua/ask-openai/tools/mcp/transports/sse/examples/docs.langchain.com/tools_list.fish
@@ -1,9 +1,20 @@
 #!/usr/bin/env fish
 
 set initalize '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2024-11-05","capabilities":{},"clientInfo":{"name":"test","version":"1.0"}}}'
+# got this error: {"jsonrpc":"2.0","error":{"code":-32000,"message":"Not Acceptable: Client must accept both application/json and text/event-stream"},"id":null}
 
 set list_tools '{ "jsonrpc": "2.0", "id": 1, "method": "tools/list" }'
 
-echo $initalize | http "https://docs.langchain.com/mcp"
+echo $initalize | http \
+    "https://docs.langchain.com/mcp" \
+    accept:"application/json;text/event-stream" > initialize.response
+# event: message
+# data: {
+#     "id": 1,
+#     "jsonrpc": "2.0",
+#     "result": {
+#         "capabilities": {
+#   ...
+
 
 
