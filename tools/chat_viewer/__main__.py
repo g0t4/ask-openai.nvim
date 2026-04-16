940c4cc3bd3689c0bbe3401a4f3c3986012c6c59
Wes Higbee <wes.mcclure@gmail.com>
2026-04-16 13:59:52 -0500

    hide

--- tools/chat_viewer/__main__.py
+++ tools/chat_viewer/__main__.py
@@ -25,11 +25,12 @@ EXCLUDED_CONTENT_HASHES: list[str] = [
     #      use jq's --join-output which joins lines thus removing \n on end
     #
     # * Rewrites
-    "4601994390a24a63f5e38160e15dd11c02361ed2be2af84d1a2ae8e77bc7392b",  # System Message - Rewrite #1 "Ground rules" (short list of 9 bullets)
-    "832293045d2b0ff4a19379aa4bfd15de6dc268434f85e01306741ca23a0f566c",  # ## General Code Preferences - Rewrite #3 (very short)
-    "3213ad85a96c3647b5ff593803be3f0f412187237932208647df3c4db75bb20d",  # Rewrite - Lua Code Preferences user message
+    "4601994390a24a63f5e38160e15dd11c02361ed2be2af84d1a2ae8e77bc7392b",  # Rewrite msg1 - System Message - #1 "Ground rules" (short list of 9 bullets)
+    "832293045d2b0ff4a19379aa4bfd15de6dc268434f85e01306741ca23a0f566c",  # Rewrite msg3 - ## General Code Preferences (very short)
+    "3213ad85a96c3647b5ff593803be3f0f412187237932208647df3c4db75bb20d",  # Rewrite -msg? - Lua Code Preferences user message
     #
     # * FIMs
+    "7da90487ce9c82207d7f5a884bc68ae2dae75ce0b72b9636b2296760b8633d7d",  # FIM System Message - appears not to have unique values
     "71675b1a7166d68175033a5f256cfa7fe6097d25a9ed1167d3c0d884f03d438e",  # FIM msg 2 - "context that's automatically provided" - matches if NO yanks
     # - won't always match b/c IIRC yanks go at end...
     # OR I could literally do a find on blobs of text to ignore (and use current prompt values for that?)
