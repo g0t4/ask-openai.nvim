12cbfa21bf81408eb7aeebe3251f51478e631824
Wes Higbee <wes.mcclure@gmail.com>
2026-04-21 16:14:22 -0500

    format => indent

--- lua/ask-openai/backends/sse/data_only_parser.tests.lua
+++ lua/ask-openai/backends/sse/data_only_parser.tests.lua
@@ -40,43 +40,43 @@ describe("data-only events", function()
     end
 
     describe("concatenate", function()
-    it("concatenate split write data value with newline at end of first write, preserves value's newline", function()
-        -- FYI it is possible this is not how I should combine when there are multiple data labels, in that case I might need to concat with \n in between
-        --   for now I am leaving this as is as I have yet to see it in the wild with openai compatible streaming endpoints, so I can't easily verify one way or the other
-        --   in this case if I concat w/ \n then I should have "hello\n\nworld"
+        it("concatenate split write data value with newline at end of first write, preserves value's newline", function()
+            -- FYI it is possible this is not how I should combine when there are multiple data labels, in that case I might need to concat with \n in between
+            --   for now I am leaving this as is as I have yet to see it in the wild with openai compatible streaming endpoints, so I can't easily verify one way or the other
+            --   in this case if I concat w/ \n then I should have "hello\n\nworld"
 
-        -- no different than if the \n were in the middle of the data value
-        local write1 = "data: hello\n"
-        local write2 = "data: world\n\n"
+            -- no different than if the \n were in the middle of the data value
+            local write1 = "data: hello\n"
+            local write2 = "data: world\n\n"
 
-        parser:write(write1)
-        parser:write(write2)
-        assert.are.same({ "hello\nworld" }, events)
-    end)
-    it("concatenate split write data value (single event)", function()
-        -- FYI it is possible this is not how I should combine when there are multiple data labels, in that case I might need to concat with \n in between
-        --   for now I am leaving this as is as I have yet to see it in the wild with openai compatible streaming endpoints, so I can't easily verify one way or the other
-        --   in this case if I concat w/ \n then I should have "hello\nworld"
+            parser:write(write1)
+            parser:write(write2)
+            assert.are.same({ "hello\nworld" }, events)
+        end)
+        it("concatenate split write data value (single event)", function()
+            -- FYI it is possible this is not how I should combine when there are multiple data labels, in that case I might need to concat with \n in between
+            --   for now I am leaving this as is as I have yet to see it in the wild with openai compatible streaming endpoints, so I can't easily verify one way or the other
+            --   in this case if I concat w/ \n then I should have "hello\nworld"
 
-        local write1 = "data: hello"
-        local write2 = "data: world\n\n"
+            local write1 = "data: hello"
+            local write2 = "data: world\n\n"
 
-        parser:write(write1)
-        parser:write(write2)
-        assert.are.same({ "helloworld" }, events)
-    end)
-    it("concatenate split write data value without 'data: ' prefix on second write", function()
-        -- FYI AFAICT this is NOT PER the spec... and is just my intution looking at how llama-server generates this one large final SSE (w/ verbose logging turned on)
-        -- see multi-line-sse.json which makes it pretty clear no \n is intended between the two lines
-        -- so even if I change 2+ data lables to use \n I would likely leave this as is with no \n added in middle
+            parser:write(write1)
+            parser:write(write2)
+            assert.are.same({ "helloworld" }, events)
+        end)
+        it("concatenate split write data value without 'data: ' prefix on second write", function()
+            -- FYI AFAICT this is NOT PER the spec... and is just my intution looking at how llama-server generates this one large final SSE (w/ verbose logging turned on)
+            -- see multi-line-sse.json which makes it pretty clear no \n is intended between the two lines
+            -- so even if I change 2+ data lables to use \n I would likely leave this as is with no \n added in middle
 
-        local write1 = "data: data_va"
-        local write2 = "lue1\n\n"
+            local write1 = "data: data_va"
+            local write2 = "lue1\n\n"
 
-        parser:write(write1)
-        parser:write(write2)
-        assert.are.same({ "data_value1" }, events)
-    end)
+            parser:write(write1)
+            parser:write(write2)
+            assert.are.same({ "data_value1" }, events)
+        end)
     end)
 
     it("multiple events in single write", function()
