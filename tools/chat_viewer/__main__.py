11e747d9a534a67b5ba8ff19752374b34baefb96
Wes Higbee <wes.mcclure@gmail.com>
2026-05-12 01:46:21 -0500

    lookup style separate of applying it

--- tools/chat_viewer/__main__.py
+++ tools/chat_viewer/__main__.py
@@ -131,16 +131,14 @@ class SectionDTO:
             # add other mappings here as needed
         }
 
-        header_render = None
-        for heading, apply_style in emphasize_headings.items():
-            if heading in header_line:
-                header_render = Text(header_line, style=apply_style)
-                break
+        override_header_style = next(
+            (style for heading, style in emphasize_headings.items() if heading in header_line), # note "in" not "==" for matching
+            None,
+        )
+        if override_header_style:
+            return Group(Text(header_line, style=override_header_style), _syntax(content, "markdown"))
 
-        if header_render:
-            return Group(header_render, _syntax(content, "markdown"))
-
-        # if no header style override then render header as markdown along with content:
+        # else treat header as markdown too:
         return _syntax(self.content, "markdown")
 
 def _split_content_into_sections(content: str) -> list[SectionDTO]:
