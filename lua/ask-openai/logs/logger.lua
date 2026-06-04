e45156f594f99cb7cd4ef37e25b25a3ec1c4a83e
Wes Higbee <wes.mcclure@gmail.com>
2026-06-04 08:40:50 -0500

    test broken code for nil with ipairs... intentionally make sure the tests are now failing => they are!

--- lua/ask-openai/logs/logger.lua
+++ lua/ask-openai/logs/logger.lua
@@ -77,8 +77,7 @@ local function build_entry(level_number, ...)
     --   also, can use:    for k,v in pairs(arg)
     -- FYI using `arg` resulted in parameters from previous calls (w/ more params) to be logged in subsequent logs...
     local stringified = {} -- new set of args to write into, don't try to use special `arg` variable
-    for i = 1, select("#", ...) do
-        local value = select(i, ...)
+    for i, value in ipairs({ ... }) do
         -- make sure everything is a string so it can be concatenated
         if type(value) == "table" then
             -- auto inspect table values
