db35a15948271e8d2d377f8b35f2082c1634b392
Wes Higbee <wes.mcclure@gmail.com>
2026-06-04 08:12:52 -0500

    stub out spot for new logger tests

---
+++ lua/ask-openai/logs/logger.tests.lua
@@ -0,0 +1,9 @@
+require("ask-openai.helpers.test_setup").modify_package_path()
+local assert = require 'luassert'
+local buffers = require('devtools.tests.buffers')
+local describe = require('devtools.tests.define.describe')
+
+describe("TODO", function()
+    -- TODO stub out innermost log function and capture calls to verify what would be logged without touching log files
+    -- TODO
+end)
