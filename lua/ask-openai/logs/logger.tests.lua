0d8679c6b420dd75c130ca5003186e2422de0beb
Wes Higbee <wes.mcclure@gmail.com>
2026-06-04 08:24:19 -0500

    setup logging tests, inject capture function in the innermost log helper... and setup failing test for nil scenario now that I have auto inspect causing the same issue as before b/c of ipairs

--- lua/ask-openai/logs/logger.tests.lua
+++ lua/ask-openai/logs/logger.tests.lua
@@ -3,7 +3,32 @@ local assert = require 'luassert'
 local buffers = require('devtools.tests.buffers')
 local describe = require('devtools.tests.define.describe')
 
-describe("TODO", function()
-    -- TODO stub out innermost log function and capture calls to verify what would be logged without touching log files
-    -- TODO
+describe("test log_auto_inspect", function()
+    local log = require("ask-openai.logs.logger"):predictions() -- for now use my single logger is fine
+    local captures = {}
+    local original_log
+    log.log = function(self, level, ...)
+        table.insert(captures, { level = level, args = { ... } })
+    end
+
+    before_each(function()
+        captures = {}
+    end)
+
+    it("table is vim.inspect'd", function()
+        local tbl = { a = 1, b = 2 }
+        log:info("message", 1, tbl)
+        assert.equals(1, #captures)
+        assert.equals([[{
+  a = 1,
+  b = 2
+}]], captures[1].args[3])
+    end)
+
+    it("pass nil before last arg => doesn't drop 'last'", function()
+        -- classic enumeration bug in lua with ipairs!
+        log:info("message", "first", nil, "last")
+        assert.equals(3, #captures)
+        assert.equals("last", captures[3].args[3])
+    end)
 end)
