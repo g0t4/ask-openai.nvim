734c9f29688e15268c6e10790200e273b8af2c2a
Wes Higbee <wes.mcclure@gmail.com>
2026-06-12 16:13:00 -0500

    format

--- lua/ask-openai/agents/viewer/window.lua
+++ lua/ask-openai/agents/viewer/window.lua
@@ -103,7 +103,10 @@ function AgentWindow:ensure_spinner_running(base_title)
                 end
 
                 ---@type vim.api.keyset.win_config
-                local title_config = { title = " " .. title .. " ", title_pos = "center" }
+                local title_config = {
+                    title = " " .. title .. " ",
+                    title_pos = "center",
+                }
                 vim.api.nvim_win_set_config(self.win_id, title_config)
             end)
         end)
