0a085afcb7055de0b34e9335d1bcecc407cab3b5
Wes Higbee <wes.mcclure@gmail.com>
2026-06-16 14:51:23 -0500

    rename to rag_dir which is where the file is at... also use absolute path to fix finding the parent (else just dot '.')

--- lua/ask-openai/rag/chunks/chunker_tests.py
+++ lua/ask-openai/rag/chunks/chunker_tests.py
@@ -10,10 +10,10 @@ from chunks.ts.ts import *
 from lsp.storage import Chunk, ChunkType
 
 # * set root dir for relative paths
-repo_root = Path(__file__).parent.parent.parent.parent.parent.parent
-my_dir = Path(__file__).parent.parent
-test_cases = my_dir / "tests/test_cases"
-test_cases_treesitter = my_dir / "tests/test_cases/treesitter"
+repo_root = Path(__file__).absolute().parent.parent.parent.parent.parent.parent
+rag_dir = Path(__file__).absolute().parent.parent
+test_cases = rag_dir / "tests/test_cases"
+test_cases_treesitter = rag_dir / "tests/test_cases/treesitter"
 test_cases_python = test_cases_treesitter / "python"
 test_cases_typescript = test_cases_treesitter / "typescript"
 test_cases_c = test_cases_treesitter / "c"
