5a9f80455c985a1f750c0aaa1fe7e1acc2ce5b08
Wes Higbee <wes.mcclure@gmail.com>
2026-06-16 21:25:52 -0500

    split out initialize "command" logic

--- lua/ask-openai/rag/language_server/__main__.py
+++ lua/ask-openai/rag/language_server/__main__.py
@@ -6,9 +6,8 @@ import lsprotocol.types as types
 from pygls.lsp.server import LanguageServer
 
 from index import fs
-from language_server import rag
 from rag.logs import get_logger, logging_fwk_to_language_server_log_file
-from language_server.commands import sleepy, grep, update_file, cancel
+from language_server.commands import sleepy, grep, update_file, cancel, initialize
 
 logging_fwk_to_language_server_log_file(logging.INFO)
 # logging_fwk_to_language_server_log_file(logging.DEBUG)
@@ -17,51 +16,10 @@ logger = get_logger(__name__)
 server = LanguageServer("ask_language_server", "v0.1")
 
 cancel.setup(server)
+initialize.setup(server)
 sleepy.register_command(server)
 update_file.register_commands(server)
 grep.register_command(server)
-
-@server.feature(types.INITIALIZE)
-async def on_initialize(_: LanguageServer, params: types.InitializeParams):
-    # TODO pass rag dir as command line arg??
-    # - TODO so I don't have to wait for on_initialize to setup Datasets!
-
-    # # PRN use workspace folders if multi-workspace ...
-    # # FYI could also get me CWD, round about way, if I wanted to prioritize that for .rag dir over git repo root
-    # logger.info(f"{params.workspace_folders=}")
-    # server.workspace.folders
-
-    await fs.set_root_dir(params.root_path)
-    if not fs.get_config().enabled or fs.is_no_rag_dir():
-        # DO NOT notify yet, that has to come after server responds to initialize request
-        return types.InitializeResult(capabilities=types.ServerCapabilities())
-
-def tell_client_to_shut_that_shit_down_now():
-    server.protocol.notify("fuu/no_dot_rag__do_the_right_thing_wink")
-
-@server.feature(types.INITIALIZED)
-def on_initialized(_: LanguageServer, _params: types.InitializedParams):
-    #  FYI server is managed by the client!
-    #  client sends initialize request first => waits for server to send InitializeResult
-    #    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#initialize
-    #  then, client sends initialized (this) request => waits for completion
-    #    does not send other requests until initialized is done
-    #  https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#initialized
-
-    if not fs.get_config().enabled:
-        logger.info("RAG disabled, notifying LSP client to shutdown")
-        tell_client_to_shut_that_shit_down_now()
-        return
-
-    if fs.is_no_rag_dir():
-        # TODO allow building the index from scratch?
-        logger.error(f"STOP on_initialize[d] b/c no .rag dir")
-        tell_client_to_shut_that_shit_down_now()
-        return
-
-    rag.load_model_and_indexes(fs.rag_project.dot_rag_dir)  # TODO! ASYNC?
-    rag.validate_rag_indexes()  # TODO! ASYNC?
-    update_file.create_queue(server)
 
 def sigkill_self_else_pygls_hangs_when_test_standalone_startup_of_LS(*_):
     logger.warning("SIGKILL myself")
---
+++ lua/ask-openai/rag/language_server/commands/initialize.py
@@ -0,0 +1,53 @@
+import lsprotocol.types as types
+
+from pygls.lsp.server import LanguageServer
+from rag.logs import get_logger
+from index import fs
+from language_server import rag
+from language_server.commands import update_file
+
+logger = get_logger(__name__)
+
+def setup(server: LanguageServer):
+
+    @server.feature(types.INITIALIZE)
+    async def on_initialize(_: LanguageServer, params: types.InitializeParams):
+        # TODO pass rag dir as command line arg??
+        # - TODO so I don't have to wait for on_initialize to setup Datasets!
+
+        # # PRN use workspace folders if multi-workspace ...
+        # # FYI could also get me CWD, round about way, if I wanted to prioritize that for .rag dir over git repo root
+        # logger.info(f"{params.workspace_folders=}")
+        # server.workspace.folders
+
+        await fs.set_root_dir(params.root_path)
+        if not fs.get_config().enabled or fs.is_no_rag_dir():
+            # DO NOT notify yet, that has to come after server responds to initialize request
+            return types.InitializeResult(capabilities=types.ServerCapabilities())
+
+    def tell_client_to_shut_that_shit_down_now():
+        server.protocol.notify("fuu/no_dot_rag__do_the_right_thing_wink")
+
+    @server.feature(types.INITIALIZED)
+    def on_initialized(_: LanguageServer, _params: types.InitializedParams):
+        #  FYI server is managed by the client!
+        #  client sends initialize request first => waits for server to send InitializeResult
+        #    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#initialize
+        #  then, client sends initialized (this) request => waits for completion
+        #    does not send other requests until initialized is done
+        #  https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#initialized
+
+        if not fs.get_config().enabled:
+            logger.info("RAG disabled, notifying LSP client to shutdown")
+            tell_client_to_shut_that_shit_down_now()
+            return
+
+        if fs.is_no_rag_dir():
+            # TODO allow building the index from scratch?
+            logger.error(f"STOP on_initialize[d] b/c no .rag dir")
+            tell_client_to_shut_that_shit_down_now()
+            return
+
+        rag.load_model_and_indexes(fs.rag_project.dot_rag_dir)  # TODO! ASYNC?
+        rag.validate_rag_indexes()  # TODO! ASYNC?
+        update_file.create_queue(server)
