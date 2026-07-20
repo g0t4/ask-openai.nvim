628804e66fb9150e691703aafe109605d0b5e4b2
Wes Higbee <wes.mcclure@gmail.com>
2026-07-19 22:10:46 -0500

    unused now

--- lua/ask-openai/rewrites/thinking/init.lua
+++
@@ -1,8 +0,0 @@
-local log = require('devtools.logs.logger').universal()
-local harmony = require('ask-openai.backends.models.gptoss.tokenizer').harmony
-
-local M = {
-    dots = require("ask-openai.frontends.thinking.dots"),
-}
-
-return M
