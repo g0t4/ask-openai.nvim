e6c5336fe2008571c72808006cdac6fab65c2b83
Wes Higbee <wes.mcclure@gmail.com>
2026-08-02 06:07:26 -0500

    review tests, add a few more notes and asserts

--- lua/ask-openai/rx/debouncing.tests.lua
+++ lua/ask-openai/rx/debouncing.tests.lua
@@ -78,7 +78,7 @@ end)
 
 describe("Observable:typingDebounceByKey", function()
     describe("integration tests", function()
-        it("immediately returns the first event, then the last event per bufnr", function()
+        it("immediately returns the first event, then the last event from typing debounce interval (per bufnr)", function()
             local input_events, typing_debounced =
                 debouncing.create_typing_debounced_observable_by_bufnr(50, 100)
             local received = {}
@@ -87,27 +87,33 @@ describe("Observable:typingDebounceByKey", function()
                 table.insert(received, event)
             end)
 
+            -- no delay on first keystroke for bufnr=1 (first buffer)
             input_events:onNext({ bufnr = 1, value = "t" })
             assert.same({ { bufnr = 1, value = "t" } }, received)
 
+            -- typing debounce period, all are debounced until no more for delay_ms
             input_events:onNext({ bufnr = 1, value = "y" })
             input_events:onNext({ bufnr = 1, value = "p" })
             input_events:onNext({ bufnr = 1, value = "i" })
+            assert.same({ { bufnr = 1, value = "t" } }, received)
 
+            -- no delay on first keystroke for bufnr=2
             input_events:onNext({ bufnr = 2, value = "a" })
             assert.same({
                 { bufnr = 1, value = "t" },
                 { bufnr = 2, value = "a" },
             }, received)
-            input_events:onNext({ bufnr = 2, value = "b" })
 
+            -- second keystroke for bufnr=2 (debounced 50ms)
+            input_events:onNext({ bufnr = 2, value = "b" })
             vim.wait(30)
+            -- so 30ms later it is not yet done waiting
             assert.same({
                 { bufnr = 1, value = "t" },
                 { bufnr = 2, value = "a" },
             }, received)
-
-            vim.wait(40)
+            vim.wait(20)
+            -- now we've past 50ms debounce delay for both bufnrs ... so last keys should now be emitted
             assert.same({
                 { bufnr = 1, value = "t" },
                 { bufnr = 2, value = "a" },
