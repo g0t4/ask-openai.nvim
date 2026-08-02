a7eb815a3cbb5028514bee69bb89c5360579c0a8
Wes Higbee <wes.mcclure@gmail.com>
2026-08-02 05:09:56 -0500

    unused

--- lua/ask-openai/rx/scheduler.tests.lua
+++ lua/ask-openai/rx/scheduler.tests.lua
@@ -9,7 +9,6 @@ local tests = require("plenary.busted")
 -- https://github.com/lunarmodules/luassert
 -- https://github.com/nvim-lua/plenary.nvim/blob/master/TESTS_README.md
 
-local rx = require("rx")
 local TimeoutScheduler = require("ask-openai.rx.scheduler")
 
 tests.describe("timeout scheduler", function()
@@ -31,25 +30,6 @@ tests.describe("timeout scheduler", function()
         assert(elapsed_ms > 90, "should block for approximately 100ms")
     end)
 end)
-
--- tests.describe("Example async test w/ assertion using a.wrap", function()
---     -- a.wrap => https://github.com/nvim-lua/plenary.nvim/blob/master/lua/plenary/async/async.lua#L76
---     -- returns an async function (future/promise, IIUC)
---     local block_until_callbacked = a.wrap(function(callback)
---         vim.defer_fn(function()
---             callback("wrapped_done")
---         end, 200) -- simulate 200ms delay
---     end, 1) -- `1` means the function has 1 callback argument
---
---     a.tests.it("should block until async operation completes", function()
---         local start_time = vim.uv.hrtime()
---         local result = block_until_callbacked()
---         local elapsed_ms = (vim.uv.hrtime() - start_time) / 1e6
---
---         assert.are.equal(result, "wrapped_done")
---         assert(elapsed_ms > 190, "should block for approximately 200ms")
---     end)
--- end)
 
 --  FYI <leader>u is now mapped to test file!
 --   nmap <leader>u <Plug>PlenaryTestFile
