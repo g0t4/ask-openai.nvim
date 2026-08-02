23da403862e8956cb5e37caee9ae2f14972d95e0
Wes Higbee <wes.mcclure@gmail.com>
2026-08-02 04:26:59 -0500

    make sure debounce is indepdent, per bufnr (buffer)

--- lua/ask-openai/predictions/rx.tests.lua
+++ lua/ask-openai/predictions/rx.tests.lua
@@ -101,5 +101,39 @@ describe("Observable:debounceByKey", function()
                 { bufnr = 2, value = 'b' },
             }, received)
         end)
+
+        it("does not allow one bufnr to debounce another", function()
+            local input_events, debounced_by_bufnr = debounced.create_debounced_observable_by_bufnr()
+
+            local received = {}
+
+            debounced_by_bufnr:subscribe(function(event)
+                table.insert(received, event)
+            end)
+
+            -- Start debounce for bufnr 1.
+            input_events:onNext({ bufnr = 1, value = "a" })
+
+            local IS_EMPTY = {}
+            vim.wait(150)
+            --- 150ms total for bufnr 1
+            assert.same(IS_EMPTY, received)
+
+            -- This should NOT reset bufnr 1's debounce timer.
+            input_events:onNext({ bufnr = 2, value = "x" })
+            assert.same(IS_EMPTY, received)
+
+            vim.wait(150)
+            -- 300ms total for bufnr 1, but only 150ms from bufnr 2.
+            assert.same({ { bufnr = 1, value = "a" }, }, received)
+
+            vim.wait(150)
+            -- 300ms total for bufnr 2
+
+            assert.same({
+                { bufnr = 1, value = "a" },
+                { bufnr = 2, value = "x" },
+            }, received)
+        end)
     end)
 end)
